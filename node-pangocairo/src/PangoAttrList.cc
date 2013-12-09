23ef2c092bbbc5d1baa86ca5803c44e614017989
Simon Cozens <simon@simon-cozens.org>
2013-12-09 09:01:09 +0900

    Support italic/oblique styles

--- node-pangocairo/src/PangoAttrList.cc
+++ node-pangocairo/src/PangoAttrList.cc
@@ -39,6 +39,14 @@ NodePangoAttrList::Insert(const Arguments &args) {
   } else if (!strcmp(*type, "family")) {
     v8::String::Utf8Value lang (args[1]->ToString());
     pango_attr_list_insert(l->_attrlist, pango_attr_family_new(*lang));
+  } else if (!strcmp(*type, "style")) {
+    PangoStyle s;
+    v8::String::Utf8Value lang (args[1]->ToString());
+    if (!strcmp(*lang, "oblique")) s = PANGO_STYLE_OBLIQUE;
+    else if (!strcmp(*lang, "italic")) s = PANGO_STYLE_ITALIC;
+    else s = PANGO_STYLE_NORMAL;
+    pango_attr_list_insert(l->_attrlist, pango_attr_style_new(s));
+
   } else if (!strcmp(*type, "size")) {
     pango_attr_list_insert(l->_attrlist, pango_attr_size_new(args[1]->IntegerValue() * PANGO_SCALE));
   } else if (!strcmp(*type, "weight")) {
