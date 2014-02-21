21407b8c435b350b123b86ae3c8c8b496eae1a02
Simon Cozens <simon@simon-cozens.org>
2014-02-21 19:53:19 +0900

    Do this a completely different way.

--- packages/grid.lua
+++ packages/grid.lua
@@ -1,29 +1,44 @@
+
+local gridLeading = function (this, v)
+  if (this.state.frameTotals.gridCursor % this.state.gridSpacing) == 0 then return end
+  local lead = this.state.gridSpacing - (this.state.frameTotals.gridCursor % this.state.gridSpacing);
+  if this.state.frameTotals.gridCursor then
+    this.super.pushVglue(this, {height = SILE.length.new({ length = lead, stretch = 0, shrink = 0})})
+    this.state.frameTotals.gridCursor = this.state.frameTotals.gridCursor + lead
+  end
+end
+
 local pushVbox = function(this, spec)
   if not this.state.frameTotals.gridCursor then this.state.frameTotals.gridCursor = 0 end
-  this.state.frameTotals.gridCursor = this.state.frameTotals.gridCursor + spec.height.length + spec.depth.length;
-  this.super.pushVbox(this, spec);
+  local vbox = SILE.nodefactory.newVbox(spec)
+  this.state.frameTotals.gridCursor = this.state.frameTotals.gridCursor + vbox.height.length  + vbox.depth.length
+  local remainder = (this.state.frameTotals.gridCursor % this.state.gridSpacing);
+  if not (remainder == 0) then
+    local lead = this.state.gridSpacing - remainder
+    vbox.depth = vbox.depth + lead
+    this.state.frameTotals.gridCursor = this.state.frameTotals.gridCursor + lead
+  end
+  table.insert(this.state.outputQueue,vbox)
 end
 
 local pushVglue = function(this, spec)
   if not this.state.frameTotals.gridCursor then this.state.frameTotals.gridCursor = 0 end
   this.state.frameTotals.gridCursor = this.state.frameTotals.gridCursor + spec.height.length;
-  this.super.pushVglue(this, spec);
-end
-
-local gridLeading = function (this, v)
-  if not this.state.frameTotals.gridCursor then this.state.frameTotals.gridCursor = 0 end
-  local lead = this.state.gridSpacing - (this.state.frameTotals.gridCursor % this.state.gridSpacing);
-  if this.state.frameTotals.gridCursor then
-    this:pushVglue({height = SILE.length.new({ length = lead, stretch = 0, shrink = 0})})
-  end
+  local remainder = (this.state.frameTotals.gridCursor % this.state.gridSpacing);
+  if not (remainder == 0) then
+    local lead = this.state.gridSpacing - remainder
+    spec.height = spec.height + lead
+    this.state.frameTotals.gridCursor = this.state.frameTotals.gridCursor + lead
+  end  
+  this.super.pushVglue(this, spec);  gridLeading(this)
 end
 
 SILE.registerCommand("grid", function(options, content)
   local t = SILE.typesetter;
-  SILE.typesetter = std.tree.clone(SILE.typesetter);
+  SILE.typesetter = SILE.typesetter {};
   SILE.typesetter.super = t;
   SILE.typesetter.state.gridSpacing = SILE.parseComplexFrameDimension(options.spacing,"h");
-  SILE.typesetter.insertLeading = gridLeading;
+  SILE.typesetter.insertLeading = function() end ; -- gridLeading;
   SILE.typesetter.pushVglue = pushVglue;
   SILE.typesetter.pushVbox = pushVbox;
 end)
