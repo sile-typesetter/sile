b3d6df95eaf3bc7954b560e12376097165363748
Simon Cozens <simon@simon-cozens.org>
2014-05-24 10:28:08 +0900

    Let’s aim for better composibility in our document classes.

--- classes/plain.lua
+++ classes/plain.lua
@@ -1,35 +1,15 @@
-SILE.require("packages/counters");
-SILE.scratch.counters.folio = { value= 1, display= "arabic" };
 
-plain = SILE.baseClass {
-  id = "plain"
-}
+plain = SILE.baseClass { id = "plain" }
 
-plain.pageTemplate.frames = {
-  a = SILE.newFrame({ left = "5%", right = "95%", top = "5%", bottom = "90%", id = "a" }),
-  folio = SILE.newFrame({left = "5%", right = "95%", top = "92%", bottom = "97%", id = "folio"})
-};
+plain:declareFrame("a",     {left = "5%",  right = "95%",  top = "5%",  bottom = "90%" });
+plain:declareFrame("folio", {left = "5%",  right = "95%",  top = "92%", bottom = "97%" });
+
 plain.pageTemplate.firstContentFrame = plain.pageTemplate.frames["a"];
+local folio = require("packages/folio");
 
-plain.newPage = function()
-  io.write("["..SILE.formatCounter(SILE.scratch.counters.folio).."] ");
-  local f = SILE.getFrame("folio");
-  if (f) then
-    local nodes = SILE.shaper.shape(SILE.formatCounter(SILE.scratch.counters.folio))
-    table.insert(nodes, 1,
-      SILE.nodefactory.newGlue({ width=SILE.length.new({length = 0, stretch= 10000 })})
-      )
-    table.insert(nodes, 1,
-      SILE.nodefactory.newHbox({ width=SILE.length.new({length = 0}), value = {glyph=0}})
-      )    
-
-    table.insert(nodes,
-      SILE.nodefactory.newGlue({ width=SILE.length.new({length = 0, stretch= 10000 })})
-    )
-    SILE.typesetNaturally(f, nodes)
-  end
-  SILE.scratch.counters.folio.value = SILE.scratch.counters.folio.value + 1
-  return SILE.baseClass.newPage(plain);
+plain.newPage = function(self)
+  folio.outputFolio()
+  return SILE.baseClass.newPage(self);
 end
 
 return plain;
