113c016e4e054e8a711cabfa1c1d4e356090e098
Simon Cozens <simon@simon-cozens.org>
2014-07-14 15:41:38 +0100

    Because the ability to process Docbook straightup is another reason to use SILE.

---
+++ classes/docbook.lua
@@ -0,0 +1,37 @@
+local book = SILE.require("classes/book");
+docbook = book { id = "docbook" };
+SILE.scratch.docbook = {
+  level = "main"
+}
+SILE.registerCommand("preface", function(options, content)
+end)
+
+SILE.registerCommand("section", function(options, content)
+  SILE.scratch.docbook.level = "section"
+  SILE.Commands["increment-counter"]({id="section"},{})
+  SILE.Commands["set-counter"]({id="subsection", value=""},{})
+  SILE.process(content)
+end)
+
+SILE.registerCommand("title", function(options, content)
+  if SILE.scratch.docbook.level == "main" then
+    SILE.Commands["left-running-head"]({}, content)
+    SILE.Commands["docbook-title-main"](options, content)
+  elseif SILE.scratch.docbook.level == "section" then
+    if not SILE.scratch.counters.folio == 1 then
+      SILE.Commands["right-running-head"]({}, content)
+    end
+    SILE.Commands["docbook-title-section"](options, content)
+  end
+end)
+
+SILE.registerCommand("link", function (options, content)
+  SILE.Commands["font"]({style = "italic"}, content)
+  SILE.typesetter:typeset(" (")
+  SILE.Commands["ttfont"]({}, function()
+    SILE.typesetter:typeset(options["xlink:href"])
+  end)
+  SILE.typesetter:typeset(")")
+end)
+
+return docbook
