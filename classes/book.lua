489e23395b59507ae5d35358a84eda4424a0fde1
Simon Cozens <simon@simon-cozens.org>
2014-07-29 10:17:22 +0100

    A first, unsatisfactory crack at insertions.

--- classes/book.lua
+++ classes/book.lua
@@ -3,30 +3,51 @@ local book = plain { id = "book" };
 
 book:declareFrame("r",    {left = "8.3%",            right = "86%",            top = "11.6%",       bottom = "83.3%"        });
 book:declareFrame("l",    {left = "100% - right(r)", right = "100% - left(r)", top = "top(r)",      bottom = "bottom(r)"    });
-book:declareFrame("folio",{left = "left(r)",         right = "right(r)",       top = "bottom(r)+3%",bottom = "bottom(r)+5%" });
+book:declareFrame("folio",{left = "left(r)",         right = "right(r)",       top = "bottom(footnotes)+3%",bottom = "bottom(footnotes)+5%" });
 book:declareFrame("lRH",  {left = "left(l)",         right = "right(l)",       top = "top(l) - 8%", bottom = "top(l)-3%"    });
 book:declareFrame("rRH",  {left = "left(r)",         right = "right(r)",       top = "top(r) - 8%", bottom = "top(r)-3%"    });
-
+book:declareFrame("footnotes", { left="left(r)", right = "right(r)", top = "bottom(r)", bottom="bottom(r)"})
 book.pageTemplate.firstContentFrame = book.pageTemplate.frames["r"];
 
 book:loadPackage("twoside", { oddPageFrameID = "r", evenPageFrameID = "l" });
-
+book:loadPackage("insertions")
 
 if not(SILE.scratch.headers) then SILE.scratch.headers = {}; end
 
-book.newPage = function()
+book.init = function()
+  book:initInsertionClass("footnote", { 
+    insertInto = "footnotes",
+    stealFrom = {"l"; "r"},
+    maxHeight = SILE.length.new({length = SILE.toPoints("25", "%","h") }),
+    topSkip = SILE.length.parse("12pt")
+  })
+  return plain:init()
+end
+
+book.endPage = function()
   if (book:oddPage()) then
+    book:declareFrame("footnotes", { left="left(l)", right = "right(l)", top = "bottom(r)", bottom="bottom(r)"})
     if (SILE.scratch.headers.right) then
       SILE.typesetNaturally(SILE.getFrame("rRH"), SILE.scratch.headers.right);
     end
   else 
+    book:declareFrame("footnotes", { left="left(r)", right = "right(r)", top = "bottom(r)", bottom="bottom(r)"})
     if (SILE.scratch.headers.left) then
       SILE.typesetNaturally(SILE.getFrame("lRH"), SILE.scratch.headers.left);
     end
   end
   book:switchPage();
-  return plain.newPage(book);
+  book:outputInsertions()
+  
+  return plain.endPage(book);
 end;
+
+SILE.registerCommand("footnote", function(options, content)
+  book:insert("footnote", SILE.Commands["vbox"]({}, function()
+    SILE.Commands["font"]({size = "9pt"}, content)
+  end
+  ))
+end)
 
 SILE.registerCommand("left-running-head", function(options, content)
   SILE.settings.temporarily(function()
---
+++ packages/insertions.lua
@@ -0,0 +1,114 @@
+SILE.scratch.insertions = {
+  classes = {},
+  thispage = {},
+  nextpage = {}
+}
+
+local initInsertionClass = function (self, classname, options)
+  SU.required(options, "insertInto", "initializing insertions")
+  SU.required(options, "stealFrom", "initializing insertions")
+  SU.required(options, "maxHeight", "initializing insertions")
+  SU.required(options, "topSkip", "initializing insertions")
+
+  SILE.scratch.insertions.classes[classname] = options
+end
+
+local reduceHeight = function(classname, amount)
+  SU.debug("insertions", "Shrinking main box by "..amount.length)
+  local opts = SILE.scratch.insertions.classes[classname]
+  local reduceList = opts["stealFrom"]
+  if type(reduceList) == "string" then reduceList = { reduceList } end
+  local stealPosition = opts["steal-position"] or "bottom"
+  for i = 1,#reduceList do local f = SILE.getFrame(reduceList[i])
+    local newHeight = f:height() - amount.length
+    f.height = function () return newHeight end
+    -- This will have to change when cassowary is implemented
+    if stealPosition == "bottom" then
+      local newBottom = f:bottom() - amount.length
+      f.bottom = function () return newBottom end
+    else
+      local newTop = f:top() - amount.length
+      f.top = function () return newTop end
+    end
+  end
+  local f = SILE.getFrame(opts["insertInto"])
+  local newHeight = f:height() + amount.length
+  f.height = function () return newHeight end
+  if stealPosition == "bottom" then 
+    local newTop = f:top() - amount.length
+    f.top = function () return newTop end
+  end
+end
+
+local addInsertion = function(classname, material)
+  reduceHeight(classname, material.height + material.depth)
+  if material:isVbox() then 
+    material.height = SILE.length.new({ length =  0 })
+  end
+  table.insert(SILE.scratch.insertions.thispage[classname], material)
+end
+
+local heightSoFar = function(classname)
+  local h = 0
+  for i = 1,#(SILE.scratch.insertions.thispage[classname]) do 
+    local ins = SILE.scratch.insertions.thispage[classname][i]
+    h = h + ins.height.length + ins.depth
+  end
+  return h
+end
+
+local insert = function (self, classname, vbox)
+  local thisclass = SILE.scratch.insertions.classes[classname]
+  if not thisclass then SU.error("Uninitialized insertion class "..classname) end
+  local opts = SILE.scratch.insertions.classes[classname]
+
+  if not SILE.scratch.insertions.thispage[classname] then
+    SILE.scratch.insertions.thispage[classname] = {}
+    if thisclass["topSkip"] then
+      local vglue = SILE.nodefactory.newVglue({ height = thisclass["topSkip"] })
+      addInsertion(classname, vglue)
+      -- HACK?
+      local f = SILE.getFrame(opts["insertInto"])
+      local newTop = f:top() + vglue.height.length
+      f.top = function () return newTop end
+    end
+  end
+
+  SU.debug("insertions", "Maxheight is "..thisclass["maxHeight"])
+  SU.debug("insertions", "height is "..(heightSoFar(classname) + vbox.height + vbox.depth))
+  if heightSoFar(classname) + vbox.height + vbox.depth < thisclass["maxHeight"] and
+    ( (vbox.height + vbox.depth).length < 0 or
+    true -- XXX "\pagetotal plus \pagedepth minus \pageshrink plus the effective
+         -- size of the insertion should be less than \pagegoal"
+    ) then
+    addInsertion(classname, vbox)
+  else
+    SU.error("I need to split this insertion and I don't know how")
+  end
+end
+
+local outputInsertions = function(self)
+
+  for classname,vboxes in pairs(SILE.scratch.insertions.thispage) do
+    local opts = SILE.scratch.insertions.classes[classname]
+    local f = SILE.getFrame(opts["insertInto"])
+    SILE.outputter:debugFrame(f)
+    SILE.outputter:debugFrame(SILE.getFrame("l"))
+    local t = SILE.defaultTypesetter {}
+    t:init(f)
+    for i = 1,#vboxes do 
+      SU.debug("insertions", "Cursor now "..t.frame.state.cursorY)
+      vboxes[i]:outputYourself(t,vboxes[i])
+    end
+    SILE.scratch.insertions.thispage[classname] = SILE.scratch.insertions.nextpage[classname]
+  end
+end
+
+return {
+  init = function () end,
+  exports = {
+    initInsertionClass = initInsertionClass,
+    insert = insert,
+    outputInsertions = outputInsertions
+  }
+}
