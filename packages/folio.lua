67910f8f0e039f66841a3d5bfce8fbb3f97aa35b
Simon Cozens <simon@simon-cozens.org>
2014-08-13 22:19:12 +0100

    Turn folios off and on at will.

--- packages/folio.lua
+++ packages/folio.lua
@@ -2,11 +2,22 @@
 SILE.require("packages/counters");
 SILE.scratch.counters.folio = { value= 1, display= "arabic" };
 
+SILE.registerCommand("folios", function () SILE.scratch.counters.folio.off = false end)
+SILE.registerCommand("nofolios", function () SILE.scratch.counters.folio.off = true end)
+SILE.registerCommand("nofoliosthispage", function () SILE.scratch.counters.folio.off = 2 end)
+
 return {
   init = function () end,
   exports = {
     outputFolio = function (this, frame) 
       if not frame then frame = "folio" end
+      if SILE.scratch.counters.folio.off then 
+        if SILE.scratch.counters.folio.off == 2 then
+          SILE.scratch.counters.folio.off = false
+        end
+        SILE.scratch.counters.folio.value = SILE.scratch.counters.folio.value + 1
+        return 
+      end
       io.write("["..SILE.formatCounter(SILE.scratch.counters.folio).."] ");
       local f = SILE.getFrame("folio");
       if (f) then
