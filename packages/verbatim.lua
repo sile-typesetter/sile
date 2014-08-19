71ad9328b984058b0023ac554f9d1eaf5e061d75
Simon Cozens <simon@simon-cozens.org>
2014-08-19 12:11:26 +0100

    Use settings not subclassing.

--- packages/verbatim.lua
+++ packages/verbatim.lua
@@ -1,27 +1,21 @@
 
 SILE.registerCommand("verbatim", function(options, content)
-  local t = SILE.typesetter;
-  t:pushVglue({ height = SILE.length.new({ length = 6 }) })
-  t:leaveHmode()
-  SILE.typesetter = SILE.typesetter {}
-  -- Keep lines
-  SILE.typesetter.parSepPattern = "\n"
-
-  -- Set ragged right
+  SILE.typesetter:pushVglue({ height = SILE.length.new({ length = 6 }) })
+  SILE.typesetter:leaveHmode()
   SILE.settings.temporarily(function()
+    SILE.settings.set("typesetter.parseppattern", "\n")
     SILE.settings.set("document.rskip", SILE.nodefactory.newGlue("0 plus 10000pt"))
     SILE.settings.set("document.parindent", SILE.nodefactory.newGlue("0"))
     SILE.settings.set("current.parindent", SILE.nodefactory.newGlue("0"))
     SILE.settings.set("document.baselineskip", SILE.nodefactory.newVglue("0"))
     SILE.settings.set("document.lineskip", SILE.nodefactory.newVglue("2pt"))
-    SILE.settings.set("document.spaceskip", SILE.length.parse("1en"))
     SILE.settings.set("font.family", "Monaco") -- XXX
     SILE.settings.set("font.size", SILE.settings.get("font.size") - 3)
+    SILE.settings.set("document.spaceskip", SILE.length.parse("1en"))
     SILE.settings.set("document.language", "xx")
     SILE.settings.set("shaper.spacepattern", '%s')
     SILE.process(content)
   end)
 
-  t:pushVglue({ height = SILE.length.new({ length = 6 }) })
-  SILE.typesetter = t
+  SILE.typesetter:pushVglue({ height = SILE.length.new({ length = 6 }) })
 end, "Typesets its contents in a monospaced font.")
