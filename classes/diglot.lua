79738376855964045813dd0803aaf0cb1a0b9d6e
Simon Cozens <simon@simon-cozens.org>
2014-08-28 07:26:50 +0100

    This actually makes sense now.

--- classes/diglot.lua
+++ classes/diglot.lua
@@ -7,11 +7,13 @@ if not(SILE.scratch.headers) then SILE.scratch.headers = {}; end
 
 diglot:declareFrame("a",    {left = "8.3%",            right = "48%",            top = "11.6%",       bottom = "80%"        });
 diglot:declareFrame("b",    {left = "52%",             right = "100% - left(a)", top = "top(a)",      bottom = "bottom(a)"    });
-diglot:declareFrame("folio",{left = "left(a)",         right = "right(b)",       top = "bottom(a)+3%",bottom = "bottom(a)+5%" });
-
-diglot.pageTemplate.firstContentFrame = diglot.pageTemplate.frames["a"];
+diglot:declareFrame("folio",{left = "left(a)",         right = "right(b)",       top = "bottom(a)+3%",bottom = "bottom(a)+8%" });
 diglot.leftTypesetter = SILE.defaultTypesetter {};
+diglot.leftTypesetter.which = "left"
 diglot.rightTypesetter = SILE.defaultTypesetter {};
+diglot.rightTypesetter.which = "right"
+diglot.rightTypesetter.other = diglot.leftTypesetter
+diglot.leftTypesetter.other = diglot.rightTypesetter
 
 local sync =   function()
   local lVbox = SILE.pagebuilder.collateVboxes(diglot.leftTypesetter.state.outputQueue)
@@ -28,22 +30,32 @@ local sync =   function()
 
 end
 
-diglot.newPage = function()
-  diglot.rightTypesetter:leaveHmode(1);
-  diglot.leftTypesetter:leaveHmode(1);
-  diglot.leftTypesetter:init(diglot.pageTemplate.frames["a"])
-  diglot.rightTypesetter:init(diglot.pageTemplate.frames["b"])
-  plain:newPage()
-  SILE.typesetter = diglot.rightTypesetter
-  return diglot.pageTemplate.frames["a"]
+diglot.finish = function(self)
+  table.insert(SILE.typesetter.other.state.outputQueue, SILE.nodefactory.vfillGlue)
+  SILE.typesetter.other:chuck()
+  plain.finish(self)
 end
 
-diglot.finish = function(self)
-  diglot.leftTypesetter.frame = diglot.pageTemplate.frames["a"]
-  diglot.rightTypesetter.frame = diglot.pageTemplate.frames["b"]
-  diglot.leftTypesetter:chuck()
-  diglot.rightTypesetter:chuck()
-  diglot:endPage()
+diglot.endPage = function(self)
+  SILE.typesetter.other:leaveHmode(1)
+  plain.endPage(self)
+end
+
+diglot.newPage = function(self)
+  plain.newPage(self)
+  if SILE.typesetter.which == "left" then 
+    SILE.typesetter.other:initFrame(SILE.getFrame("b"))
+    return SILE.getFrame("a")
+  else
+    SILE.typesetter.other:initFrame(SILE.getFrame("a"))
+    return SILE.getFrame("b")
+  end
+end
+
+diglot.init = function(self)
+  diglot.leftTypesetter:init(SILE.getFrame("a"))
+  diglot.rightTypesetter:init(SILE.getFrame("b"))
+  return SILE.baseClass.init(self)
 end
 
 SILE.registerCommand("leftfont", function(options, content)
@@ -57,18 +69,12 @@ end, "Set the font for the right side")
 SILE.registerCommand("left", function(options, content)
   SILE.settings.set("typesetter.parseppattern", -1)
   SILE.typesetter = diglot.leftTypesetter;
-  if (not SILE.typesetter.frame) then 
-    SILE.typesetter:init(diglot.pageTemplate.frames["a"]) 
-  end
   SILE.Commands["font"](SILE.scratch.diglot.leftfont, {})
 end, "Begin entering text on the left side")
 
 SILE.registerCommand("right", function(options, content)
   SILE.settings.set("typesetter.parseppattern", -1)  
   SILE.typesetter = diglot.rightTypesetter;
-  if (not SILE.typesetter.frame) then 
-    SILE.typesetter:init(diglot.pageTemplate.frames["b"]) 
-  end
   SILE.Commands["font"](SILE.scratch.diglot.rightfont, {})
 end, "Begin entering text on the right side")
 
