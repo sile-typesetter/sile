c671058015174f54d07b49aaf5b0fbad5263b15e
Simon Cozens <simon@simon-cozens.org>
2014-09-11 11:08:10 +0900

    Various fixes.

--- core/cairo-output.lua
+++ core/cairo-output.lua
@@ -1,6 +1,8 @@
 local lgi = require("lgi");
 local cairo = lgi.cairo
 local pango = lgi.Pango
+local fm = lgi.PangoCairo.FontMap.get_default()
+local pango_context = lgi.Pango.FontMap.create_context(fm)
 
 if (not SILE.outputters) then SILE.outputters = {} end
 
@@ -25,6 +27,8 @@ SILE.outputters.cairo = {
   end,
   showGlyphString = function(f,pgs, options)
     sgs(cr, f,pgs)
+  end,
+  setFont = function (f,font)
   end,
   drawPNG = function (src, x,y,w,h)
     local image = cairo.ImageSurface.create_from_png(src)
@@ -66,9 +70,12 @@ SILE.outputters.cairo = {
   end,
   debugHbox = function(typesetter, hbox, scaledWidth)
     cr:set_source_rgb(0.9,0.9,0.9);
+    cr:set_line_width(0.5);
     cr:rectangle(typesetter.frame.state.cursorX, typesetter.frame.state.cursorY-(hbox.height), scaledWidth, hbox.height+hbox.depth);
     if (hbox.depth) then cr:rectangle(typesetter.frame.state.cursorX, typesetter.frame.state.cursorY-(hbox.height), scaledWidth, hbox.height); end
+    cr:stroke();
     cr:set_source_rgb(0,0,0);
+    cr:move_to(typesetter.frame.state.cursorX, typesetter.frame.state.cursorY);
   end
 }
 
