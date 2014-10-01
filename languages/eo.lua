884fd590090c2c0cdeb10012f107e33d66055ef9
Joop Kiefte <ikojba@gmail.com>
2014-10-01 21:39:04 +0200

    Create eo.lua
    Basic eo.lua file. According to the official rules hyphenation can happen anywhere. Any improvement made on this would be mostly cosmetic.

---
+++ languages/eo.lua
@@ -0,0 +1,4 @@
+SILE.hyphenator.languages["el"] = {exceptions={}};
+SILE.hyphenator.languages["el"].patterns = {
+"a1", "b1", "c1", "ĉ1", "d1", "e1", "f1", "g1", "ĝ1", "h1", "ĥ1", "h1", "i1", "j1", "ĵ1", "k1", "l1", "m1", "n1", "o1", "p1", "r1", "s1", "ŝ1", "t1", "u1", "ŭ1", "v1", "z1",
+}
