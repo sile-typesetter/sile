c469e737775a814a1fa5f9de40530a65ac161b2d
Simon Cozens <simon@simon-cozens.org>
2014-10-18 07:59:35 +0900

    Fix the REPL. It’s really, really useful to have a working one.

--- core/sile.lua
+++ core/sile.lua
@@ -108,9 +108,18 @@ end
 
 function SILE.repl()
   if _VERSION:match("5.2") then
-    -- No repl for you!
-    print("luarepl does not support 5.2; sorry, REPL unavailable.")
-    return
+    setfenv = function(f, t)
+        f = (type(f) == 'function' and f or debug.getinfo(f + 1, 'f').func)
+        local name
+        local up = 0
+        repeat
+            up = up + 1
+            name = debug.getupvalue(f, up)
+        until name == '_ENV' or name == nil
+        if name then
+    debug.upvaluejoin(f, up, function() return t end, 1) -- use unique upvalue, set it to f
+        end
+    end
   end
   if not SILE._repl then SILE.initRepl() end
   SILE._repl:run()
