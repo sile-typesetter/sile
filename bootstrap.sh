41df6fa8530263ba82a2318a71b085f455e1329b
Simon Cozens <simon@simon-cozens.org>
2014-10-23 16:28:01 +0900

    Don't forget to checkout the submodule!

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 touch NEWS README AUTHORS THANKS ChangeLog # HATE YOU GNU
+git submodule update --init --recursive
 autoreconf --install
 sed 's/core//g' configure > config.cache; mv config.cache configure ; chmod +x configure
