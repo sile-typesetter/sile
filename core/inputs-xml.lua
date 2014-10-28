c99a6d1a5da58f28cb32dea11f374b90fbce4c01
Simon Cozens <simon@simon-cozens.org>
2014-10-28 11:48:42 +0900

    More mucking about with preambles.

--- core/inputs-xml.lua
+++ core/inputs-xml.lua
@@ -3,7 +3,7 @@ SILE.inputs.XML = {
     local lom = require("lxp.lom")
     local fh = io.open(fn)
     local t = lom.parse(fh:read("*all"))
-    local root = SILE.documentState.documentClass == nil
+    local root = not SILE.preamble
     if root then
       if not(t.tag == "sile") then
         SU.error("This isn't a SILE document!")
