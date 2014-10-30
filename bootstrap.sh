55e1f345c4ca759693cf544f5c7c63b294aa1a75
Simon Cozens <simon@simon-cozens.org>
2014-10-30 14:50:24 +0900

    Tidy this up.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,7 @@
 touch NEWS README AUTHORS THANKS ChangeLog # HATE YOU GNU
 git submodule update --init --recursive
 autoreconf --install
-sed 's/core//g' configure > config.cache; mv config.cache configure ; chmod +x configure
+(cd libtexpdf; autoreconf)
+sed 's/core conftest.err/conftest.err/' configure > config.cache
+mv config.cache configure
+chmod +x configure
