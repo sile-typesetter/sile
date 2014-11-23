5bf23289cbfa22f77d600ba35a494221c67e2eae
Simon Cozens <simon@simon-cozens.org>
2014-11-23 22:46:22 +0900

    Long-standing grid bug fix.

--- packages/grid.lua
+++ packages/grid.lua
@@ -1,36 +1,59 @@
 local gridSpacing -- Should be a setting
 
+local makeUp = function ()
+  local toadd = gridSpacing - (SILE.typesetter.frame.state.totals.gridCursor % gridSpacing)   
+  SILE.typesetter.frame.state.totals.gridCursor = SILE.typesetter.frame.state.totals.gridCursor + toadd
+  return SILE.nodefactory.newVglue({ height = SILE.length.new({ length = toadd }) })
+end
+
 local leadingFor = function(this, vbox, previous)
-  if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = 0 end
-  if not previous then 
-    return SILE.nodefactory.newVglue({height = SILE.length.new()});
-  end
-  this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + previous.height.length
-  if previous:isVbox() then 
-    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + previous.depth.length
-  end
-  local lead = gridSpacing - (this.frame.state.totals.gridCursor % gridSpacing);
-  this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor  + lead
-  return SILE.nodefactory.newVglue({height = SILE.length.new({ length = lead })});
+  if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = SILE.typesetter.frame.state.cursorY end
+  this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + previous.height.length + previous.depth.length
+  return makeUp()
 end
 
 local pushVglue = function(this, spec)
-  if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = 0 end
+  if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = SILE.typesetter.frame.state.cursorY end
+  this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + spec.height.length
   SILE.defaultTypesetter.pushVglue(this, spec);
-  SILE.defaultTypesetter.pushVglue(this, leadingFor(this, nil, SILE.nodefactory.newVglue(spec)));
+  SILE.defaultTypesetter.pushVglue(this, makeUp())
+end
+
+local newBoxup = function (this)
+  local b = SILE.defaultTypesetter.boxUpNodes(this)
+  if #b > 1 then
+    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + b[#b].height.length + b[#b].depth.length
+  end
+  return b
+end
+
+local debugGrid = function()
+  local t = SILE.typesetter
+  if not t.frame.state.totals.gridCursor then t.frame.state.totals.gridCursor = SILE.typesetter.frame.state.cursorY end
+  local g = t.frame.state.totals.gridCursor
+  while g < t.frame:bottom() do
+    SILE.outputter.rule(t.frame:left(), g, t.frame:width(), 0.1)
+    g = g + gridSpacing
+  end
 end
 
 SILE.registerCommand("grid", function(options, content)
   SU.required(options, "spacing", "grid package")
-  -- t:initline()
-  -- t:leaveHmode()
   gridSpacing = SILE.parseComplexFrameDimension(options.spacing,"h");
+  SILE.typesetter:chuck()
+
   SILE.typesetter.leadingFor = leadingFor
-  SILE.typesetter.pushVglue = pushVglue;
+  SILE.typesetter.pushVglue = pushVglue
+  SILE.typesetter.boxUpNodes = newBoxup
+  SILE.typesetter.setVerticalGlue = function () end
+  SILE.typesetter.frame.state.totals.gridCursor = SILE.typesetter.frame.state.cursorY
+  -- add some now
+  SILE.defaultTypesetter.pushVglue(SILE.typesetter, makeUp())
 end, "Begins typesetting on a grid spaced at <spacing> intervals.")
 
 SILE.registerCommand("no-grid", function (options, content)
   SILE.typesetter.leadingFor = SILE.defaultTypesetter.leadingFor
   SILE.typesetter.pushVglue = SILE.defaultTypesetter.pushVglue
+  SILE.typesetter.setVerticalGlue = SILE.defaultTypesetter.setVerticalGlue
   -- SILE.typesetter.state = t.state
 end, "Stops grid typesetting.")
