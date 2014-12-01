6dc22dcf1de25566c004b0eafca66bf990985cbe
Simon Cozens <simon@simon-cozens.org>
2014-12-01 13:45:00 +0900

    Rotatable boxes.

--- packages/rotate.lua
+++ packages/rotate.lua
@@ -29,13 +29,69 @@ end
 table.insert(SILE.framePrototype.enterHooks, enter)
 table.insert(SILE.framePrototype.leaveHooks, leave)
 
--- This code in't there yet.
+-- What is the width, depth and height of a rectangle width w and height h rotated by angle theta?
+-- rect1 = Rectangle[{0, 0}, {w, h}]
+-- {{xmin, xmax}, {ymin, ymax}} = Refine[RegionBounds[TransformedRegion[rect1, 
+--                                                     RotationTransform[theta, {w/2,h/2}]]],
+--                                      w > 0 && h > 0 && theta > 0 && theta < 2 Pi ]
+-- PiecewiseExpand[xmax - xmin]
+    -- \[Piecewise]  -w Cos[theta]-h Sin[theta]  Sin[theta]<=0&&Cos[theta]<=0
+    --                w Cos[theta]-h Sin[theta]  Sin[theta]<=0&&Cos[theta]>0
+    --               -w Cos[theta]+h Sin[theta]  Sin[theta]>0&&Cos[theta]<=0
+    --                w Cos[theta]+h Sin[theta]  True
 
--- SILE.registerCommand("rotate", function(options, content)
---   local x = math.rad(SU.required(options, "angle", "rotate command"))
---   local hbox = SILE.Commands["hbox"]({}, content)
+local outputRotatedHbox = function (self, typesetter,line)
+  local origbox = self.value.orig
+  local x = self.value.theta
+  -- Find origin of untransformed hbox
+  local save = typesetter.frame.state.cursorX
+  typesetter.frame.state.cursorX = typesetter.frame.state.cursorX - (origbox.width.length-self.width)/2
+  
+  local horigin = typesetter.frame.state.cursorX + origbox.width.length / 2
+  local vorigin = -(typesetter.frame.state.cursorY - (origbox.height) /2)
+  pdf:gsave()
+  pdf.setmatrix(1,0,0,1,horigin,vorigin)
+  pdf.setmatrix(math.cos(x), math.sin(x), -math.sin(x), math.cos(x), 0, 0)
+  pdf.setmatrix(1,0,0,1,-horigin,-vorigin)
+  origbox:outputYourself(typesetter,line)
+  pdf:grestore()
+  typesetter.frame.state.cursorX = save
+  typesetter.frame:moveX(self.width)
+end
 
---   pdf.gsave()
---   pdf.setmatrix(math.cos(x), math.sin(x), -math.sin(x), math.cos(x), 0, 0)
---   pdf.grestore()
--- end)
+SILE.registerCommand("rotate", function(options, content)
+  local theta = -math.rad(SU.required(options, "angle", "rotate command"))
+  local origbox = SILE.Commands["hbox"]({}, content)
+  SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes] = nil
+  local h = (origbox.height + origbox.depth)
+  local w = origbox.width.length
+  local st = math.sin(theta)
+  local ct = math.cos(theta)
+  local height, width, depth
+  if st <= 0 and ct <= 0    then 
+    width  = -w * ct - h * st
+    height = 0.5*(h-h*ct-w*st)
+    depth  = 0.5*(h+h*ct+w*st)
+  elseif st <=0 and ct > 0  then 
+    width  =  w * ct - h * st
+    height = 0.5*(h+h*ct-w*st)
+    depth  = 0.5*(h-h*ct+w*st)
+  elseif st > 0 and ct <= 0 then
+    width  = -w * ct + h * st
+    height = 0.5*(h-h*ct+w*st)
+    depth  = 0.5*(h+h*ct-w*st)
+  else
+    width  =  w * ct + h * st
+    height = 0.5*(h+h*ct+w*st)
+    depth  = 0.5*(h-h*ct-w*st)
+  end
+  depth = -depth
+  if depth < 0 then depth = 0 end
+  SILE.typesetter:pushHbox({ 
+    value = { orig = origbox, theta = theta},
+    height = height,
+    width = width,
+    depth = depth,
+    outputYourself= outputRotatedHbox
+  });
+end)
