e3611e5b43b9026de076062241f0703db5c86952
Simon Cozens <simon@simon-cozens.org>
2015-02-11 16:33:23 +0900

    Another attempt to get older autotools going.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git submodule update --init --recursive
 autoreconf --install
-(cd libtexpdf; autoreconf; aclocal -Im4; autoconf)
+(cd libtexpdf; autoreconf -I m4)
 sed 's/rm -f core/rm -f/' configure > config.cache
 mv config.cache configure
 chmod +x configure
