bd9514ffbbfd789b0682531ae02999737b243b7a
Simon Cozens <simon@simon-cozens.org>
2015-02-27 21:40:48 +0900

    And frankly my Perl is't that great either.

--- tests/attack.pl
+++ tests/attack.pl
@@ -1,5 +1,5 @@
 for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
     next if /macros.sil/;
     print "### Compiling $_\n";
-    exit $? if system("./sile", $_);
+    exit $? >> 8 if system("./sile", $_);
 }
