8db26502e4d0fab0f18a197a50e590517948302b
Simon Cozens <simon@simon-cozens.org>
2015-03-23 10:52:56 +0900

    A proper regression test suite. (Font version dependent? Let’s find out.)

--- tests/attack.pl
+++ tests/attack.pl
@@ -1,5 +1,16 @@
+use strict; use warnings;
 for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
     next if /macros.sil/;
     print "### Compiling $_\n";
-    exit $? >> 8 if system("./sile", $_);
+    my $expectation = $_; $expectation =~ s/\.sil$/\.expected/;
+    if (-f $expectation) {
+        my $out = $_; $out =~ s/\.sil$/\.actual/;
+        exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
+        if (system("diff -u $expectation $out")) {
+            print("\n\n<<< EXPECTED >>>\n"); system("cat $expectation");  
+            print("\n\n<<< GOT >>>\n"); system("cat $out");  
+        }
+    } else {
+        exit $? >> 8 if system("./sile", $_);
+    }
 }
