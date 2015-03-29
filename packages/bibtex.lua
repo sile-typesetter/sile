33e061c044c3e64e03ab95ff627dba1a0c81ba14
Simon Cozens <simon@simon-cozens.org>
2015-03-29 16:34:49 +0900

    Some fixes to the bibtex parser.

--- packages/bibtex.lua
+++ packages/bibtex.lua
@@ -1,23 +1,24 @@
 epnf = require( "epnf" )
 
-local ID = lpeg.C( SILE.parserBits.letter * (SILE.parserBits.letter+SILE.parserBits.digit)^0 )
+local ID = lpeg.C(  (SILE.parserBits.letter+SILE.parserBits.digit)^1 )
 local identifier = (ID + lpeg.P("-"))^1
 
-local balanced = lpeg.C{ "{" * lpeg.P(" ")^0 * lpeg.C(((1 - lpeg.S"{}") + lpeg.V(1))^0) * "}" } 
+local balanced = lpeg.C{ "{" * lpeg.P(" ")^0 * lpeg.C(((1 - lpeg.S"{}") + lpeg.V(1))^0) * "}" } / function(...) t={...}; return t[2] end
 local doubleq = lpeg.C( lpeg.P '"' * lpeg.C(((1 - lpeg.S '"\r\n\f\\') + (lpeg.P '\\' * 1)) ^ 0) * '"' )
 
 bibtexparser = epnf.define(function (_ENV)
   local _ = WS^0
   local sep = lpeg.S(",;") * _
   local myID = C( identifier + lpeg.P(1) ) / function (t) return t end
-  local value = balanced + doubleq
+  local value = balanced + doubleq + myID
   local pair = lpeg.Cg(myID * _ * "=" * _ * C(value)) * _ * sep^-1   / function (...) local t= {...}; return t[1], t[#t] end
   local list = lpeg.Cf(lpeg.Ct("") * pair^0, rawset)
 
   START "document";
-  document = (V"entry" + V"comment")^1
+  document = (V"entry" + V"comment")^1 * (-1 + E("Unexpected character at end of input"))
   comment  = WS + 
-    ((P("%") * (1-lpeg.S("\r\n"))^0 * lpeg.S("\r\n")) /function () return "" end) -- Don't bother telling me about comments
+    ( V"blockcomment" + (P("%") * (1-lpeg.S("\r\n"))^0 * lpeg.S("\r\n")) /function () return "" end) -- Don't bother telling me about comments
+  blockcomment = P("@comment")+ balanced/function () return "" end -- Don't bother telling me about comments
   entry = Ct( P("@") * Cg(myID, "type") * _ * P("{") * _ * Cg(myID, "label") * _ * sep * list * P("}") * _ )
 end)
 
@@ -25,11 +26,9 @@ local parseBibtex = function(fn)
   local fh = io.open(fn)
   local doc = fh:read("*all")
   local t = epnf.parsestring(bibtexparser, doc)
-  print(doc)
   if not(t) or not(t[1]) or t.id ~= "document" then
     SU.error("Error parsing bibtex")
   end
-  foobar = t
   local entries = {}
   for i =1,#t do
     if t[i].id == "entry" then
