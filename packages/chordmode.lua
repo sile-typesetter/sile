dbaa19a279c1ae9c1caa392e6932c1eb6c4227ea
Simon Cozens <simon@simon-cozens.org>
2015-04-02 11:29:10 +0900

    Revert ""Everything should be as simple as possible, but not simpler.""
    This reverts commit 1d1012c285173987afe4c1145e98f3951c02147f.

--- packages/chordmode.lua
+++ packages/chordmode.lua
@@ -5,16 +5,69 @@ SILE.require("packages/raiselower")
 
 local function addChords(text, content)
   local result = {}
+  local chordName = nil
+  local currentText = ""
+  local process = nil
+  local processText, processChordName, processChordText
 
-  for token in SU.gtoke(text, "<[^>]+>") do
-    if (token.string) then
-      table.insert(result, token.string)
-    else
-      local c = string.sub(token.separator, 2, -2)
-      table.insert(result, inputfilter.createCommand(
-        content.pos, content.col, content.line,
-        "ch", {name=c}))
+  local function insertChord()
+    table.insert(result, inputfilter.createCommand(
+      content.pos, content.col, content.line,
+      "ch", {name=chordName}, currentText
+    ))
+    chordName = nil
+  end
+
+  local function insertText()
+    if (#currentText>0) then table.insert(result, currentText) end
+    currentText = ""
+  end
+
+  local function ignore(separator)
+    currentText = currentText .. separator
+  end
+
+  processText = {
+    ["<"] = function(separator)
+      insertText()
+      process = processChordName
     end
+  }
+
+  processChordName = {
+    [">"] = function(separator)
+      chordName = currentText
+      currentText = ""
+      process = processChordText
+    end
+  }
+
+  processChordText = {
+    ["<"] = function(separator)
+      insertChord()
+      currentText = ""
+      process = processChordName
+    end;
+    ["\n"] = function(separator)
+      insertChord()
+      currentText = separator
+      process = processText
+    end
+  }
+  process = processText
+
+  for token in SU.gtoke(text, "[<\n>]") do
+    if(token.string) then
+      currentText = currentText .. token.string
+    else
+      (process[token.separator] or ignore)(token.separator)
+    end
+  end
+
+  if (chordName ~= nil) then
+    insertChord()
+  else
+    insertText()
   end
   return result
 end
