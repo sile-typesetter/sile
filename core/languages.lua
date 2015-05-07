0ac0614d80066b190203bf618bfdb5a643b070d7
Simon Cozens <simon@simon-cozens.org>
2015-05-07 20:50:13 +0900

    Hyphenation data compiler.

--- core/languages.lua
+++ core/languages.lua
@@ -9,6 +9,16 @@ SILE.languageSupport = {
     if not pcall(function () SILE.require("languages/"..language) end) then
       return
     end
+  end,
+  compile = function(language)
+    local ser = require("serpent")
+    -- Ensure things are loaded
+    SILE.languageSupport.loadLanguage(language)
+    SILE.hyphenate({ SILE.nodefactory.newNnode({language=language, text=""}) })
+    local f = io.open("languages/"..language.."-compiled.lua", "w")
+    f:write("_hyphenators."..language.."="..ser.line(_hyphenators[language]).."\n")
+    f:write("SILE.hyphenator.languages."..language.."="..ser.line(SILE.hyphenator.languages[language]).."\n")
+    f:close()
   end
 }
 
