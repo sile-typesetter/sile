60e6d68e6e2c1329488cd9908cd58647b67ec877
Simon Cozens <simon@simon-cozens.org>
2015-05-11 12:06:20 +0900

    Only print a message for the tests we plan to run.

--- tests/attack.pl
+++ tests/attack.pl
@@ -2,9 +2,9 @@ use strict; use warnings;
 my $regression = ($ARGV[0] && $ARGV[0] =~ /--regression/i);
 for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
     next if /macros.sil/;
-    print "### Compiling $_\n";
     my $expectation = $_; $expectation =~ s/\.sil$/\.expected/;
     if (-f $expectation) {
+        print "### Compiling $_\n";
         my $out = $_; $out =~ s/\.sil$/\.actual/;
         exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
         if (system("diff -u $expectation $out")) {
@@ -13,6 +13,7 @@ for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
             exit;
         }
     } elsif (!$regression) {
+        print "### Compiling $_\n";
         exit $? >> 8 if system("./sile", $_);
     }
 }
