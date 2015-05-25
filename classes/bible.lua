5d3b01fe3e4843a52a8dea7c73cc027e65698172
Simon Cozens <simon@simon-cozens.org>
2015-05-26 07:11:07 +0900

    Let’s try balanced frames. Bad interaction with drop caps right now.

--- classes/bible.lua
+++ classes/bible.lua
@@ -16,8 +16,8 @@ end
 function bible:twoColumnMaster()
   local gutterWidth = self.options.gutter or "3%"
   self:defineMaster({ id = "right", firstContentFrame = "contentA", frames = {
-    contentA = {left = "8.3%", right = "left(gutter)", top = "11.6%", bottom = "top(footnotes)", next = "contentB" },
-    contentB = {left = "right(gutter)", width="width(contentA)", right = "86%", top = "11.6%", bottom = "top(footnotes)" },
+    contentA = {left = "8.3%", right = "left(gutter)", top = "11.6%", bottom = "top(footnotes)", next = "contentB", balanced = true },
+    contentB = {left = "right(gutter)", width="width(contentA)", right = "86%", top = "11.6%", bottom = "top(footnotes)", balanced = true },
     gutter = { left = "right(contentA)", right = "left(contentB)", width = gutterWidth },
     folio = {left = "left(contentA)", right = "right(contentB)", top = "bottom(footnotes)+3%",bottom = "bottom(footnotes)+5%" },
     runningHead = {left = "left(contentA)", right = "right(contentB)", top = "top(contentA) - 8%", bottom = "top(contentA)-3%" },
@@ -25,6 +25,7 @@ function bible:twoColumnMaster()
   }})
   -- Later we'll have an option for two fn frames
   self:loadPackage("footnotes", { insertInto = "footnotes", stealFrom = {"contentA", "contentB"} } )
+  self:loadPackage("balanced-frames")
 end
 
 local _twocolumns
