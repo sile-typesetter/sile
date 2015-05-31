ab5a0a94422c3cc6d775868dddd7d6a942dfe4bf
Simon Cozens <simon@simon-cozens.org>
2015-05-31 23:12:05 +0900

    Diagnostics.

--- packages/autodoc.lua
+++ packages/autodoc.lua
@@ -1,5 +1,6 @@
 SILE.registerCommand("package-documentation", function (o,c)
   local package = SU.required(o, "src", "src for package documentation")
+  io.write("<"..package..">")
   SILE.process(
     SILE.inputs.TeXlike.docToTree(
       require(package).documentation
