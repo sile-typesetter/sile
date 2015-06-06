cc674ad2d2fc1c31940a7965b396ea6026776222
Simon Cozens <simon@simon-cozens.org>
2015-06-07 08:48:35 +0900

    So extensions can define their own units.

--- core/measurements.lua
+++ core/measurements.lua
@@ -1,24 +1,24 @@
-SILE.toPoints = function(num, unit, dimension)
-  local toPoints = {
-    
-    mm = function(m) return 2.8346457 * m end,
-    pt = function (i) return i end,
-    cm = function (c) return 2.8346457 * 10 * c end,
-  }
-  toPoints["in"] = function(i) return 72.0 * i end
-  toPoints["%"] = function(v, dimension) 
+SILE.xToPoints = {
+  mm = function(m) return 2.8346457 * m end,
+  pt = function (i) return i end,
+  cm = function (c) return 2.8346457 * 10 * c end,
+  ["in"] = function(i) return 72.0 * i end,
+  ["%" ]= function(v, dimension) 
     return tonumber(v) / 100 * SILE.documentState.paperSize[ dimension == 'w' and 1 or 2]
-  end
-  toPoints["ex"] =  function(v) return tonumber(v) * SILE.shaper:measureDim("x") end
-  toPoints["em"] =  function(v) return tonumber(v) * SILE.shaper:measureDim("m") end
-  toPoints["en"] =  function(v) return tonumber(v) * SILE.shaper:measureDim("n") end
+  end,
+  ["ex"] =  function(v) return tonumber(v) * SILE.shaper:measureDim("x") end,
+  ["em"] =  function(v) return tonumber(v) * SILE.shaper:measureDim("m") end,
+  ["en"] =  function(v) return tonumber(v) * SILE.shaper:measureDim("n") end
+}
+
+SILE.toPoints = function(num, unit, dimension)
   if (not unit) then
     if (type(num) == "string") then -- split into num and unit parts
       num,unit = string.match(num, "(-?[%d%.]+)%s*([%%%a]+)")
       num = tonumber(num)
     else return tonumber(num) end
   end
-  if (not toPoints[unit]) then SU.error( "Unknown unit "..unit ) end
+  if (not SILE.xToPoints[unit]) then SU.error( "Unknown unit "..unit ) end
   num =  tonumber(string.match(num, "(-?[%d%.]+)"))
-  return toPoints[unit](num, dimension);
+  return SILE.xToPoints[unit](num, dimension);
 end
