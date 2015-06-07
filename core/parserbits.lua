176e2036062cfd4fe3782cd0128c10b3d5108c59
Simon Cozens <simon@simon-cozens.org>
2015-06-07 15:40:16 +0900

    Defining new units should alter the parser.

--- core/parserbits.lua
+++ core/parserbits.lua
@@ -24,7 +24,12 @@ number.scientific =
 -- Matches all of the above
 number.number = C(number.decimal + number.scientific) / function (n) return tonumber(n) end
 local whitespace = S('\r\n\f\t ')^0
-local units = P("mm") + P("cm") + P("in") + P("pt") + P("em") + P("ex") + P("en")
+local units = lpeg.Cmt(C(R("az")^-2), function (s,i,p)
+  for k,v in pairs(SILE.xToPoints) do
+    if p == k then return true end
+  end
+  return false
+end)
 local zero = P("0") / function(...) return 0 end
 local dimensioned_string = ( C(number.number) * whitespace * C(units) ) / function (x,n,u) return  SILE.toPoints(n, u) end
 
