7f78b0011d8329cf9fc1acac3b83c3af8ccf8113
Simon Cozens <simon@simon-cozens.org>
2015-06-07 20:21:21 +0900

    Implements \smash and friends.

---
+++ packages/rebox.lua
@@ -0,0 +1,13 @@
+SILE.registerCommand("rebox", function (options, content)
+  local box = SILE.Commands["hbox"]({}, content)
+  if options.width then box.width = SILE.toPoints(options.width) end
+  if options.height then box.height = SILE.toPoints(options.height) end
+  if options.depth then box.depth = SILE.toPoints(options.depth) end
+  if options.phantom then
+    box.outputYourself = function (self, typesetter, line)
+      typesetter.frame:moveX(self:scaledWidth(line))
+    end
+  end
+  table.insert(SILE.typesetter.state.nodes, box)
+  })
+end, "Place the output within a box of specified width, height, depth and visibility")
