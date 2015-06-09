617463fcddedce8fe8dacb146cfb54695ddaf53a
Simon Cozens <simon@simon-cozens.org>
2015-06-10 08:22:13 +0900

    Allow tate frames.

--- packages/hanmenkyoshi.lua
+++ packages/hanmenkyoshi.lua
@@ -1,3 +1,4 @@
+SILE.require("packages/tate")
 SILE.registerCommand("show-hanmen", function(options, content)
   local f = SILE.typesetter.frame
   if not f.hanmen then SU.error("show-hanmen called on a frame with no hanmen") end
@@ -6,14 +7,14 @@ SILE.registerCommand("show-hanmen", function(options, content)
   SILE.outputter:pushColor({r = 1, g= 0.9, b = 0.9 })
   local g = f:top()
   while g < f:bottom() do
-    SILE.outputter.rule(f:left(), g, f:width(), 0.5)
+    SILE.outputter.rule(f:left(), g - 0.25, f:width(), 0.5)
     local l = f:left()
     while l <= f:right() do
-      SILE.outputter.rule(l-0.25, g + f.hanmen.gridsize, 0.5, f.hanmen.gridsize)
+      SILE.outputter.rule(l-0.25, g + f.hanmen.gridsize - 0.25, 0.5, f.hanmen.gridsize)
       l = l + f.hanmen.gridsize
     end
     g = g + f.hanmen.gridsize
-    SILE.outputter.rule(f:left(), g, f:width(), 0.5)
+    SILE.outputter.rule(f:left(), g - 0.25, f:width(), 0.5)
     g = g + f.hanmen.linegap
   end
   SILE.outputter:popColor()
@@ -33,7 +34,7 @@ local declareHanmenFrame = function (self, id, spec)
   SILE.settings.set("document.baselineskip", SILE.nodefactory.newVglue(skip.."pt"))
   SILE.settings.set("document.parskip", SILE.nodefactory.newVglue((spec.hanmen.linegap+0.5) .. "pt"))
 
-  self.pageTemplate.frames[id] = SILE.newFrame(spec)
+  self.pageTemplate.frames[id] = SILE.newFrame(spec, spec.tate and SILE.tateFramePrototype or SILE.framePrototype)
 end
 
 return {
