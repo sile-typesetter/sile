67d173f01a06920916dda531368a1d0ec19054d2
Simon Cozens <simon@simon-cozens.org>
2015-06-15 14:09:45 +0900

    Nearly-complete tate support (needs Harfbuzz fixes) with support for rotated LTR in TTB.

--- classes/jplain.lua
+++ classes/jplain.lua
@@ -1,11 +1,14 @@
 -- Basic! Transitional! In development! Not very good! Don't use it!
 local plain = SILE.require("classes/plain");
+SILE.call("bidi-off")
 local jplain = plain { id = "jplain", base = plain };
 if not(SILE.scratch.headers) then SILE.scratch.headers = {}; end
 jplain:loadPackage("hanmenkyoshi")
 jplain:declareHanmenFrame( "content", {
-  left = "8.3%", top = "11.6%", bottom = "90%",
+  left = "8.3%", top = "11.6%",
   gridsize = 10, linegap = 7, linelength = 50, 
+  linecount = 30,
+  tate = true
 })
 jplain.pageTemplate.firstContentFrame = jplain.pageTemplate.frames.content
 
--- packages/tate.lua
+++ packages/tate.lua
@@ -1,3 +1,5 @@
+local pdf = require("justenoughlibtexpdf")
+
 SILE.tateFramePrototype = SILE.framePrototype {
   moveX = function(self, amount)
     self.state.cursorY = self.state.cursorY + amount
@@ -16,17 +18,27 @@ SILE.tateFramePrototype = SILE.framePrototype {
     self.state.cursorX = self:right()
   end,
   enterHooks = { function (self)
+    self.oldtypesetter = SILE.typesetter
     self.state.oldBreak = SILE.settings.get("typesetter.breakwidth")
-    SILE.typesetter.oldPagetarget = SILE.typesetter.pageTarget
     SILE.settings.set("typesetter.breakwidth", SILE.length.new({length = self:height() }))
+    
     SILE.typesetter.pageTarget = function(self)
       return self.frame:width()
     end
+    SILE.typesetter.leadingFor = function(self, v)
+      v.height = SILE.toPoints("1zw")
+      return SILE.settings.get("document.parskip")
     end
+    SILE.typesetter.breakIntoLines = function (self, nl, breakWidth)
+      local breaks = SILE.linebreak:doBreak( nl, breakWidth, true)
+      return self.frame.oldtypesetter:breakpointsToLines(breaks);
+    end
+  end
   },
   leaveHooks = { function (self)
     SILE.settings.set("typesetter.breakwidth", self.state.oldBreak)
-    SILE.typesetter.pageTarget = SILE.typesetter.oldPagetarget
+    SILE.typesetter = self.oldtypesetter
+
   end
   }
 }
@@ -38,3 +50,79 @@ end
 SILE.registerCommand("tate-frame", function (options, content)
       SILE.documentState.thisPageTemplate.frames[options.id] = SILE.newTateFrame(options);
     end, "Declares (or re-declares) a frame on this page.")
+
+local swap = function (x)
+  local w = x.width
+  x.width = SILE.length.new({}) + x.height
+  x.height = type(w) == "table" and w.length or w
+end
+
+local outputLatinInTate = function (self, typesetter, line)
+  local origbox = self.value
+  -- My baseline moved
+  typesetter.frame:moveX(SILE.toPoints("-1zw"))
+  local horigin = typesetter.frame.state.cursorX
+  local vorigin = -typesetter.frame.state.cursorY
+  pdf:gsave()
+  pdf.setmatrix(1,0,0,1,horigin,vorigin)
+  pdf.setmatrix(0, -1, 1, 0, 0, 0)
+  pdf.setmatrix(1,0,0,1,-horigin,-vorigin)
+  self:oldOutputYourself(typesetter,line)
+  pdf:grestore()
+  typesetter.frame.state.cursorY = -vorigin
+  typesetter.frame:moveX(self.height)
+  -- My baseline moved
+  typesetter.frame:moveX(SILE.toPoints("1zw"))
+end
+
+-- Eventually will be automatically called by script detection, but for now
+-- called manually
+SILE.registerCommand("latin-in-tate", function (options, content)
+  local nodes
+  local oldT = SILE.typesetter
+  local prevDirection = oldT.frame.direction
+  SILE.require("packages/rotate")
+  SILE.settings.temporarily(function()
+    local latinT = SILE.defaultTypesetter {}
+    latinT.frame = oldT.frame
+    latinT:initState()
+    SILE.typesetter = latinT
+    latinT.frame.direction = "LTR"
+    SILE.settings.set("document.language", "xx")
+    SILE.process(content)
+    nodes = SILE.typesetter.state.nodes
+    for i=1,#nodes do
+      if nodes[i]:isUnshaped() then nodes[i] = nodes[i]:shape() end
+    end
+    SILE.typesetter.frame.direction = prevDirection
+  end)
+  SILE.typesetter = oldT
+  SILE.typesetter:pushGlue({
+    width = SILE.length.new({length = SILE.toPoints("0.5zw"), 
+                             stretch = SILE.toPoints("0.25zw"),
+                              shrink = SILE.toPoints("0.25zw")
+                            })
+  })
+  for i = 1,#nodes do
+    if SILE.typesetter.frame.direction ~= "TTB" then 
+      SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes+1] = nodes[i]
+    elseif nodes[i]:isGlue() then
+      nodes[i].width = nodes[i].width
+      SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes+1] = nodes[i]
+    elseif nodes[i].width > 0 then
+      SILE.call("hbox", {}, function ()
+        SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes+1] = nodes[i]
+      end)
+      local n = SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes]
+      n.oldOutputYourself = n.outputYourself
+      n.outputYourself = outputLatinInTate
+      swap(n)
+    end
+  end
+  SILE.typesetter:pushGlue({
+    width = SILE.length.new({length = SILE.toPoints("0.5zw"), 
+                             stretch = SILE.toPoints("0.25zw"),
+                              shrink = SILE.toPoints("0.25zw")
+                            })
+  })  
+end, "Declares (or re-declares) a frame on this page.")
