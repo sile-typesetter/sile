64fcc2588aceda0c4a84a5809ea6cb2cd0b07ec3
Caleb Maclennan <caleb@alerque.com>
2015-06-18 11:19:35 +0300

    check build conditions before blindly using `git submodule`
    This should allow the bootstrap script to be run on both packaged source
    distributions as well as from in a git repository. The autoreconf
    routine is not strictly necessary in the former situation but it is
    recommended procedure for building on most distros. Checking the
    conditions in the bootstrap script and handling the case of being in a
    git repository but not having the submodule initialized should allow the
    same build instructions to apply to all scenarios, simplifying the
    documentation and reducing confusion.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
 
-git submodule update --init --recursive --remote
+# We neet a local copy of the libtexpdf library to compile. If this was
+# downloaded as a src distibution package this will exist already, but if not
+# and we are part of a git repository that the user has not fully initialized,
+# go ahead and do the step of fetching the the submodule so the compile process
+# can run.
+if [[ ! -f libtexpdf/configure.ac && -d .git ]]; then
+    git submodule update --init --recursive --remote
+fi
+
 autoreconf --install
 (cd libtexpdf; autoreconf -I m4)
 sed 's/rm -f core/rm -f/' -i configure
