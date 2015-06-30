e0fa1b2e1d324848ccf7095e74fd550d69de1153
Caleb Maclennan <caleb@alerque.com>
2015-06-30 08:25:22 +0300

    cleanup trailing whitespace

--- tests/attack.pl
+++ tests/attack.pl
@@ -8,8 +8,8 @@ for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
         my $out = $_; $out =~ s/\.sil$/\.actual/;
         exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
         if (system("diff -u $expectation $out")) {
-            print("\n\n<<< EXPECTED >>>\n"); system("cat $expectation");  
-            print("\n\n<<< GOT >>>\n"); system("cat $out");  
+            print("\n\n<<< EXPECTED >>>\n"); system("cat $expectation");
+            print("\n\n<<< GOT >>>\n"); system("cat $out");
             exit 1;
         }
     } elsif (!$regression) {
