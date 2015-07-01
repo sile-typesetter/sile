98d3d0ced1f7243cddadc64550ddbc2a345cfcfb
Caleb Maclennan <caleb@alerque.com>
2015-07-01 17:57:58 +0300

    run all regression tests even if some fail
    The Travis environment is really hard (and slow) to debug, so it's more
    useful to get data about all the regression tests at once rather than
    waiting to find out others failed after fixing one. This will
    
    * Still fail immediately for any failure in the normal compile checks
    * Keep processing regression checks after a failure, but output the
      right exit code in the end.
    * Not be quite as verbose. The actual/expected data is in the file
      system anyway and the diff is far more useful for debuging from Travis
      logs than the full dumps which are hard to even scroll past.

--- tests/attack.pl
+++ tests/attack.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 my $regression = ($ARGV[0] && $ARGV[0] =~ /--regression/i);
+my $exit = 0;
 for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
     next if /macros.sil/;
     my $expectation = $_; $expectation =~ s/\.sil$/\.expected/;
@@ -12,16 +13,15 @@ for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
         if ($_ =~ /_darwin\.sil/ && $^O !~ 'darwin') {
             next;
         }
-        print "### Compiling $_\n";
+        print "### Regression testing $_\n";
         my $out = $_; $out =~ s/\.sil$/\.actual/;
         exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
-        if (system("diff -u $expectation $out")) {
-            print("\n\n<<< EXPECTED >>>\n"); system("cat $expectation");
-            print("\n\n<<< GOT >>>\n"); system("cat $out");
-            exit 1;
+        if (system("diff -U0 $expectation $out")) {
+			$exit = 1;
         }
     } elsif (!$regression) {
         print "### Compiling $_\n";
         exit $? >> 8 if system("./sile", $_);
     }
 }
+exit $exit;
