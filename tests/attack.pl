b1865338af107b8cde7fb7dc245e3efc3f63a949
Caleb Maclennan <caleb@alerque.com>
2015-07-01 20:58:15 +0300

    separate compile tests from regression tests
    The logic of checking whether all the stuff compiles vs whether it
    exactly matches the reference implementation was getting too convoluted.
    This separates the concerns so that only documentation and examples get
    compile checks while the tests get compile and regression checks
    (depending of the mode of the flag at run time). Travis for example runs
    both modes, but separately so what failed is more apparent than if they
    were lumped in together.

--- tests/attack.pl
+++ tests/attack.pl
@@ -4,24 +4,26 @@ use strict;
 use warnings;
 
 my $regression = ($ARGV[0] && $ARGV[0] =~ /--regression/i);
-my $exit = 0;
-for (<tests/*.sil>, <examples/*.sil>, "documentation/sile.sil") {
-    next if /macros.sil/;
-    my $expectation = $_; $expectation =~ s/\.sil$/\.expected/;
-    if (-f $expectation and $regression) {
-        # Only test OSX specific regressions if running OSX
-        if ($_ =~ /_darwin\.sil/ && $^O !~ 'darwin') {
-            next;
-        }
-        print "### Regression testing $_\n";
-        my $out = $_; $out =~ s/\.sil$/\.actual/;
-        exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
-        if (system("diff -U0 $expectation $out")) {
-			$exit = 1;
-        }
-    } elsif (!$regression) {
-        print "### Compiling $_\n";
-        exit $? >> 8 if system("./sile", $_);
-    }
+if ($regression) {
+	my $exit = 0;
+	for (<tests/*.sil>) {
+		my $expectation = $_; $expectation =~ s/\.sil$/\.expected/;
+		if (-f $expectation) {
+			# Only test OS specific regressions on their respective OSes
+			next if ($_ =~ /_\w+\.sil/ && $_ !~ /_$^O\.sil/);
+			print "### Regression testing $_\n";
+			my $out = $_; $out =~ s/\.sil$/\.actual/;
+			exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
+			if (system("diff -U0 $expectation $out")) {
+				$exit = 1;
+			}
+		}
+	}
+	exit $exit;
+} else {
+	for (<examples/*.sil>, "documentation/sile.sil") {
+		next if /macros.sil/;
+		print "### Compiling $_\n";
+		exit $? >> 8 if system("./sile", $_);
+	}
 }
-exit $exit;
