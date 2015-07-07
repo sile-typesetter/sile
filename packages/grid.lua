fcb30d30fdd2fa104ad6094d91789165fb33b3d5
Simon Cozens <simon@simon-cozens.org>
2015-07-07 18:59:37 +0900

    Major fixes to grid mode. New page still not quite right but everything else looks good.

--- packages/grid.lua
+++ packages/grid.lua
@@ -1,45 +1,42 @@
 local gridSpacing -- Should be a setting
 
 local makeUp = function ()
-  local toadd = gridSpacing - (SILE.typesetter.frame.state.totals.gridCursor % gridSpacing)
+  if not SILE.typesetter.frame.state.totals.gridCursor then SILE.typesetter.frame.state.totals.gridCursor = 0 end
+  local toadd = gridSpacing - (SILE.typesetter.frame.state.totals.gridCursor % gridSpacing) 
   SILE.typesetter.frame.state.totals.gridCursor = SILE.typesetter.frame.state.totals.gridCursor + toadd
   return SILE.nodefactory.newVglue({ height = SILE.length.new({ length = toadd }) })
 end
 
 local leadingFor = function(this, vbox, previous)
+print("Leading ", previous, vbox)
   if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = 0 end
-  if type(previous.height) == "table" then
-    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + previous.height.length + previous.depth
+  if type(vbox.height) == "table" then
+    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + vbox.height.length + previous.depth
   else
-    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + previous.height + previous.depth
+    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + vbox.height + previous.depth
   end
-  return makeUp()
+  g = makeUp()
+  print(g)
+  return g
 end
 
 local pushVglue = function(this, spec)
-  if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = 0 end
-  this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + spec.height.length
-  SILE.defaultTypesetter.pushVglue(this, spec);
-  SILE.defaultTypesetter.pushVglue(this, makeUp())
-end
-
-local newBoxup = function (this)
-  local b = SILE.defaultTypesetter.boxUpNodes(this)
-  if not this.frame.state.totals.gridCursor then this.frame.state.totals.gridCursor = 0 end
-
-  if #b > 1 then
-    local h = type(b[#b].height) == "table" and b[#b].height.length or b[#b].height
-    this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + h + b[#b].depth
+  if not this.frame.state.totals.gridCursor then 
+    print("Setting")
+    this.frame.state.totals.gridCursor = 0 
   end
-  return b
+  this.frame.state.totals.gridCursor = this.frame.state.totals.gridCursor + spec.height.length
+  print("Adding")
+  SILE.defaultTypesetter.pushVglue(this, spec)
+  SILE.defaultTypesetter.pushVglue(this, makeUp())
 end
 
 local debugGrid = function()
   local t = SILE.typesetter
   if not t.frame.state.totals.gridCursor then t.frame.state.totals.gridCursor = 0 end
-  local g = t.frame:top() + t.frame.state.totals.gridCursor + SILE.toPoints("1em")
+  local g = t.frame.state.totals.gridCursor
   while g < t.frame:bottom() do
-    SILE.outputter.rule(t.frame:left(), g, t.frame:width(), 0.1)
+    SILE.outputter.rule(t.frame:left(), t.frame:top() + g, t.frame:width(), 0.1)
     g = g + gridSpacing
   end
 end
@@ -53,11 +50,14 @@ SILE.registerCommand("grid", function(options, content)
 
   SILE.typesetter.leadingFor = leadingFor
   SILE.typesetter.pushVglue = pushVglue
-  SILE.typesetter.boxUpNodes = newBoxup
   SILE.typesetter.setVerticalGlue = function () end
-  SILE.typesetter.frame.state.totals.gridCursor = 0 -- Start the grid on the first baseline
-  -- add some now
-  -- SILE.defaultTypesetter.pushVglue(SILE.typesetter, makeUp())
+  SILE.typesetter.frame.state.totals.gridCursor = 0
+  SILE.typesetter.state.previousVbox = SILE.defaultTypesetter.pushVbox(SILE.typesetter,{})
+  SILE.typesetter:registerNewPageHook(function(this)
+    SILE.typesetter.frame.state.totals.gridCursor = 0
+    SILE.typesetter.state.previousVbox = SILE.defaultTypesetter.pushVbox(SILE.typesetter,{})
+  end)
+
 end, "Begins typesetting on a grid spaced at <spacing> intervals.")
 
 SILE.registerCommand("no-grid", function (options, content)
