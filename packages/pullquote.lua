7b9a62beccf5182925887e14ae9ddb1940986609
Caleb Maclennan <caleb@alerque.com>
2015-07-10 10:03:28 +0300

    normalize whitespace in Lua on 2 space indent

--- packages/pullquote.lua
+++ packages/pullquote.lua
@@ -2,62 +2,62 @@ SILE.require("packages/color")
 SILE.registerCommand("pullquote:font", function(options, content)
 end, "The font chosen for the pullquote environment")
 SILE.registerCommand("pullquote:author-font", function(options, content)
-	SILE.settings.set("font.style", "italic")
+  SILE.settings.set("font.style", "italic")
 end, "The font style with which to typeset the author attribution.")
 SILE.registerCommand("pullquote:mark-font", function(options, content)
-	SILE.settings.set("font.family", "Libertine Sans")
+  SILE.settings.set("font.family", "Libertine Sans")
 end, "The font from which to pull the quotation marks.")
 
 local typesetMark = function (open, setback, scale, color, mark)
-	SILE.typesetter:chuck()
-	local saveX = SILE.typesetter.frame.state.cursorX;
-	local saveY = SILE.typesetter.frame.state.cursorY;
-	SILE.settings.temporarily(function()
-		SILE.call("pullquote:mark-font")
-		if open then
-			SILE.typesetter.frame:moveX(- SILE.toPoints(setback))
-			SILE.typesetter.frame:moveY(SILE.toPoints("1ex"))
-		else
-			SILE.typesetter.frame:moveX(SILE.toPoints(setback))
-			SILE.typesetter.frame:moveY(- SILE.toPoints(scale -1 .. "ex"))
-		end
-		SILE.settings.set("font.size", SILE.settings.get("font.size") * scale)
-		SILE.call("color", {color = color}, function ()
-			SILE.call(open and "raggedright" or "raggedleft", {}, function ()
-				SILE.typesetter:typeset(mark)
-			end)
-		end)
-	end)
-	SILE.typesetter:chuck()
-	SILE.typesetter.frame.state.cursorX = saveX
-	SILE.typesetter.frame.state.cursorY = saveY
+  SILE.typesetter:chuck()
+  local saveX = SILE.typesetter.frame.state.cursorX;
+  local saveY = SILE.typesetter.frame.state.cursorY;
+  SILE.settings.temporarily(function()
+    SILE.call("pullquote:mark-font")
+    if open then
+      SILE.typesetter.frame:moveX(- SILE.toPoints(setback))
+      SILE.typesetter.frame:moveY(SILE.toPoints("1ex"))
+    else
+      SILE.typesetter.frame:moveX(SILE.toPoints(setback))
+      SILE.typesetter.frame:moveY(- SILE.toPoints(scale -1 .. "ex"))
+    end
+    SILE.settings.set("font.size", SILE.settings.get("font.size") * scale)
+    SILE.call("color", {color = color}, function ()
+      SILE.call(open and "raggedright" or "raggedleft", {}, function ()
+        SILE.typesetter:typeset(mark)
+      end)
+    end)
+  end)
+  SILE.typesetter:chuck()
+  SILE.typesetter.frame.state.cursorX = saveX
+  SILE.typesetter.frame.state.cursorY = saveY
 end
 
 SILE.registerCommand("pullquote", function(options, content)
-	local author = options.author or nil
-	local setback = options.setback or "2em"
-	local scale = options.scale or 3
-	local color = options.color or "#999999"
-	SILE.settings.temporarily(function()
-		SILE.settings.set("document.rskip", SILE.nodefactory.newGlue(setback))
-		SILE.settings.set("document.lskip", SILE.nodefactory.newGlue(setback))
-		SILE.settings.set("current.parindent", SILE.nodefactory.zeroGlue)
-		SILE.call("pullquote:font")
-		typesetMark(true, setback, scale, color, "“")
-		SILE.process(content)
-		typesetMark(false, setback, scale, color, "”")
-		if author then
-			SILE.settings.temporarily(function()
-				SILE.typesetter:leaveHmode()
-				SILE.call("pullquote:author-font")
-				SILE.call("raggedleft", {}, function ()
-					SILE.typesetter:typeset("— " .. author)
-				end)
-			end)
-		else
-			SILE.call("par")
-		end
-	end)
+  local author = options.author or nil
+  local setback = options.setback or "2em"
+  local scale = options.scale or 3
+  local color = options.color or "#999999"
+  SILE.settings.temporarily(function()
+    SILE.settings.set("document.rskip", SILE.nodefactory.newGlue(setback))
+    SILE.settings.set("document.lskip", SILE.nodefactory.newGlue(setback))
+    SILE.settings.set("current.parindent", SILE.nodefactory.zeroGlue)
+    SILE.call("pullquote:font")
+    typesetMark(true, setback, scale, color, "“")
+    SILE.process(content)
+    typesetMark(false, setback, scale, color, "”")
+    if author then
+      SILE.settings.temporarily(function()
+        SILE.typesetter:leaveHmode()
+        SILE.call("pullquote:author-font")
+        SILE.call("raggedleft", {}, function ()
+          SILE.typesetter:typeset("— " .. author)
+        end)
+      end)
+    else
+      SILE.call("par")
+    end
+  end)
 end, "Typesets its contents in a formatted blockquote.")
 
 return [[\begin{document}
