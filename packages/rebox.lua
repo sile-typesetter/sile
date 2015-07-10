40612d91555f0a88cdc41e1f7fde8cb37a214827
Simon Cozens <simon@simon-cozens.org>
2015-07-11 00:58:17 +0900

    Some fixes.

--- packages/rebox.lua
+++ packages/rebox.lua
@@ -1,6 +1,6 @@
 SILE.registerCommand("rebox", function (options, content)
   local box = SILE.Commands["hbox"]({}, content)
-  if options.width then box.width = SILE.toPoints(options.width) end
+  if options.width then box.width = SILE.length.new({length = SILE.toPoints(options.width)}) end
   if options.height then box.height = SILE.toPoints(options.height) end
   if options.depth then box.depth = SILE.toPoints(options.depth) end
   if options.phantom then
@@ -9,5 +9,4 @@ SILE.registerCommand("rebox", function (options, content)
     end
   end
   table.insert(SILE.typesetter.state.nodes, box)
-  })
 end, "Place the output within a box of specified width, height, depth and visibility")
