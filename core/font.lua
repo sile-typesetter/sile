9d48b4133286c8987cd6da684245e15e42b18cf9
Simon Cozens <simon@simon-cozens.org>
2015-07-14 22:14:26 +0900

    Allow setting the text direction again, for cases when text direction != frame direction. See #83.

--- core/font.lua
+++ core/font.lua
@@ -13,6 +13,7 @@
       if (options.style)  then SILE.settings.set("font.style", options.style) end
       if (options.variant)  then SILE.settings.set("font.variant", options.variant) end
       if (options.features)  then SILE.settings.set("font.features", options.features) end
+      if (options.direction)  then SILE.settings.set("font.direction", options.direction) end
 
       if (options.language)  then 
         SILE.settings.set("document.language", options.language)
