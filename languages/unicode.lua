8d7617cad70938985fefefd100c729327d5092bf
Simon Cozens <simon@simon-cozens.org>
2015-08-14 21:15:41 +0100

    Harmonize ICU/non-ICU tokenising a little. See #159.

--- languages/unicode.lua
+++ languages/unicode.lua
@@ -1,7 +1,7 @@
 require("char-def")
 local chardata  = characters.data
 
-pcall( function () icu = require("justenoughicu") end)
+-- pcall( function () icu = require("justenoughicu") end)
 if icu then
   SILE.tokenizers.unicode = function(text)
     local chunks = {icu.breakpoints(text)}
@@ -25,6 +25,7 @@ else
     local chunks = SU.splitUtf8(text)
     return coroutine.wrap(function()
       local tmp = {}
+      local lasttype
       for i = 1,#chunks do
         local cp = SU.codepoint(chunks[i])
         if chardata[cp] and chardata[cp].linebreak == "sp" then
@@ -36,9 +37,14 @@ else
           coroutine.yield({ string = table.concat(tmp, "") })
           tmp = {}
           coroutine.yield({ node = SILE.nodefactory.newPenalty({ penalty = 0 }) })
+        elseif lasttype and chardata[cp].linebreak ~= lasttype then
+          coroutine.yield({ string = table.concat(tmp, "") })
+          tmp = {}
+          tmp[#tmp+1] = chunks[i]
         else
           tmp[#tmp+1] = chunks[i]
         end
+        lasttype = chardata[cp].linebreak
       end
       coroutine.yield({ string = table.concat(tmp, "") })
     end)
