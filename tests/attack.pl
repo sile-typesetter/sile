3b60c217291d9d6b3426b64bc4028cdcdc7cec06
Simon Cozens <simon@simon-cozens.org>
2015-08-15 12:50:08 +0100

    Some tests are meant to be broken.

--- tests/attack.pl
+++ tests/attack.pl
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use Term::ANSIColor;
-my (@failed, @passed);
+my (@failed, @passed, @knownbad);
 my $regression = 0;
 my $upstream = 0;
 
@@ -19,6 +19,7 @@ if ($regression) {
 	my $exit = 0;
 	for (@specifics ? @specifics : <tests/*.sil>) {
 		my $expectation = $_; $expectation =~ s/\.sil$/\.expected/;
+		my $knownbad;
 		if (-f $expectation) {
 			# Only run regression tests for upstream bugs if specifically asked
 			if ($_ =~ /_upstream\.sil/) {
@@ -29,13 +30,19 @@ if ($regression) {
 			}
 			print "### Regression testing $_\n";
 			my $out = $_; $out =~ s/\.sil$/\.actual/;
+			if (!system("grep KNOWNBAD $_ >/dev/null")) {
+				$knownbad = 1;
+			}
 			exit $? >> 8 if system qq{./sile -e 'require("core/debug-output")' $_ > $out};
 			if (system("diff -U0 $expectation $out")) {
-				push @failed, $_;
-			} else { push @passed, $_ }
+				push ($knownbad ? \@knownbad : \@failed, $_);
+			} else { push $knownbad ? \@failed: \@passed, $_ }
 		}
 	}
 	print "\n",color("green"), "Passing tests:\n • ",join(", ", @passed),"\n";
+	if (@knownbad){
+		print "\n",color("yellow"), "Known bad tests:\n • ",join(", ", @knownbad),"\n";
+	}
 	if (@failed) {
 		print "\n",color("red"), "Failed tests: \n";
 		for (@failed) { print " • ",$_,"\n"}
