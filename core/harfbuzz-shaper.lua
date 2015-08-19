sile-typesetter/sile

commit 1f6a2fdd716aa2a350e91bee287d6c8fe54bb894

Support kerning by allowing libtexpdf to know the glyph width, and hence pass the difference between glyph width and advance to the TJ operator. Fixes #170.

core/harfbuzz-shaper.lua
@@ -30,17 +30,15 @@ SILE.shapers.harfbuzz = SILE.shapers.base {
   end,
   preAddNodes = function(self, items, nnodeValue) -- Check for complex nodes
     for i=1,#items do
-      if items[i].y_offset then
+      if items[i].y_offset or items[i].width ~= items[i].glyphWidth then
         nnodeValue.complex = true; break
       end
     end
   end,
   addShapedGlyphToNnodeValue = function (self, nnodevalue, shapedglyph)
     if nnodevalue.complex then
-
       if not nnodevalue.items then nnodevalue.items = {} end
       nnodevalue.items[#nnodevalue.items+1] = shapedglyph
-      return
     end
     if not nnodevalue.glyphString then nnodevalue.glyphString = {} end
     if not nnodevalue.glyphNames then nnodevalue.glyphNames = {} end

core/libtexpdf-output.lua
@@ -34,7 +34,7 @@ SILE.outputters.libtexpdf = {
       for i=1,#(value.items) do
         local glyph = value.items[i].codepoint
         local buf = string.char(math.floor(glyph % 2^32 / 2^8)) .. string.char(glyph % 0x100)
-        pdf.setstring(cursorX + (value.items[i].x_offset or 0), cursorY + (value.items[i].y_offset or 0), buf, string.len(buf), font, value.items[i].width)
+        pdf.setstring(cursorX + (value.items[i].x_offset or 0), cursorY + (value.items[i].y_offset or 0), buf, string.len(buf), font, value.items[i].glyphWidth)
         cursorX = cursorX + value.items[i].width
       end
       return

src/justenoughharfbuzz.c
@@ -348,11 +348,16 @@ int shape (lua_State *L) {
       double height = extents.y_bearing * point_size / upem;
       double tHeight = extents.height * point_size / upem;
       double width = glyph_pos[j].x_advance * point_size / upem;
+      double glyphWidth = extents.width * point_size / upem;
       if (direction == HB_DIRECTION_TTB) { /* XXX */
         height = -glyph_pos[j].y_advance * point_size / upem;
         tHeight = 0;
-        width = extents.width * point_size / upem;
+        width = glyphWidth;
       }
+      lua_pushstring(L, "glyphWidth");
+      lua_pushnumber(L, glyphWidth);
+      lua_settable(L, -3);
+
       lua_pushstring(L, "width");
       lua_pushnumber(L, width);
       lua_settable(L, -3);

src/justenoughlibtexpdf.c
@@ -124,7 +124,7 @@ int pdf_setstring(lua_State *L) {
   int    chrlen  = luaL_checkinteger(L, 4);
   int    font_id = luaL_checkinteger(L, 5);
   double w = luaL_checknumber(L,6);
-  texpdf_dev_set_string(p, precision * x, precision * (-height+y), s, chrlen, w * 65536 * precision, font_id, -1);
+  texpdf_dev_set_string(p, precision * x, precision * (-height+y), s, chrlen, w * precision, font_id, -1);
   return 0;
 }
 

commit 285dd8bd944fd42660d49f4763e650f4a6e7469d

Partially revert 1f6a2fd, which broke rendering of Arabic. See #170 and #171.

core/harfbuzz-shaper.lua
@@ -32,15 +32,17 @@ SILE.shapers.harfbuzz = SILE.shapers.base {
   end,
   preAddNodes = function(self, items, nnodeValue) -- Check for complex nodes
     for i=1,#items do
-      if items[i].y_offset or items[i].width ~= items[i].glyphWidth then
+      if items[i].y_offset then
         nnodeValue.complex = true; break
       end
     end
   end,
   addShapedGlyphToNnodeValue = function (self, nnodevalue, shapedglyph)
     if nnodevalue.complex then
+
       if not nnodevalue.items then nnodevalue.items = {} end
       nnodevalue.items[#nnodevalue.items+1] = shapedglyph
+      return
     end
     if not nnodevalue.glyphString then nnodevalue.glyphString = {} end
     if not nnodevalue.glyphNames then nnodevalue.glyphNames = {} end

core/libtexpdf-output.lua
@@ -34,7 +34,7 @@ SILE.outputters.libtexpdf = {
       for i=1,#(value.items) do
         local glyph = value.items[i].codepoint
         local buf = string.char(math.floor(glyph % 2^32 / 2^8)) .. string.char(glyph % 0x100)
-        pdf.setstring(cursorX + (value.items[i].x_offset or 0), cursorY + (value.items[i].y_offset or 0), buf, string.len(buf), font, value.items[i].glyphWidth)
+        pdf.setstring(cursorX + (value.items[i].x_offset or 0), cursorY + (value.items[i].y_offset or 0), buf, string.len(buf), font, value.items[i].width)
         cursorX = cursorX + value.items[i].width
       end
       return