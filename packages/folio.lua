2c474e8fd613195be85d5437259287b248b7c288
Khaled Hosny <khaledhosny@eglug.org>
2015-09-16 09:36:03 +0200

    Rearrange this code to avoid duplication
    A minor change but the indentation messes the diff, best viewed with
    --ignore-space-change.

--- packages/folio.lua
+++ packages/folio.lua
@@ -11,26 +11,24 @@ return {
   exports = {
     outputFolio = function (this, frame)
       if not frame then frame = "folio" end
+      io.write("["..SILE.formatCounter(SILE.scratch.counters.folio).."] ");
       if SILE.scratch.counters.folio.off then
         if SILE.scratch.counters.folio.off == 2 then
           SILE.scratch.counters.folio.off = false
         end
-        io.write("["..SILE.formatCounter(SILE.scratch.counters.folio).."] ");
-        SILE.scratch.counters.folio.value = SILE.scratch.counters.folio.value + 1
-        return
-      end
-      io.write("["..SILE.formatCounter(SILE.scratch.counters.folio).."] ");
-      local f = SILE.getFrame("folio");
-      if (f) then
-        SILE.typesetNaturally(f, function()
-          SILE.settings.pushState()
-          SILE.settings.reset()
-          SILE.call("center", {}, function()
-            SILE.typesetter:typeset(SILE.formatCounter(SILE.scratch.counters.folio))
+      else
+        local f = SILE.getFrame("folio");
+        if (f) then
+          SILE.typesetNaturally(f, function()
+            SILE.settings.pushState()
+            SILE.settings.reset()
+            SILE.call("center", {}, function()
+              SILE.typesetter:typeset(SILE.formatCounter(SILE.scratch.counters.folio))
+            end)
+            SILE.typesetter:leaveHmode()
+            SILE.settings.popState()
           end)
-          SILE.typesetter:leaveHmode()
-          SILE.settings.popState()
-        end)
+        end
       end
       SILE.scratch.counters.folio.value = SILE.scratch.counters.folio.value + 1
     end
