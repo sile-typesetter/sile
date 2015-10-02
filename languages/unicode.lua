3743cb6d612be86662e16f20e11afcb3c234af55
Simon Cozens <simon@simon-cozens.org>
2015-10-02 14:06:13 +0900

    More complete, and tidier, version of the Unicode node maker.

--- languages/unicode.lua
+++ languages/unicode.lua
@@ -4,20 +4,39 @@ local chardata  = characters.data
 SILE.nodeMakers.unicode = function(items,text,options)
   local contents = {}
   local token = ""
+
+  local shipToken = function ()
+    if #contents>0 then
+      coroutine.yield(SILE.shaper:formNnode(contents, token, options))
+    end
+    contents = {} ; token = ""
+  end
+  local addToken = function (char,item)
+    token = token .. char
+    contents[#contents+1] = item
+  end
+
   return coroutine.wrap(function()
     for i = 1,#items do item = items[i]
       local char = items[i].text
       local cp = SU.codepoint(char)
-      if chardata[cp] and chardata[cp].linebreak == "sp" then
-        if #contents>0 then coroutine.yield(SILE.shaper:formNnode(contents, token, options)) end
-        contents = {} ; token = ""
+      local thistype = chardata[cp] and chardata[cp].linebreak
+      if chardata[cp] and thistype == "sp" then
+        shipToken()
         coroutine.yield(SILE.shaper:makeSpaceNode(options))
-      else -- XXX
-        contents[#contents+1] = item
-        token = token .. char
+      elseif chardata[cp] and (thistype == "ba" or  thistype == "zw") then
+        addToken(char,item)
+        shipToken()
+        coroutine.yield( SILE.nodefactory.newPenalty({ penalty = 0 }) )
+      elseif lasttype and (thistype ~= lasttype and thistype ~= "cm") then
+        shipToken()
+        addToken(char,item)
+      else
+        addToken(char,item)
       end
+      if thistype ~= "cm" then lasttype = chardata[cp] and chardata[cp].linebreak end
     end
-    if #contents>0 then coroutine.yield(SILE.shaper:formNnode(contents,token,options)) end
+    shipToken()
   end)
 end
 
@@ -41,33 +60,4 @@ if icu then
     end)
   end
 else
-  SILE.tokenizers.unicode = function(text)
-    local chunks = SU.splitUtf8(text)
-    return coroutine.wrap(function()
-      local tmp = {}
-      local lasttype
-      for i = 1,#chunks do
-        local cp = SU.codepoint(chunks[i])
-        local thistype = chardata[cp] and chardata[cp].linebreak
-        if chardata[cp] and thistype == "sp" then
-          if #tmp>0 then coroutine.yield({ string = table.concat(tmp, "") }) end
-          tmp = {}
-          coroutine.yield({ separator = chunks[i]})
-        elseif chardata[cp] and (thistype == "ba" or  thistype == "zw") then
-          tmp[#tmp+1] = chunks[i]
-          coroutine.yield({ string = table.concat(tmp, "") })
-          tmp = {}
-          coroutine.yield({ node = SILE.nodefactory.newPenalty({ penalty = 0 }) })
-        elseif lasttype and (thistype ~= lasttype and thistype ~= "cm") then
-          if #tmp>0 then coroutine.yield({ string = table.concat(tmp, "") }) end
-          tmp = {}
-          tmp[#tmp+1] = chunks[i]
-        else
-          tmp[#tmp+1] = chunks[i]
-        end
-        if thistype ~= "cm" then lasttype = chardata[cp] and chardata[cp].linebreak end
-      end
-      coroutine.yield({ string = table.concat(tmp, "") })
-    end)
-  end
 end
