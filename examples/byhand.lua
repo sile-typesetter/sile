55b2cfdfa63b00b1ad86dbe0af1e3d1fab96de38
Simon Cozens <simon@simon-cozens.org>
2015-10-09 10:08:29 +0900

    Things change.

--- examples/byhand.lua
+++ examples/byhand.lua
@@ -1,4 +1,8 @@
+package.path = (os.getenv("SILE_PATH") and
+os.getenv("SILE_PATH").."/?.lua" or "") .. ';?.lua;/usr/local/share/sile/?.lua;/usr/local/share/sile/lua-libraries/?.lua;/usr/local/share/sile/lua-libraries/?/init.lua;lua-libraries/?.lua;lua-libraries/?/init.lua;' .. package.path
+package.cpath = package.cpath .. ";core/?.so;/usr/local/lib/sile/?.so;"
 require("core/sile")
+SILE.init()
 SILE.outputFilename="byhand.pdf"
 local plain = require("classes/plain")
 plain.options.papersize("a4")
