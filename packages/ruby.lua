fdd2c13b91bd1df68ee2f5150de4e0fdcd402ebc
Simon Cozens <simon@simon-cozens.org>
2015-11-06 15:00:39 +0900

    Ruby fixes.

--- packages/ruby.lua
+++ packages/ruby.lua
@@ -3,7 +3,7 @@ SILE.registerCommand("ruby", function (o,c)
   SILE.call("hbox", {}, function()
     SILE.settings.temporarily(function ()
       SILE.call("noindent")
-      SILE.call("font", {size = "0.5zw"})
+      SILE.call("font", {size = "0.6zw", weight = 800 })
       SILE.typesetter:typeset(reading)
     end)
   end)
@@ -11,23 +11,28 @@ SILE.registerCommand("ruby", function (o,c)
   rubybox.outputYourself = function (self, typesetter, line)
     local ox = typesetter.frame.state.cursorX
     typesetter.frame.state.cursorX = typesetter.frame.state.cursorX + rubybox.width.length / 2
-    typesetter.frame:advancePageDirection(-SILE.toPoints("1zw"))
+    typesetter.frame:advancePageDirection(-SILE.toPoints("0.8zw"))
     SILE.outputter.moveTo(typesetter.frame.state.cursorX, typesetter.frame.state.cursorY)
     for i = 1, #(self.value) do local node = self.value[i]
       node:outputYourself(typesetter, line)
     end
     typesetter.frame.state.cursorX = ox
-    typesetter.frame:advancePageDirection(SILE.toPoints("1zw"))
+    typesetter.frame:advancePageDirection(SILE.toPoints("0.8zw"))
   end
 
   -- measure the content
   SILE.call("hbox", {}, c)
   cbox = SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes]
-  if cbox.width > rubybox.width then
-    rubybox.width = cbox.width - rubybox.width
+  local measure
+  if SILE.typesetter.frame:writingDirection() == "LTR" then measure = "width"
+  else measure = "height" end
+  if cbox[measure] > rubybox[measure] then
+    rubybox[measure] = cbox[measure] - rubybox[measure]
   else
-    local to_insert = SILE.length.new({length = (rubybox.width - cbox.width).length / 2, })
-    cbox.width = rubybox.width
+    local diff = rubybox[measure] - cbox[measure]
+    if type(diff) == "table" then diff = diff.length end
+    local to_insert = SILE.length.new({length = diff / 2, })
+    cbox[measure] = rubybox[measure]
     rubybox.width = SILE.length.zero
     -- add spaces at beginning and end
     table.insert(cbox.value, 1, SILE.nodefactory.newGlue({ width = to_insert }))
