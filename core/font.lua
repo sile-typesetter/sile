431dcaab554a31aaf3c33bdc0dd56c452b683deb
Simon Cozens <simon@simon-cozens.org>
2015-11-20 14:34:30 +0900

    Placate Lua 5.3?

--- core/font.lua
+++ core/font.lua
@@ -52,7 +52,7 @@ SILE.settings.declare({name = "document.language", type = "string", default = "e
 SILE.fontCache = {}
 
 local _key = function(options)
-  return table.concat({options.font;options.size;("%d"):format(options.weight);options.style;options.variant;options.features;options.direction;options.filename},";")
+  return table.concat({options.font;("%g"):format(options.size);("%d"):format(options.weight);options.style;options.variant;options.features;options.direction;options.filename},";")
 end
 
 
