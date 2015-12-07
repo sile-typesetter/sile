47fea172a5e5d217fbf3b4627b9bc605e90271df
Simon Cozens <simon@simon-cozens.org>
2015-12-07 12:12:22 +0900

    This is tidier but there is still a bug (#245) measuring the width of hboxes in TTB mode.

--- packages/ruby.lua
+++ packages/ruby.lua
@@ -10,30 +10,32 @@ SILE.registerCommand("ruby", function (o,c)
   local rubybox = SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes]
   rubybox.outputYourself = function (self, typesetter, line)
     local ox = typesetter.frame.state.cursorX
-    typesetter.frame.state.cursorX = typesetter.frame.state.cursorX + rubybox.width.length / 2
-    typesetter.frame:advancePageDirection(-SILE.toPoints("0.8zw"))
+    local oy = typesetter.frame.state.cursorY
+    typesetter.frame.state.cursorX = typesetter.frame.state.cursorX + rubybox.width
+    typesetter.frame:advancePageDirection(-SILE.toPoints("1zw"))
     SILE.outputter.moveTo(typesetter.frame.state.cursorX, typesetter.frame.state.cursorY)
     for i = 1, #(self.value) do local node = self.value[i]
       node:outputYourself(typesetter, line)
     end
     typesetter.frame.state.cursorX = ox
-    typesetter.frame:advancePageDirection(SILE.toPoints("0.8zw"))
+    typesetter.frame.state.cursorY = oy
   end
-
   -- measure the content
   SILE.call("hbox", {}, c)
   cbox = SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes]
-  local measure
-  if SILE.typesetter.frame:writingDirection() == "LTR" then measure = "width"
-  else measure = "height" end
-  if cbox[measure] > rubybox[measure] then
-    rubybox[measure] = cbox[measure] - rubybox[measure]
+  SU.debug("ruby", "base box is "..cbox)
+  SU.debug("ruby", "reading is  "..rubybox)
+  if cbox:lineContribution() > rubybox:lineContribution() then
+    SU.debug("ruby", "Base is longer, offsetting ruby to fit")
+    -- This is actually the offset against the base
+    rubybox.width = SILE.length.make(cbox:lineContribution() - rubybox:lineContribution()).length
   else
-    local diff = rubybox[measure] - cbox[measure]
+    local diff = rubybox:lineContribution() - cbox:lineContribution()
     if type(diff) == "table" then diff = diff.length end
     local to_insert = SILE.length.new({length = diff / 2, })
-    cbox[measure] = rubybox[measure]
-    rubybox.width = SILE.length.zero
+    SU.debug("ruby", "Ruby is longer, inserting " ..to_insert.." either side of base")
+    cbox.width = SILE.length.make(rubybox:lineContribution())
+    rubybox.width = 0
     -- add spaces at beginning and end
     table.insert(cbox.value, 1, SILE.nodefactory.newGlue({ width = to_insert }))
     table.insert(cbox.value, SILE.nodefactory.newGlue({ width = to_insert }))
