26355e1d011afc9f36fb5566b525191848566afe
Simon Cozens <simon@simon-cozens.org>
2015-12-16 05:54:24 +0900

    Now we can do a bit more with insertions.

---
+++ tests/classes/testsidenote.lua
@@ -0,0 +1,25 @@
+local plain = SILE.require("classes/plain");
+local testsidenote = plain { id = "testsidenote" };
+
+local gutterWidth = "3%"
+testsidenote:declareFrame("contentA", {left = "left(content)", right = "left(gutter)", top = "top(content)", bottom = "bottom(content)" })
+testsidenote:declareFrame("sidenotes", {left = "right(gutter)", width="width(contentA) * 2 / 3", right = "right(content)", top = "top(content)", bottom = "bottom(content)", balanced = true })
+testsidenote:declareFrame("gutter", {left = "right(contentA)", right = "left(sidenotes)", width = gutterWidth })
+
+testsidenote.pageTemplate.firstContentFrame = testsidenote.pageTemplate.frames["contentA"];
+
+local insertions = SILE.require("packages/insertions")
+SILE.require("packages/footnotes")
+
+testsidenote.init = function(self)
+  insertions.exports:initInsertionClass("footnote", {
+    maxHeight = SILE.length.new({length = SILE.toPoints("75", "%","h") }),
+    topBox = SILE.nodefactory.zeroVglue,
+    interInsertionSkip = SILE.length.parse("1ex"),
+    insertInto = { frame = "sidenotes", ratio = 0 },
+    stealFrom = {  },
+  })
+  return plain.init(self)
+end
+
+return testsidenote
