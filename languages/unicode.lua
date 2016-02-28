ca72fb6d74ee21327f380b457d575a4b119fbef9
Simon Cozens <simon@simon-cozens.org>
2016-02-28 21:44:45 +1100

    Make this more subclassable.

--- languages/unicode.lua
+++ languages/unicode.lua
@@ -44,24 +44,86 @@ SILE.nodeMakers.base = std.object {
 }
 
 SILE.nodeMakers.unicode = SILE.nodeMakers.base {
+  dealWith = function (self, item)
+    local char = item.text
+    local cp = SU.codepoint(char)
+    local thistype = chardata[cp] and chardata[cp].linebreak
+    if chardata[cp] and thistype == "sp" then
+      self:makeToken()
+      self:makeGlue()
+    elseif chardata[cp] and (thistype == "ba" or  thistype == "zw") then
+      self:addToken(char,item)
+      self:makeToken()
+      self:makePenalty(0)
+    elseif lasttype and (thistype ~= lasttype and thistype ~= "cm") then
+      self:makeToken()
+      self:addToken(char,item)
+    else
+      if SILE.settings.get("document.letterspaceglue") then
+        self:makeToken()
+        self:makeLetterSpaceGlue()
+      end
+      self:addToken(char,item)
+    end
+    if thistype ~= "cm" then lasttype = chardata[cp] and chardata[cp].linebreak end
+  end,
   iterator = function (self, items)
     self:init()
-    local ics = SILE.settings.get("document.letterspaceglue")
     return coroutine.wrap(function()
-      for i = 1,#items do item = items[i]
-        local char = items[i].text
+      for i = 1,#items do self:dealWith(items[i]) end
+      if SILE.settings.get("document.letterspaceglue") then self:makeLetterSpaceGlue() end
+      self:makeToken()
+    end)
+  end
+}
+
+pcall( function () icu = require("xjustenoughicu") end)
+if icu then
+  SILE.nodeMakers.unicode.iterator = function (self, items)
+    local fulltext = ""
+    local ics = SILE.settings.get("document.letterspaceglue")
+    for i = 1,#items do item = items[i]
+      fulltext = fulltext .. items[i].text
+    end
+    local chunks = {icu.breakpoints(fulltext, self.options.language)}
+    self:init()
+    table.remove(chunks,1)
+    return coroutine.wrap(function()
+      -- Special-case initial glue
+      local i = 1
+      while i < #items do item = items[i]
+        local char = item.text
         local cp = SU.codepoint(char)
         local thistype = chardata[cp] and chardata[cp].linebreak
-        if chardata[cp] and thistype == "sp" then
-          self:makeToken()
-          self:makeGlue()
-        elseif chardata[cp] and (thistype == "ba" or  thistype == "zw") then
-          self:addToken(char,item)
-          self:makeToken()
-          self:makePenalty(0)
-        elseif lasttype and (thistype ~= lasttype and thistype ~= "cm") then
-          self:makeToken()
-          self:addToken(char,item)
+        if thistype == "sp" then self:makeGlue() else break end
+        i = i + 1
+      end
+      -- And now onto the real thing
+      for i = i,#items do item = items[i]
+        local char = items[i].text
+        local cp = SU.codepoint(char)
+        if chunks[1] and (items[i].index >= chunks[1].index) then
+          -- There's a break here
+          local thistype = chardata[cp] and chardata[cp].linebreak
+          local bp = chunks[1]
+          while chunks[1] and items[i].index >= chunks[1].index do
+            table.remove(chunks,1)
+          end
+          if bp.type == "word" then
+            if chardata[cp] and thistype == "sp" then
+              -- Spacing word break
+              self:makeToken()
+              self:makeGlue()
+            else -- a word break which isn't a space
+              self:makeToken()
+              self:addToken(char,item)
+            end
+          elseif bp.type == "line" then
+            -- Line break
+            self:makeToken()
+            self:makePenalty(bp.subtype == "soft" and 0 or -1000)
+            self:addToken(char,item)
+          end
         else
           if ics then
             self:makeToken()
@@ -69,73 +131,9 @@ SILE.nodeMakers.unicode = SILE.nodeMakers.base {
           end
           self:addToken(char,item)
         end
-        if thistype ~= "cm" then lasttype = chardata[cp] and chardata[cp].linebreak end
       end
       if ics then self:makeLetterSpaceGlue() end
       self:makeToken()
     end)
   end
-}
-
-pcall( function () icu = require("justenoughicu") end)
-if icu then
-  SILE.nodeMakers.unicode = SILE.nodeMakers.base {
-    iterator = function (self, items)
-      local fulltext = ""
-      local ics = SILE.settings.get("document.letterspaceglue")
-      for i = 1,#items do item = items[i]
-        fulltext = fulltext .. items[i].text
-      end
-      local chunks = {icu.breakpoints(fulltext, self.options.language)}
-      self:init()
-      table.remove(chunks,1)
-      return coroutine.wrap(function()
-        -- Special-case initial glue
-        local i = 1
-        while i < #items do item = items[i]
-          local char = item.text
-          local cp = SU.codepoint(char)
-          local thistype = chardata[cp] and chardata[cp].linebreak
-          if thistype == "sp" then self:makeGlue() else break end
-          i = i + 1
-        end
-        -- And now onto the real thing
-        for i = i,#items do item = items[i]
-          local char = items[i].text
-          local cp = SU.codepoint(char)
-          if chunks[1] and (items[i].index >= chunks[1].index) then
-            -- There's a break here
-            local thistype = chardata[cp] and chardata[cp].linebreak
-            local bp = chunks[1]
-            while chunks[1] and items[i].index >= chunks[1].index do
-              table.remove(chunks,1)
-            end
-            if bp.type == "word" then
-              if chardata[cp] and thistype == "sp" then
-                -- Spacing word break
-                self:makeToken()
-                self:makeGlue()
-              else -- a word break which isn't a space
-                self:makeToken()
-                self:addToken(char,item)
-              end
-            elseif bp.type == "line" then
-              -- Line break
-              self:makeToken()
-              self:makePenalty(bp.subtype == "soft" and 0 or -1000)
-              self:addToken(char,item)
-            end
-          else
-            if ics then
-              self:makeToken()
-              self:makeLetterSpaceGlue()
-            end
-            self:addToken(char,item)
-          end
-        end
-        if ics then self:makeLetterSpaceGlue() end
-        self:makeToken()
-      end)
-    end
-  }
 end
