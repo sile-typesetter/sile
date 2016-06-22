d8c898fc3438535764a47e0b7dac3faee72f0fe0
Simon Cozens <simon@simon-cozens.org>
2016-06-22 17:07:59 +1000

    Documentation for the linespacing package.

--- packages/linespacing.lua
+++ packages/linespacing.lua
@@ -63,7 +63,7 @@ local getLineMetrics = function (l)
   return linemetrics
 end
 
-SILE.typesetter.leadingFor = function (self, v, previous)
+local linespacingLeading = function (self, v, previous)
   local method = SILE.settings.get("linespacing.method")
 
   local firstline = SILE.settings.get("linespacing.minimumfirstlineposition"):absolute()
@@ -121,3 +121,75 @@ SILE.typesetter.leadingFor = function (self, v, previous)
 
   SU.error("Unknown line spacing method "..method)
 end
+
+SILE.typesetter.leadingFor = linespacingLeading
+SILE.registerCommand("linespacing-on", function ()
+  SILE.typesetter.leadingFor = linespacingLeading
+end)
+SILE.registerCommand("linespacing-off", function ()
+  SILE.typesetter.leadingFor = SILE.defaultTypesetter.leadingFor
+end)
+
+
+return { documentation = [[\begin{document}
+\linespacing-on
+
+SILE’s default method of inserting leading between lines should be familiar to
+users of TeX, but it is not the most friendly system for book designers. The
+\code{linespacing} package provides a better choice of leading systems.
+
+After loading the package (with \code{\\script[src=packages/linespacing]}),
+you are able to choose the linespacing mode by setting the \code{linespacing.method}
+parameter. The following examples have funny sized words in them so that you can see
+how the different methods interact.
+
+By default, this is set to \code{tex}. The other options available are:
+
+\medskip
+\set[parameter=linespacing.method,value=fixed]
+\set[parameter=linespacing.fixed.baselinedistance,value=1.5em]
+\noindent{}• \code{fixed}. This set the lines at a fixed baseline-to-baseline distance,
+determined by the \code{linespacing.\goodbreak{}fixed.baselinedistance} parameter. You
+can specify this parameter either relative to the type size (e.g. \code{1.2em})
+or as a absolute distance (\code{15pt}). This paragraph is set with a fixed 1.5em
+baseline-to-baseline distance.
+
+\medskip
+\set[parameter=linespacing.method,value=fit-glyph]
+\noindent{}• \code{fit-glyph}. This sets the lines solid; that is, the lowest point
+on line 1 (either a descender like \font[size=20pt]{q} or, if there are no descenders, the baseline) will
+touch the \font[size=20pt]{highest} point of line 2, as in this paragraph.
+You generally don’t want to use this as is.
+
+\set[parameter=linespacing.fit-glyph.extra-space,value=5pt]
+
+What you probably want to do is insert a constant (relative or absolute) s\font[size=20pt]{p}ace
+between the lines by setting the \code{linespacing.fit-glyph.extra-space} parameter.
+\font[size=20pt]{T}his paragraph is set with 5 points of space between the descenders and the ascenders.
+
+\medskip
+\set[parameter=linespacing.method,value=fit-font]
+\noindent{}• \code{fit-font}. This inspects each hbox on the line, and asks the fonts
+it finds for their bounding boxes - the highest ascender and the lower descender. It
+then sets the lines solid. Essentially each character is treated as if it is the
+same height, rather like composing a slug of metal type. If
+there are things other than text on your line, or the text is buried inside other
+boxes, this may not work so well.
+
+\set[parameter=linespacing.fit-font.extra-space,value=5pt]
+
+As with \code{fit-glyph}, you can insert extra space between the lines with the
+\code{linespacing.fit-font.\goodbreak{}extra-space} parameter.
+
+\medskip
+\set[parameter=linespacing.method,value=css]
+\set[parameter=linespacing.css.line-height,value=2em]
+\noindent{}• \code{css}. This is similar to the method used in browsers; the baseline
+distance is set with the \code{linespacing.\goodbreak{}css.line-height} parameter, and the excess
+\font[size=20pt]{space} between this parameter and the actual height of the line is distributed
+between the top and bottom of the line.
+\medskip
+
+\linespacing-off
+\end{document}
+]]}
