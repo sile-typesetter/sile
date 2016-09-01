80de17e6f8f7dcafd8136e32ff0d3559043def90
Caleb Maclennan <caleb@alerque.com>
2016-09-01 14:37:15 +0300

    Revert parts of bootstrap removed in 3fdf3d0 that were good
    Some of the updates were fine, but some of them backtracked things that
    were actually meaningful.
    
    * Testing for conditions before running git commands keeps people from
      tripping up if they came from a tarball and Travis from tripping up on
      its own cache.
    
    * Using /usr/bin/env helps avoid environment variable issue on some
      platforms (e.g.g some "hardened" Linux distros & Android).
    
    * In place editing because it's cleaner and makes the exit code more
      reliable. (In spite of internet rumor in place editing -i does work on
      BSD sed, you just have to be sure and pass it an extension or
      zero length value.)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,27 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
-git submodule update --init --recursive --remote
+
+# We neet a local copy of the libtexpdf library to compile. If this was
+# downloaded as a src distibution package this will exist already, but if not
+# and we are part of a git repository that the user has not fully initialized,
+# go ahead and do the step of fetching the the submodule so the compile process
+# can run.
+if [ ! -f "libtexpdf/configure.ac" ] && [ -d ".git" ]; then
+    git submodule update --init --recursive --remote
+fi
+
 autoreconf --install
-case `uname` in Darwin*) glibtoolize ;;
-  *) libtoolize ;; esac
+
+# See discussion in https://github.com/simoncozens/sile/issues/82
+# http://blog.gaku.net/autoconf/
+case `uname` in
+    Darwin*) glibtoolize ;;
+    *)        libtoolize ;;
+esac
 aclocal
 automake --force-missing --add-missing
 autoreconf
+
 (cd libtexpdf; autoreconf -I m4)
-sed 's/rm -f core/rm -f/' configure > config.cache
-mv config.cache configure
-chmod +x configure
+
+sed -i -e 's/rm -f core/rm -f/' configure
