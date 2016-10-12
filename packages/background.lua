438a2c8cf7ae119928557ffeb2a245f1aa6cf8c3
Caleb Maclennan <caleb@alerque.com>
2016-10-12 10:19:10 +0300

    Fix background package so it doesn't make the docs all sepia

--- packages/background.lua
+++ packages/background.lua
@@ -23,16 +23,18 @@ SILE.registerCommand("background", function (options, content)
 end, "Draws a solid background color <color> on pages after initialization.")
 
 return { documentation = [[\begin{document}
-The \code{background} package allows you to set the color of the canvas background
-(by drawing a solid color block the full size of the page on page initialization).
-The package provides a \code{\\background} command which requires at least one
-parameter, \code{color=\em{<color \nobreak{}specification}}, and sets the backgound
-of the current and all following pages to that color. If setting only the current
-page background different from the default is desired, an extra parameter
+The \code{background} package allows you to set the color of the canvas
+background (by drawing a solid color block the full size of the page on page
+initialization). The package provides a \code{\\background} command which
+requires at least one parameter,
+\code{color=\em{<color \nobreak{}specification}}, and sets the backgound of the
+current and all following pages to that color. If setting only the current page
+background different from the default is desired, an extra parameter
 \code{allpages=false} can be passed. The color specification in the same as
 specified in the \code{color} package.
 
-\background[color=#e9d8ba]
+\background[color=#e9d8ba,allpages=false]
 
-So, for example, \code{\\background[color=#e9d8ba]} will set a sepia tone background.
+So, for example, \code{\\background[color=#e9d8ba,allpages=false]} will set a
+sepia tone background on the current page.
 \end{document}]] }
