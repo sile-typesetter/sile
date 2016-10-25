17c8fa48cf9b77fbe7301cec8822cfba0ca7f2f2
Caleb Maclennan <caleb@alerque.com>
2016-10-25 09:39:54 +0300

    Cleanup Lua coding style with long descriptive variables

--- packages/chapterverse.lua
+++ packages/chapterverse.lua
@@ -1,53 +1,57 @@
 SILE.require("packages/infonode")
 SILE.scratch.chapterverse = {}
 
-SILE.registerCommand("save-book-title", function (o,c)
-  SU.debug("bcv", "book: "..c[1])
-  SILE.scratch.chapterverse.book = c[1]
+SILE.registerCommand("save-book-title", function (options, content)
+  SU.debug("bcv", "book: " .. content[1])
+  SILE.scratch.chapterverse.book = content[1]
 end)
 
-SILE.registerCommand("save-chapter-number", function (o,c)
-  SU.debug("bcv", "chapter: "..c[1])
-  SILE.scratch.chapterverse.chapter = c[1]
+SILE.registerCommand("save-chapter-number", function (options, content)
+  SU.debug("bcv", "chapter: " .. content[1])
+  SILE.scratch.chapterverse.chapter = content[1]
 end)
 
-SILE.registerCommand("save-verse-number", function (o,c)
-  SU.debug("bcv", "verse: "..c[1])
-  SILE.scratch.chapterverse.verse = c[1]
-  local ref = { b = SILE.scratch.chapterverse.book, c = SILE.scratch.chapterverse.chapter, v = SILE.scratch.chapterverse.verse }
-  SU.debug("bcv", "ref: "..ref)
+SILE.registerCommand("save-verse-number", function (options, content)
+  SU.debug("bcv", "verse: " .. content[1])
+  SILE.scratch.chapterverse.verse = content[1]
+  local ref = {
+    book = SILE.scratch.chapterverse.book,
+    chapter = SILE.scratch.chapterverse.chapter,
+    verse = SILE.scratch.chapterverse.verse
+  }
+  SU.debug("bcv", "ref: " .. ref)
   SILE.Commands["info"]({ category = "references", value = ref }, {})
 end)
 
-SILE.registerCommand("first-reference", function (o,c)
+SILE.registerCommand("first-reference", function (options, content)
   local refs = SILE.scratch.info.thispage.references
-  SU.debug("bcv", "first-reference: "..SILE.scratch.info)
-  if refs then 
-    SU.debug("bcv", "first-reference: "..refs[1])
+  SU.debug("bcv", "first-reference: " .. SILE.scratch.info)
+  if refs then
+    SU.debug("bcv", "first-reference: " .. refs[1])
     SILE.call("format-reference", {}, refs[1])
   else
     SU.debug("bcv", "first-reference: none")
   end
 end)
 
-SILE.registerCommand("last-reference", function (o,c)
+SILE.registerCommand("last-reference", function (options, content)
   local refs = SILE.scratch.info.thispage.references
   if refs then
-    SU.debug("bcv", "last-reference: "..refs[#(refs)])
+    SU.debug("bcv", "last-reference: " .. refs[#(refs)])
     SILE.call("format-reference", {}, refs[#(refs)])
   else
     SU.debug("bcv", "last-reference: none")
   end
 end)
 
-SILE.registerCommand("format-reference", function (o,c)
-  SU.debug("bcv", "formatting: "..c)
+SILE.registerCommand("format-reference", function (options, content)
+  SU.debug("bcv", "formatting: " .. content)
   local ref
-  if c.b then
-    ref =  c.b .. " " .. c.c .. ":" .. c.v
+  if content.book then
+    ref = content.book .. " " .. content.chapter .. ":" .. content.verse
   else
-  	ref =  c.c .. ":" .. c.v
+    ref = content.chapter .. ":" .. content.verse
   end
-  SU.debug("bcv", "formatting: "..ref)
+  SU.debug("bcv", "formatting: " .. ref)
   SILE.typesetter:typeset(ref)
 end)
