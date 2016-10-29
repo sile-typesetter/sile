b966ccd9259820b7608ac956a8ab40aebfc6ee84
Simon Cozens <simon@simon-cozens.org>
2016-10-29 22:13:52 +1100

    Here’s the ICU code for #390, but I’m not wiring it up to counters.lua tonight.

--- src/justenoughicu.c
+++ src/justenoughicu.c
@@ -165,6 +165,30 @@ int icu_canonicalize_language(lua_State *L) {
   return 1;
 }
 
+
+int icu_format_number(lua_State *L) {
+  double a = luaL_checknumber(L, 1);
+  /* See http://www.unicode.org/repos/cldr/tags/latest/common/bcp47/number.xml
+     for valid system names */
+  char* system = luaL_checkstring(L, 2);
+  char locale[18]; // "@numbers=12345678";
+  UChar buf[256];
+  char utf8[256];
+  int32_t needed;
+  UErrorCode status = U_ZERO_ERROR;
+
+  snprintf(locale, 18, "@numbers=%s", system);
+  UNumberFormat* fmt = unum_open(UNUM_DECIMAL, 0, 0, locale, 0, &status);
+  if(U_FAILURE(status)) {
+    luaL_error(L, "Locale %s unavailable: %s", locale, u_errorName(status));
+  }
+  needed = unum_formatDouble(fmt, a, buf, 256, NULL, &status);
+  assert(!U_FAILURE(status));
+  u_austrncpy(utf8, buf, 256);
+  lua_pushstring(L, utf8);
+  return 1;
+}
+
 #if !defined LUA_VERSION_NUM
 /* Lua 5.0 */
 #define luaL_Reg luaL_reg
@@ -192,6 +216,7 @@ static const struct luaL_Reg lib_table [] = {
   {"breakpoints", icu_breakpoints},
   {"case", icu_case},
   {"canonicalize_language", icu_canonicalize_language},
+  {"format_number", icu_format_number},
   {NULL, NULL}
 };
 
