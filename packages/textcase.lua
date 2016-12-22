11e222cb9190cb6c019b6a70c60f40e09df29392
Caleb Maclennan <caleb@alerque.com>
2016-12-22 10:33:03 +0300

    Pass arguments to allow overriding language of casing functions

--- packages/textcase.lua
+++ packages/textcase.lua
@@ -1,28 +1,37 @@
 local icu = require("justenoughicu")
 local inputfilter = SILE.require("packages/inputfilter").exports
 
-local uppercase = function (input)
-  return icu.case(input, SILE.settings.get("document.language"), "upper")
+local uppercase = function (input, extraArgs)
+  if not extraArgs then extraArgs = {} end
+  if not extraArgs.attr then extraArgs.attr = {} end
+  local lang = extraArgs.attr.language or SILE.settings.get("document.language")
+  return icu.case(input, lang, "upper")
 end
 
-local lowercase = function (input)
-  return icu.case(input, SILE.settings.get("document.language"), "lower")
+local lowercase = function (input, extraArgs)
+  if not extraArgs then extraArgs = {} end
+  if not extraArgs.attr then extraArgs.attr = {} end
+  local lang = extraArgs.attr.language or SILE.settings.get("document.language")
+  return icu.case(input, lang, "lower")
 end
 
-local titlecase = function (input)
-  return icu.case(input, SILE.settings.get("document.language"), "title")
+local titlecase = function (input, extraArgs)
+  if not extraArgs then extraArgs = {} end
+  if not extraArgs.attr then extraArgs.attr = {} end
+  local lang = extraArgs.attr.language or SILE.settings.get("document.language")
+  return icu.case(input, lang, "title")
 end
 
 SILE.registerCommand("uppercase", function(options, content)
-  SILE.process(inputfilter.transformContent(content, uppercase))
+  SILE.process(inputfilter.transformContent(content, uppercase, options))
 end, "Typeset the enclosed text as uppercase")
 
 SILE.registerCommand("lowercase", function(options, content)
-  SILE.process(inputfilter.transformContent(content, lowercase))
+  SILE.process(inputfilter.transformContent(content, lowercase, options))
 end, "Typeset the enclosed text as lowercase")
 
 SILE.registerCommand("titlecase", function(options, content)
-  SILE.process(inputfilter.transformContent(content, titlecase))
+  SILE.process(inputfilter.transformContent(content, titlecase, options))
 end, "Typeset the enclosed text as titlecase")
 
 return {
