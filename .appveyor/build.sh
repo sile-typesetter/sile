8970dd06769d233c96bb08670a85b1d4aaa9a94a
Simon Cozens <simon@simon-cozens.org>
2017-01-20 14:51:43 +1100

    More env vars, and try installing some luarocks.

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -6,8 +6,25 @@ export TRAVIS_BUILD_DIR=/c/projects/sile/
 export TRAVIS_OS_NAME=mingw
 export LUAROCKS=2.2.2
 source .travis/setenv_lua.sh
+export LUA_HOME_DIR=$TRAVIS_BUILD_DIR/install/lua
+export LUA=~/.lua/lua
+export LUA_INCLUDE=-I$LUA_HOME_DIR/include
+export LD_LIBRARY_PATH=$LUA_HOME_DIR/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$LUA_HOME_DIR/lib:$LD_LIBRARY_PATH
+export LD_RUN_PATH=$LUA_HOME_DIR/lib:$LD_RUN_PATH
+export PATH=.travis:$PATH
 
 lua -e "print('Hello ' .. _VERSION .. '!')"
+
+luarocks install lpeg
+luarocks install lua-zlib
+luarocks install luaexpat
+luarocks install luafilesystem
+luarocks install lua_cliargs 2.3-3
+luarocks install busted
+luarocks install luacov 0.8-1
+luarocks install luacov-coveralls
+
 ./bootstrap.sh || exit 1
 ./configure || exit 1
 make || exit 1
