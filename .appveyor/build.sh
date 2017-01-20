9266c3066be9e439d5a013794bdf7e7dacd45079
Simon Cozens <simon@simon-cozens.org>
2017-01-20 13:27:19 +1100

    More Travis-emulating environment vars

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 cd $APPVEYOR_BUILD_FOLDER
+
+# Pretend we are on travis so that the Lua build code can be reused
+export TRAVIS_BUILD_DIR=$APPVEYOR_BUILD_FOLDER
 export TRAVIS_OS_NAME=mingw64
 source .travis/setenv_lua.sh
+
 lua -e "print('Hello ' .. _VERSION .. '!')"
 ./bootstrap.sh || exit 1
 ./configure || exit 1
