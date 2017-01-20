d335653a0ac7e48c169d7b9e8762d76f0fc41c3b
Simon Cozens <simon@simon-cozens.org>
2017-01-20 14:43:23 +1100

    We are getting further!

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -4,6 +4,7 @@ cd $APPVEYOR_BUILD_FOLDER
 # Pretend we are on travis so that the Lua build code can be reused
 export TRAVIS_BUILD_DIR=/c/projects/sile/
 export TRAVIS_OS_NAME=mingw
+export LUAROCKS=2.2.2
 source .travis/setenv_lua.sh
 
 lua -e "print('Hello ' .. _VERSION .. '!')"
