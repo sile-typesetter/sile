08c786a7a50a1c289a623f6d9dd5ec21b77671c7
Simon Cozens <simon@simon-cozens.org>
2017-01-20 13:31:00 +1100

    Lua needs a known platform name. See if we can build like this.

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -3,7 +3,7 @@ cd $APPVEYOR_BUILD_FOLDER
 
 # Pretend we are on travis so that the Lua build code can be reused
 export TRAVIS_BUILD_DIR=$APPVEYOR_BUILD_FOLDER
-export TRAVIS_OS_NAME=mingw64
+export TRAVIS_OS_NAME=mingw
 source .travis/setenv_lua.sh
 
 lua -e "print('Hello ' .. _VERSION .. '!')"
