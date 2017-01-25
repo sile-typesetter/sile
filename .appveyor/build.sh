0b4facd1f70e26de89d7406b335d95ad2c162642
Simon Cozens <simon@simon-cozens.org>
2017-01-25 15:17:02 +1100

    Fiddle include directory for lfs.

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -27,7 +27,7 @@ echo "---"
 
 echo "Building lfs"
 cd vendor/luafilesystem
-make LUADIR=$LUA_HOME_DIR/include LIB_OPTION="-shared "$HOME/.lua/lua*.dll
+make LUA_INC=$LUA_HOME_DIR/include LIB_OPTION="-shared "$HOME/.lua/lua*.dll
 mkdir -p /usr/local/lib/lua/5.3/
 cp src/lfs.so /usr/local/lib/lua/5.3/lfs.so
 cd ../..
