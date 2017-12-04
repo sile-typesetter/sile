eccb26862a8320542fc8ed3761de0710d1913500
Simon Cozens <simon@simon-cozens.org>
2017-12-05 09:30:00 +1100

    Align column tops

--- tests/classes/testtwocol.lua
+++ tests/classes/testtwocol.lua
@@ -2,7 +2,7 @@ local plain = SILE.require("plain", "classes")
 local testtwocols = plain { id = "testtwocols" }
 
 local gutterWidth = "3%pw"
-testtwocols:declareFrame("contentA", {left = "left(content)", right = "left(gutter)", top = "5%pw", bottom = "83.3%ph", next = "contentB", balanced = true })
+testtwocols:declareFrame("contentA", {left = "left(content)", right = "left(gutter)", top = "5%ph", bottom = "83.3%ph", next = "contentB", balanced = true })
 testtwocols:declareFrame("contentB", {left = "right(gutter)", width="width(contentA) * 2 / 3", right = "right(content)", top = "5%ph", bottom = "top(footnotes)", balanced = true })
 testtwocols:declareFrame("gutter", {left = "right(contentA)", right = "left(contentB)", width = gutterWidth })
 testtwocols:declareFrame("footnotes", { left="left(contentB)", right = "right(contentB)", height = "0", bottom="83.3%ph"})
