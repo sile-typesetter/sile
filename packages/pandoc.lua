4a9dab4326b6700a09605e6cd4a1d423f142c3f4
Caleb Maclennan <caleb@alerque.com>
2019-02-25 13:40:14 +0300

    chore(packages): Support more commands matching pandoc template

--- packages/pandoc.lua
+++ packages/pandoc.lua
@@ -27,42 +27,53 @@ SILE.registerCommand("rule", function (options, _)
   SILE.call("hrule", options)
 end)
 
-SILE.registerCommand("textem", function (options, content)
+SILE.registerCommand("span", function (options, content)
+  handlePandocArgs(options)
+  SILE.process(content)
+end,"Generic inline wrapper")
+
+SILE.registerCommand("emph", function (options, content)
   handlePandocArgs(options)
   SILE.call("em", {}, content)
 end,"Inline emphasis wrapper")
 
-SILE.registerCommand("textstrong", function (options, content)
+local oldStrong = SILE.Commands["strong"]
+SILE.registerCommand("strong", function (options, content)
   handlePandocArgs(options)
-  SILE.call("strong", {}, content)
+  oldStrong(options, content)
 end,"Inline strong wrapper")
 
-SILE.registerCommand("textsc", function (options, content)
+SILE.registerCommand("smallcaps", function (options, content)
   handlePandocArgs(options)
   SILE.call("font", { features = "+smcp" }, content)
 end,"Inline small caps wrapper")
 
-SILE.registerCommand("textup", function (options, content)
+SILE.registerCommand("csl-no-emph", function (options, content)
   handlePandocArgs(options)
   SILE.call("font", { style = "Roman" }, content)
 end,"Inline upright wrapper")
 
-SILE.registerCommand("textnormal", function (options, content)
+SILE.registerCommand("csl-no-strong", function (options, content)
   handlePandocArgs(options)
   SILE.call("font", { weight = 400 }, content)
-end,"Inline upright wrapper")
+end,"Inline normal weight wrapper")
+
+SILE.registerCommand("csl-no-smallcaps", function (options, content)
+  handlePandocArgs(options)
+  SILE.call("font", { features = "-smcp" }, content)
+end,"Inline smallcaps disable wrapper")
 
 local scriptOffset = "0.7ex"
 local scriptSize = "1.5ex"
 
-SILE.registerCommand("textsuperscript", function (options, content)
+SILE.registerCommand("superscript", function (options, content)
   handlePandocArgs(options)
   SILE.call("raise", { height = scriptOffset }, function ()
     SILE.call("font", { size = scriptSize }, content)
   end)
 end,"Inline superscript wrapper")
 
-SILE.registerCommand("textsubscript", function (options, content)
+SILE.registerCommand("subscript", function (options, content)
   handlePandocArgs(options)
   SILE.call("lower", { height = scriptOffset }, function ()
     SILE.call("font", { size = scriptSize }, content)
@@ -73,11 +84,9 @@ SILE.registerCommand("unimplemented", function (options, content)
   handlePandocArgs(options)
   SU.debug("pandoc", "Un-implemented function")
   SILE.process(content)
-end,"Inline small caps wrapper")
+end,"Unimplemented Pandoc function wrapper")
 
-SILE.Commands["strike"] = SILE.Commands["unimplemented"]
-SILE.Commands["strike"] = SILE.Commands["unimplemented"]
-
+SILE.Commands["strikeout"] = SILE.Commands["unimplemented"]
 
 return { documentation = [[\begin{document}
 
@@ -88,16 +97,20 @@ Provided by in base classes etc.:
 \listitem \code{listarea}
 \listitem \code{listitem}
 
+Modified from default:
+
+\listitem \code{strong}
+
 Provided specifically for Pandoc:
 
 \listitem \code{span}
-\listitem \code{textem}
-\listitem \code{textstrong}
-\listitem \code{textsc}
-\listitem \code{textnosc}
-\listitem \code{textnoem}
-\listitem \code{textnostrong}
-\listitem \code{textsuperscript}
-\listitem \code{textsubscript}
+\listitem \code{emph}
+\listitem \code{smallcaps}
+\listitem \code{strikeout}
+\listitem \code{csl-no-emph}
+\listitem \code{csl-no-strong}
+\listitem \code{csl-no-smallcaps}
+\listitem \code{superscript}
+\listitem \code{subscript}
 
 \end{document}]] }
