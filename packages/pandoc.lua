6b73e5806dc9139548f794a603284aa417cb8044
Caleb Maclennan <caleb@alerque.com>
2019-02-25 14:25:39 +0300

    style(packages): Match case to Pandoc's internal AST

--- packages/pandoc.lua
+++ packages/pandoc.lua
@@ -11,6 +11,12 @@ local handlePandocArgs = function (options)
   if options.id then
     SU.debug("pandoc", "Set ID on tag")
   end
+  for _, class in pairs(options.classes) do
+    if SILE.Commands["Class:"..class] then
+      SILE.Call("Class:"..class, options, {})
+    end
+    SU.debug("pandoc", "Add div class "..class)
+  end
 end
 
 SILE.registerCommand("label", function(options, content)
@@ -21,75 +27,66 @@ SILE.registerCommand("tt", function(options, content)
   SILE.call("verbatim:font", options, content)
 end)
 
-SILE.registerCommand("rule", function (options, _)
+SILE.registerCommand("HorizontalRule", function (options, _)
   options.height = options.height or "0.2pt"
   options.width = options.width or SILE.typesetter.frame:lineWidth()
   SILE.call("hrule", options)
 end)
 
-SILE.registerCommand("span", function (options, content)
-  handlePandocArgs(options)
-  for _, class in pairs(options.classes) do
-    SU.debug("pandoc", "Add div class "..class)
-  end
-  SILE.process(content)
+SILE.registerCommand("Span", function (options, content)
+  SILE.settings.temporarily(function ()
+    handlePandocArgs(options)
+    SILE.process(content)
+  end)
 end,"Generic inline wrapper")
 
-SILE.registerCommand("div", function (options, content)
-  handlePandocArgs(options)
-  SILE.process(content)
+SILE.registerCommand("Div", function (options, content)
+  SILE.settings.temporarily(function ()
+    handlePandocArgs(options)
+    SILE.process(content)
+  end)
 end,"Generic inline wrapper")
 
-SILE.registerCommand("emph", function (options, content)
-  handlePandocArgs(options)
+SILE.registerCommand("Emph", function (options, content)
   SILE.call("em", {}, content)
 end,"Inline emphasis wrapper")
 
-local oldStrong = SILE.Commands["strong"]
-SILE.registerCommand("strong", function (options, content)
-  handlePandocArgs(options)
-  oldStrong(options, content)
+SILE.registerCommand("Strong", function (options, content)
+  SILE.call("strong", {}, content)
 end,"Inline strong wrapper")
 
-SILE.registerCommand("smallcaps", function (options, content)
-  handlePandocArgs(options)
+SILE.registerCommand("SmallCaps", function (options, content)
   SILE.call("font", { features = "+smcp" }, content)
 end,"Inline small caps wrapper")
 
 SILE.registerCommand("csl-no-emph", function (options, content)
-  handlePandocArgs(options)
   SILE.call("font", { style = "Roman" }, content)
 end,"Inline upright wrapper")
 
 SILE.registerCommand("csl-no-strong", function (options, content)
-  handlePandocArgs(options)
   SILE.call("font", { weight = 400 }, content)
 end,"Inline normal weight wrapper")
 
 SILE.registerCommand("csl-no-smallcaps", function (options, content)
-  handlePandocArgs(options)
   SILE.call("font", { features = "-smcp" }, content)
 end,"Inline smallcaps disable wrapper")
 
 local scriptOffset = "0.7ex"
 local scriptSize = "1.5ex"
 
-SILE.registerCommand("superscript", function (options, content)
-  handlePandocArgs(options)
+SILE.registerCommand("Superscript", function (options, content)
   SILE.call("raise", { height = scriptOffset }, function ()
     SILE.call("font", { size = scriptSize }, content)
   end)
 end,"Inline superscript wrapper")
 
-SILE.registerCommand("subscript", function (options, content)
-  handlePandocArgs(options)
+SILE.registerCommand("Subscript", function (options, content)
   SILE.call("lower", { height = scriptOffset }, function ()
     SILE.call("font", { size = scriptSize }, content)
   end)
 end,"Inline subscript wrapper")
 
 SILE.registerCommand("unimplemented", function (options, content)
-  handlePandocArgs(options)
   SU.debug("pandoc", "Un-implemented function")
   SILE.process(content)
 end,"Unimplemented Pandoc function wrapper")
@@ -107,19 +104,19 @@ Provided by in base classes etc.:
 
 Modified from default:
 
-\listitem \code{strong}
 
 Provided specifically for Pandoc:
 
-\listitem \code{span}
-\listitem \code{div}
-\listitem \code{emph}
-\listitem \code{smallcaps}
-\listitem \code{strikeout}
+\listitem \code{Span}
+\listitem \code{Div}
+\listitem \code{Emph}
+\listitem \code{Strong}
+\listitem \code{SmallCaps}
+\listitem \code{Strikeout}
 \listitem \code{csl-no-emph}
 \listitem \code{csl-no-strong}
 \listitem \code{csl-no-smallcaps}
-\listitem \code{superscript}
-\listitem \code{subscript}
+\listitem \code{Superscript}
+\listitem \code{Subscript}
 
 \end{document}]] }
