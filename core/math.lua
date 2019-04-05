0489c04d944df52a1396cd5c902344ef8c53db2d
Rui Xia <ruixia@microsoft.com>
2019-04-05 05:28:44 -0700

    feat(math): Add subscript and superscript; add math constants

--- core/math.lua
+++ core/math.lua
@@ -1,7 +1,7 @@
 local nodefactory = require("core/nodefactory")
-require("core/typesetter")
+local hb = require("justenoughharfbuzz")
 
-local mathStyle = {
+local mathMode = {
   display = 0,
   displayCramped = 1,
   text = 2,
@@ -28,6 +28,35 @@ local atomType = {
   vcenter = 12
 }
 
+local scriptType = {
+  upright = 1,
+  bold = 2, -- also have Greek and digits
+  italic = 3, -- also have Greek
+  boldItalic = 4, -- also have Greek
+  script = 5,
+  boldScript = 6,
+  fraktur = 7,
+  boldFraktur = 8,
+  doubleStruck = 9, -- also have digits
+  sansSerif = 10, -- also have digits
+  sansSerifBold = 11, -- also have Greek and digits
+  sansSerifItalic = 12,
+  sansSerifBoldItalic = 13, -- also have Greek
+  monospace = 14, -- also have digits
+}
+
+-- local scriptConversionTable {
+--   scriptType.bold =
+-- }
+
+local function isCrampedMode(mode)
+  return mode % 2 == 1
+end
+
+local function isItalicScript(script)
+  return script == 3 or script == 4 or script == 12
+end
+
 local mathScriptConversionTable = {
   italicLatinUpper = function(codepoint) return codepoint + 0x1D434 - 0x41 end,
   italicLatinLower = function(codepoint) return codepoint == 0x68 and 0x210E or codepoint + 0x1D44E - 0x61 end
@@ -40,35 +69,45 @@ local function getFont(options)
     family=SILE.settings.get("math.font.family")
   })
   if options.family then font.family = options.family end
+  if options.size then font.size = options.size end
   return font
 end
 
--- Style transition functions for superscript and subscript
-local function getSuperscriptStyle(style)
-  if style == mathStyle.display or style == mathStyle.displayCramped then return mathStyle.script                 -- D, T -> S
-  elseif style == mathStyle.displayCramped or style == mathStyle.textCramped then return mathStyle.scriptCramped  -- D', T' -> S'
-  elseif style == mathStyle.script or style == mathStyle.scriptScript then return mathStyle.scriptScript          -- S, SS -> SS
-  else return mathStyle.scriptScriptCramped end                                                                   -- S', SS' -> SS'
+local function getMathConstants(options)
+  local font = getFont(options)
+  local face = SILE.font.cache(font, SILE.shaper.getFace)
+  if not face then
+    SU.error("Could not find requested font "..options.." or any suitable substitutes")
+  end
+  return hb.get_math_constants(face.data, face.index, font.size)
 end
-local function getSubscriptStyle(style)
-  if style == mathStyle.display or style == mathStyle.displayCramped
-      or style == mathStyle.displayCramped or style == mathStyle.textCramped then return mathStyle.scriptCramped  -- D, T, D', T' -> S'
-  else return mathStyle.scriptScriptCramped end                                                                   -- S, SS, S', SS' -> SS'
+
+-- Style transition functions for superscript and subscript
+local function getSuperscriptMode(mode)
+  if mode == mathMode.display or mode == mathMode.displayCramped then return mathMode.script                 -- D, T -> S
+  elseif mode == mathMode.displayCramped or mode == mathMode.textCramped then return mathMode.scriptCramped  -- D', T' -> S'
+  elseif mode == mathMode.script or mode == mathMode.scriptScript then return mathMode.scriptScript          -- S, SS -> SS
+  else return mathMode.scriptScriptCramped end                                                                   -- S', SS' -> SS'
+end
+local function getSubscriptMode(mode)
+  if mode == mathMode.display or mode == mathMode.displayCramped
+      or mode == mathMode.displayCramped or mode == mathMode.textCramped then return mathMode.scriptCramped  -- D, T, D', T' -> S'
+  else return mathMode.scriptScriptCramped end                                                                   -- S, SS, S', SS' -> SS'
 end
 
 -- Style transition functions for fraction (numerator and denominator)
-local function getNumeratorStyle(style)
-  if style == mathStyle.display then return mathStyle.text                                                -- D -> T
-  elseif style == mathStyle.displayCramped then return mathStyle.textCramped                              -- D' -> T'
-  elseif style == mathStyle.text then return mathStyle.script                                             -- T -> S
-  elseif style == mathStyle.textCramped then return mathStyle.scriptCramped                               -- T' -> S'
-  elseif style == mathStyle.script or style == mathStyle.scriptScript then return mathStyle.scriptScript  -- S, SS -> SS
-  else return mathStyle.scriptScriptCramped end                                                           -- S', SS' -> SS'
+local function getNumeratorMode(mode)
+  if mode == mathMode.display then return mathMode.text                                                -- D -> T
+  elseif mode == mathMode.displayCramped then return mathMode.textCramped                              -- D' -> T'
+  elseif mode == mathMode.text then return mathMode.script                                             -- T -> S
+  elseif mode == mathMode.textCramped then return mathMode.scriptCramped                               -- T' -> S'
+  elseif mode == mathMode.script or mode == mathMode.scriptScript then return mathMode.scriptScript  -- S, SS -> SS
+  else return mathMode.scriptScriptCramped end                                                           -- S', SS' -> SS'
 end
-local function getDenominatorStyle(style)
-  if style == mathStyle.display or style == mathStyle.displayCramped then return mathStyle.text           -- D, D' -> T'
-  elseif style == mathStyle.text or style == mathStyle.textCramped then return mathStyle.script           -- T, T' -> S'
-  else return mathStyle.scriptScriptCramped end                                                           -- S, SS, S', SS' -> SS'
+local function getDenominatorMode(mode)
+  if mode == mathMode.display or mode == mathMode.displayCramped then return mathMode.text           -- D, D' -> T'
+  elseif mode == mathMode.text or mode == mathMode.textCramped then return mathMode.script           -- T, T' -> S'
+  else return mathMode.scriptScriptCramped end                                                           -- S, SS, S', SS' -> SS'
 end 
 
 function _box:isMathBox () return self.type == "math" end
@@ -78,21 +117,16 @@ function _box:isMathBox () return self.type == "math" end
 -- math box, box with a horizontal shift value and could contain zero or more _mbox'es (or its child classes)
 -- the entire math environment itself is a top-level mbox.
 -- Typesetting of mbox evolves four steps:
---   1. Determine the style for each mbox according to their parent.
+--   1. Determine the mode for each mbox according to their parent.
 --   2. Shape the mbox hierarchy from leaf to top. Get the shape and relative position.
---   3. Recursively determine the position of the mbox's to the root mbox (i.e., add up the relative positions along the tree path)
---   4. Convert mbox into _nnode's to put in SILE's typesetting framwork
+--   3. Convert mbox into _nnode's to put in SILE's typesetting framwork
 local _mbox = _box {
   _type = "Mbox",
   type = "math",
-  style = nil,
   options = {},
-  level = 0, -- The level in mbox hierarchy
   children = {}, -- The child nodes
-  relX = nil, -- x position relative to its parent box
-  relY = nil, -- y position relative to its parent box
-  absX = nil, -- x position relative to the root
-  absY = nil, -- y position relative to the root
+  relX = 0, -- x position relative to its parent box
+  relY = 0, -- y position relative to its parent box
   value = {},
   __tostring = function (s) return s.type end,
   init = function(self) return self end,
@@ -105,7 +139,7 @@ local _mbox = _box {
     SU.error("This is a virtual function that need to be overriden by its child classes")
   end,
 
-  shapeYourself = function(self)
+  shape = function(self)
     SU.error("This is a virtual function that need to be overriden by its child classes")
   end,
 
@@ -113,11 +147,11 @@ local _mbox = _box {
     SU.error("This is a virtual function that need to be overriden by its child classes")
   end,
 
-  -- Determine the style of its descendants
+  -- Determine the mode of its descendants
   styleDescendants = function(self)
     self:styleChildren()
     for i, n in ipairs(self.children) do
-      n:styleDescendants()
+      if n then n:styleDescendants() end
     end
   end,
 
@@ -127,28 +161,23 @@ local _mbox = _box {
   -- and (relX, relY) which the relative position to its parent
   shapeTree = function(self)
     for i, n in ipairs(self.children) do
-      n:shapeTree()
+      if n then n:shapeTree() end
     end
     self:setChildrenRelXY()
-    self:shapeYourself()
-  end,
-
-  -- positionDescendants calculates the "absolute" position of the descendants, not including itself.
-  positionDescendants = function(self)
-    for i, n in ipairs(self.children) do
-      n.absX = self.absX + n.relX
-      n.absY = self.absY + n.relY
-      n:positionDescendants()
-    end
+    self:shape()
   end,
 
   -- Output the node and all its descendants
   outputTree = function(self, x, y)
     self:output(x, y)
     for i, n in ipairs(self.children) do
-      n:outputTree(x, y)
+      if n then n:outputTree(x + n.relX, y + n.relY) end
     end
   end
+}
+
+local _nil = _mbox {
+  _type = 'Nil'
 }
 
 -- _stackbox stacks its content one, either horizontally or vertically
@@ -166,7 +195,7 @@ local _stackbox = _mbox {
 
   styleChildren = function(self)
     for i, n in ipairs(self.children) do
-      n.style = self.style
+      n.mode = self.mode
     end
   end,
   setChildrenRelXY = function(self)
@@ -192,7 +221,7 @@ local _stackbox = _mbox {
       self.relY = 0
     end
   end,
-  shapeYourself = function(self)
+  shape = function(self)
     -- Get the minimum box that contains all children. Baseline is determined by relX, relY.
     -- Along the stacking direction, the size of computed as last.end - first.start.
     -- On the other direction, the size is max(end) - min(start).
@@ -232,9 +261,75 @@ local _stackbox = _mbox {
   outputYourself = function(self, typesetter, line)
     local mathX = typesetter.frame.state.cursorX
     local mathY = typesetter.frame.state.cursorY
-    self:outputTree(mathX, mathY)
+    self:outputTree(mathX + self.relX, mathY + self.relY)
     typesetter.frame.state.cursorX = mathX + self.width
     typesetter.frame.state.curosrY = mathY
+  end,
+  output = function(self, x, y) end
+}
+
+local _subscript = _mbox {
+  _type = "Subscript",
+  kind = "sub",
+  init = function(self)
+    if self.kind == "sup" then
+      self.children[3] = self.children[2]
+      self.children[2] = false
+    end
+  end,
+  styleChildren = function(self)
+    self.children[1].mode = self.mode
+    if self.children[2] then self.children[2].mode = getSubscriptMode(self.mode) end
+    if self.children[3] then self.children[3].mode = getSuperscriptMode(self.mode) end
+  end,
+  setChildrenRelXY = function(self)
+    print(self.children)
+    local constants = getMathConstants(self.options)
+    self.children[1].relX = 0
+    self.children[1].relY = 0
+    if self.children[2] then
+      self.children[2].relX = self.children[1].width
+      self.children[2].relY = table.maxn({
+        constants.SubscriptShiftDown,
+        self.children[1].depth + constants.SubscriptBaselineDropMin,
+        self.children[2].height - constants.SubscriptTopMax
+      })
+    end
+    if self.children[3] then
+      self.children[3].relX = self.children[1].width
+      self.children[3].relY = -table.maxn({
+        isCrampedMode(self.children[3].mode) and constants.SuperscriptShiftUpCramped or constants.SuperscriptShiftUp, -- or cramped
+        self.children[1].height - constants.SuperscriptBaselineDropMax,
+        self.children[3].depth + constants.SuperscriptBottomMin
+      })
+    end
+    if self.children[2] and self.children[3] then
+      local gap = self.children[2].relY - self.children[2].height - self.children[3].relY - self.children[3].depth
+      if gap < constants.SubSuperscriptGapMin then
+        local supShift, subShift = constants.SubSuperscriptGapMin - gap, 0
+        if supShift > constants.SuperscriptBottomMaxWithSubscript then
+          subShift = supShift - constants.SuperscriptBottomMaxWithSubscript
+          supShift = constants.SuperscriptBottomMaxWithSubscript
+        end
+        self.children[3].relY = self.children[3].relY - supShift
+        self.children[2].relY = self.children[2].relY + subShift
+      end
+    end
+  end,
+  shape = function(self)
+    local height, depth = self.children[1].height, self.children[1].depth
+    local width1, width2 = self.children[1].width, self.children[1].width
+    if self.children[2] then
+      depth = self.children[2].depth + 1
+      width1 = width1 + self.children[2].width
+    end
+    if self.children[3] then
+      height = self.children[3].height + 3.75
+      width2 = width2 + self.children[3].width
+    end
+    self.height = height
+    self.depth = depth
+    self.width = math.max(width1, width2)
   end,
   output = function(self, x, y) end
 }
@@ -250,29 +345,36 @@ local _terminal = _mbox {
 local _text = _terminal {
   _type = "Text",
   text = "",
-  options = { style="Italic" },
-  __tostring = function(self) return "Text("..self.text..")" end,
+  script = scriptType.upright,
+  __tostring = function(self) return "Text("..(self.originalText or self.text)..")" end,
   init = function(self)
     local converted = ""
     for uchr in SU.utf8codes(self.text) do
+      local dst_char = SU.utf8char(uchr)
       if uchr >= 0x41 and uchr <= 0x5A then -- Latin capital letter
-        if self.options.style == "Italic" then
-          converted = converted..SU.utf8char(mathScriptConversionTable.italicLatinUpper(uchr))
-        else
-          converted = converted..SU.utf8char(uchr)
+        if self.script == scriptType.italic then
+          dst_char = SU.utf8char(mathScriptConversionTable.italicLatinUpper(uchr))
         end
       elseif uchr >= 0x61 and uchr <= 0x7A then -- Latin non-capital letter
-        if self.options.style == "Italic" then
-          converted = converted..SU.utf8char(mathScriptConversionTable.italicLatinLower(uchr))
-        else
-          converted = converted..SU.utf8char(uchr)
+        if self.script == scriptType.italic then
+          dst_char = SU.utf8char(mathScriptConversionTable.italicLatinLower(uchr))
         end
       end
+      converted = converted..dst_char
     end
-    self.text = converted
+    self.originalText = self.text
+    self.text = converted 
   end,
-  shapeYourself = function(self)
-    local glyphs = SILE.shaper:shapeToken(self.text, getFont(self.options))
+  shape = function(self)
+    local font = getFont(self.options)
+    if self.mode == mathMode.script or self.mode == mathMode.scriptCramped then
+      local fontSize = math.floor(font.size * 0.7)
+      font.size = fontSize
+      self.options.size = fontSize
+    end
+    local glyphs = SILE.shaper:shapeToken(self.text, font)
+    SILE.shaper:preAddNodes(glyphs, self.value)
+    self.value.items = glyphs
     self.value.glyphString = {}
     if glyphs and #glyphs > 0 then
       for i = 1, #glyphs do
@@ -296,14 +398,15 @@ local _text = _terminal {
   end,
   output = function(self, x, y)
     if not self.value.glyphString then return end
-    SILE.outputter.moveTo(x + self.absX, y + self.absY)
+    print('Output '..self.value.glyphString.." to "..x..", "..y)
+    SILE.outputter.moveTo(x, y)
     SILE.outputter.setFont(getFont(self.options))
     SILE.outputter.outputHbox(self.value, self.width)
   end
 }
 
 local newText = function(spec)
-  local ret = _text(spec)
+  local ret = std.tree.clone(_text(spec))
   ret:init()
   return ret
 end
@@ -314,28 +417,46 @@ local newStackbox = function(spec)
   return ret
 end
 
+local newSubscript = function(spec)
+  local ret = std.tree.clone(_subscript(spec))
+  ret:init()
+  return ret
+end
+
 -- convert MathML into mbox
-local function ConvertMathML(content, mbox)
-  for i,v in ipairs(content) do
-    if type(v) == "string" then
-      v = v:gsub("^%s*(.-)%s*$", "%1")
-      if v and v ~= "" then
-        -- Add text node to mbox
-      end
-    elseif type(v) == "table" then
-      if v.id == 'command' then
-        if v.tag == 'mrow' then
-          local hbox = newStackbox({ direction='H', level=mbox.level+1 })
-          ConvertMathML(v, hbox)
-          table.insert(mbox.children, hbox)
-        elseif v.tag == 'mi' then
-          local text = newText({ text=v[1], level=mbox.level+1 })
-          table.insert(mbox.children, text)
-        end
-      else
-        ConvertMathML(v, mbox)
-      end
+local function ConvertMathML(content)
+  if content == nil or content.id == nil or content.tag == nil then return nil end
+  local convertChildren = function(content)
+    local mboxes = {}
+    for i, n in ipairs(content) do
+      local box = ConvertMathML(n)
+      if box then table.insert(mboxes, box) end
     end
+    return mboxes
+  end
+  if content.tag == 'math' then -- toplevel
+    return newStackbox({
+      direction='V', children=convertChildren(content) })
+  elseif content.tag == 'mrow' then
+    return newStackbox({
+      direction='H', children=convertChildren(content) })
+  elseif content.tag == 'mi' then
+    return newText({ script=scriptType.italic, text=content[1] })
+  elseif content.tag == 'mo' then
+    return newText({ script=scriptType.upright, text=content[1] })
+  elseif content.tag == 'mn' then
+    return newText({ script=scriptType.upright, text=content[1] })
+  elseif content.tag == 'msub' then
+    return newSubscript({
+      kind="sub", children=convertChildren(content) })
+  elseif content.tag == 'msup' then
+    return newSubscript({
+      kind="sup", children=convertChildren(content) })
+  elseif content.tag == 'msubsup' then
+    return newSubscript({
+      kind="subsup", children=convertChildren(content) })
+  else
+    return nil
   end
 end
 
@@ -348,24 +469,19 @@ SILE.typesetter.pushMath = function(self, mbox)
   return self:pushHorizontal(mbox)
 end
 
-SILE.registerCommand("mathml", function (options, content)
-  local mbox = newStackbox({ direction='V' })
-  ConvertMathML(content, mbox)
+SILE.registerCommand("math", function (options, content)
+  local mode = (options and options.mode) and options.mode or 'text'
+
+  local mbox = ConvertMathML(content, mbox)
 
   if #(mbox.children) == 1 then
     mbox = mbox.children[1]
   end
 
-  mbox.style = mathStyle.display -- or text
+  mbox.mode = mathMode.display -- or text
   mbox:styleDescendants()
 
   mbox:shapeTree()
-
-  mbox.absX = 0
-  mbox.absY = 0
-  mbox:positionDescendants()
-
-  print(mbox.width..' '..mbox.height..' '..mbox.depth)
 
   SILE.typesetter:pushMath(mbox)
 
