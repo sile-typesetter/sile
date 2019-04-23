23bdb255f7eea9e787142d81d2bd397a55438fd1
Caleb Maclennan <caleb@alerque.com>
2019-04-23 17:17:37 +0300

    [backends] Roughly approximate line-by-line output in text backend

--- core/text-output.lua
+++ core/text-output.lua
@@ -1,11 +1,17 @@
 if (not SILE.outputters) then SILE.outputters = {} end
 
 local outfile
+local cursorX = 0
+local cursorY = 0
+local hboxCount = 0
+
 local writeline = function (...)
   local args = table.pack(...)
+  if hboxCount >= 1 then outfile:write(" ") end
   for i=1, #args do
     outfile:write(args[i])
     if i < #args then outfile:write(" ") end
+    hboxCount = hboxCount + 1
   end
 end
 
@@ -28,7 +34,14 @@ SILE.outputters.text = {
   setFont = function () end,
   drawImage = function () end,
   imageSize = function () end,
-  moveTo = function () end,
+  moveTo = function (x, y)
+    if y > cursorY or x <= cursorX then
+      outfile:write("\n")
+      hboxCount = 0
+    end
+    cursorY = y
+    cursorX = x
+  end,
   rule = function () end,
   debugFrame = function (self) end,
   debugHbox = function() end
