b28bf3557d9bee64face0ec37d19803e8b73a5a4
Simon Cozens <simon@simon-cozens.org>
2019-05-16 22:15:37 +0100

    docs: Document all the packages
    Additionally, make autodoc scream if there is no docs.

--- packages/autodoc.lua
+++ packages/autodoc.lua
@@ -1,6 +1,15 @@
 SILE.registerCommand("package-documentation", function (o,c)
   local package = SU.required(o, "src", "src for package documentation")
   io.stderr:write("<"..package..">")
+  local exports = require(package)
+  if type(exports) ~= "table" then
+    SU.error("Undocumented package "..package)
+  end
+  local doc = exports.documentation
+  if not doc then
+    SU.error("Undocumented package "..package)
+  end
+
   SILE.process(
     SILE.inputs.TeXlike.docToTree(
       require(package).documentation
