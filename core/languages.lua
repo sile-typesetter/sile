b8c8a04364b273ec9a1bc04ed4725abe3a4ee54c
Caleb Maclennan <caleb@alerque.com>
2019-10-12 20:08:13 +0300

    chore(languages): Validate languages against possible CLDR locales

--- core/languages.lua
+++ core/languages.lua
@@ -4,6 +4,7 @@ SILE.languageSupport = {
     if SILE.languageSupport.languages[language] then return end
     if SILE.hyphenator.languages[language] then return end
     if not(language) or language == "" then language = "en" end
+    language = SILE.cldr.locales[language] and language or "und"
     ok, fail = pcall(function () SILE.require("languages/" .. language) end)
     if fail then
       if fail:match("not found") then fail = "no support for this language" end
