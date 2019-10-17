17c6aee5c34d7825dcf56f272ab797caf99f7d6b
Caleb Maclennan <caleb@alerque.com>
2019-10-17 12:58:22 +0300

    refactor(core): Avoid shadowing variables

--- core/tracestack.lua
+++ core/tracestack.lua
@@ -51,7 +51,7 @@ function traceStack:pushDocument(file, sniff, _)
     sniff = sniff
   }
   setmetatable(frame, {
-      __tostring = function(self) return "<file> (" .. self.sniff .. ")" end,
+      __tostring = function(this) return "<file> (" .. this.sniff .. ")" end,
     })
   return self:pushFrame(frame)
 end
@@ -72,9 +72,9 @@ function traceStack:pushCommand(command, content, options)
     options = options or {}
   }
   setmetatable(frame, {
-    __tostring = function(self)
-      local options = (table.nitems(self.options) > 0 and tostring(self.options):gsub("^{", "["):gsub("}$", "]") or "")
-      return "\\" .. self.command .. options
+    __tostring = function(this)
+      local opts = (table.nitems(this.options) > 0 and tostring(this.options):gsub("^{", "["):gsub("}$", "]") or "")
+      return "\\" .. this.command .. opts
     end
   })
   return self:pushFrame(frame)
@@ -99,9 +99,9 @@ function traceStack:pushContent(content, command)
       options = content.options or {}
     }
   setmetatable(frame, {
-    __tostring = function(self)
-      local options = (table.nitems(self.options) > 0 and tostring(self.options):gsub("^{", "["):gsub("}$", "]") or "")
-      return "\\" .. self.command .. options
+    __tostring = function(this)
+      local options = (table.nitems(this.options) > 0 and tostring(this.options):gsub("^{", "["):gsub("}$", "]") or "")
+      return "\\" .. this.command .. options
     end
   })
   return self:pushFrame(frame)
@@ -114,13 +114,12 @@ function traceStack:pushText(text)
     text = text
   }
   setmetatable(frame, {
-    __tostring = function(self)
-      local text = self.text
-      if text:len() > 20 then
-        text = text:sub(1, 18) .. "…"
+    __tostring = function(this)
+      if this.text:len() > 20 then
+        this.text = this.text:sub(1, 18) .. "…"
       end
-      text = text:gsub("\n", "␤"):gsub("\t", "␉"):gsub("\v", "␋")
-      return '"' .. text .. '"'
+      this.text = this.text:gsub("\n", "␤"):gsub("\t", "␉"):gsub("\v", "␋")
+      return '"' .. this.text .. '"'
     end
   })
   return self:pushFrame(frame)
