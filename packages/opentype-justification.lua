7523dd67256f69542b25bd52860041fc1f19e489
Simon Cozens <simon@simon-cozens.org>
2020-01-13 12:44:04 +0000

    improvement(packages): Implement JSTF table substitutions
    This includes the world’s worst OpenType shaping engine.

--- packages/opentype-justification.lua
+++ packages/opentype-justification.lua
@@ -1,13 +1,23 @@
 local oldMakeSpaceNode = SILE.shaper.makeSpaceNode
 local ot = SILE.require("core/opentype-parser")
+local metrics = require("fontmetrics")
+
+local fontSuggestionsCache = {}
+
+local _key = function (options)
+  return table.concat({ options.family;("%g"):format(options.size);("%d"):format(options.weight);options.style;options.variant;options.features;options.direction;options.filename }, ";")
+end
 
 local getSuggestions = function (options)
   local face = SILE.font.cache(options, SILE.shaper.getFace)
+  key = _key(options)
+  if fontSuggestionsCache[key] then return fontSuggestionsCache[key] end
   local font = ot.parseFont(face)
   if not font.jstf then return nil end
   local scriptRecord = font.jstf[options.script] or font.jstf["DFLT"]
   if not scriptRecord then return nil end
   local suggestions = scriptRecord.languages[options.language] or scriptRecord.defaultJstfLangSysTable
+  fontSuggestionsCache[key] = suggestions
   return suggestions
 end
 
@@ -34,7 +44,11 @@ local getSpaceWidth = function(options)
   if options.spaceWidth then return options.spaceWidth end
   local suggestions = getSuggestions(options)
   local upem = getUpem(options)
-  if not suggestions then return oldMakeSpaceNode(self,options,item) end
+  if not suggestions then
+    spaceNode = oldMakeSpaceNode(self,options,item)
+    SU.debug("opentype-justification", "No  "..options.spaceWidth)
+
+  end
   local items, width = SILE.shaper:shapeToken(" ", options)
   local gid = items[1].gid
   local widthU = math.floor(items[1].width * upem / options.size)
@@ -77,4 +91,92 @@ SILE.shaper.makeSpaceNode = function (self, options, item)
   else
     return oldMakeSpaceNode(self,options,item)
   end
+end
+
+local nnodeMaker = SILE.shaper.createNnodes
+
+local function mapUsedGIDs(node)
+  usedGids = {}
+  for _, subnode in ipairs(node.nodes) do
+    for _, gid in ipairs(subnode.value.glyphString) do
+      usedGids[gid] = true
+    end
+  end
+  return usedGids
+end
+
+local function lookupsApply(node, lookups, options)
+  if not node:isNnode() then return {} end
+  local gidsUsed = mapUsedGIDs(node)
+  local face = SILE.font.cache(options, SILE.shaper.getFace)
+  local font = ot.parseFont(face)
+  local lookupsWhichApply = {}
+  for _, lid in ipairs(lookups) do
+    local lookup = font.gsub[lid]
+    for src,dst in pairs(lookup) do
+      if gidsUsed[src] then
+        SU.debug("opentype-justification", "Node contains ",src," which is in lookup ",lid)
+        lookupsWhichApply[#lookupsWhichApply+1] = lookup
+      end
+    end
+  end
+  return lookupsWhichApply
+end
+
+local applyLookups = function (lookups, node, face, options)
+  node2 = std.tree.clone(node)
+  widthDiff = SILE.length.new({})
+  for _, subnode in ipairs(node2.nodes) do
+    if subnode.value.items then
+      subnode.value.items = SU.map(function (item)
+        local oldWidth = item.width
+        for _,l in ipairs(lookups) do
+          if l[item.gid] then item.gid = l[item.gid] end
+        end
+        item.width = metrics.glyphwidth(item.gid, face.data, face.index)  * options.size
+        local thisWidthDiff = (item.width - oldWidth)
+        item.glyphAdvance = item.glyphAdvance + thisWidthDiff
+        widthDiff = widthDiff + thisWidthDiff
+        subnode.width = subnode.width + thisWidthDiff
+        return item
+      end, subnode.value.items)
+    else
+      subnode.value.glyphString = SU.map(function (ingid)
+        local oldWidth = metrics.glyphwidth(ingid, face.data, face.index)  * options.size
+        for _,l in ipairs(lookups) do
+          if l[ingid] then
+            local newWidth = metrics.glyphwidth(l[ingid], face.data, face.index)  * options.size
+            widthDiff = widthDiff + (newWidth - oldWidth)
+            subnode.width = subnode.width + (newWidth - oldWidth)
+            return l[ingid]
+          end
+        end
+        return ingid
+      end, subnode.value.glyphString)
+    end
+  end
+  node2.width = node2.width + widthDiff
+  node2.penalty = 500
+  return SILE.nodefactory.newAlternative({
+    options = {node, node2},
+    selected = 1
+  })
+end
+
+SILE.shaper.createNnodes = function (self,text, options)
+  local face = SILE.font.cache(options, SILE.shaper.getFace)
+  local suggestions = getSuggestions(options)
+  local nnodes = nnodeMaker(self,text,options)
+  if not suggestions then return nnodes end
+  for _, suggestion in pairs(suggestions) do
+    if #suggestion.extensionEnableGSUB > 0 then
+      for i, node in pairs(nnodes) do
+        local lookupsWhichApply = lookupsApply(node, suggestion.extensionEnableGSUB, options)
+        if #lookupsWhichApply > 0 then
+          nnodes[i] = applyLookups(lookupsWhichApply, node, face, options)
+        end
+      end
+    end
+  end
+  return nnodes
 end
