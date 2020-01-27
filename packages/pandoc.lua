2fa87b46c640acf6400f4fd53fa41e71b31d1d31
Caleb Maclennan <caleb@alerque.com>
2020-01-27 23:29:49 +0300

    chore(packages): Sketch out functions for all Pandoc inline types

--- packages/pandoc.lua
+++ packages/pandoc.lua
@@ -151,8 +151,91 @@ end)
 
 -- Inlines
 
--- -\define[command=listitem]{\smallskip{}\glue[width=-1em]• \glue[width=0.3em]\process\smallskip}%
+SILE.registerCommand("Cite", function (options, content)
+  -- TODO: options is citation list?
+end, "Creates a Cite inline element")
 
+SILE.registerCommand("Code", function (options, content)
+  local wrapper, args = handlePandocArgs(options)
+  wrapper(function ()
+    SILE.call("code", args, content)
+  end)
+end, "Creates a Code inline element")
+
+SILE.registerCommand("Emph", function (_, content)
+  SILE.call("em", {}, content)
+end, "Creates an inline element representing emphasised text.")
+
+SILE.registerCommand("Image", function (options, _)
+  local wrapper, args = handlePandocArgs(options)
+  wrapper(function ()
+    SILE.call("img", args)
+  end)
+end, "Creates a Image inline element")
+
+SILE.registerCommand("LineBreak", function (_, _)
+  SILE.call("break")
+end, "Create a LineBreak inline element")
+
+SILE.registerCommand("Link", function (options, content)
+  local wrapper, args = handlePandocArgs(options)
+  wrapper(function ()
+    SILE.call("url", args, content)
+  end)
+end, "Creates a link inline element, usually a hyperlink.")
+
+SILE.registerCommand("Math", function (options, content)
+  -- TODO options is math type
+  SILE.process("content")
+end, "Creates a Math element, either inline or displayed.")
+
+SILE.registerCommand("Note", function (_, content)
+  SILE.call("footnote", {}, content)
+end, "Creates a Note inline element")
+
+SILE.registerCommand("Quoted", function (options, content)
+  -- TODO: options.type
+  SILE.process(content)
+end, "Creates a Quoted inline element given the quote type and quoted content.")
+
+SILE.registerCommand("RawInline", function (options, content)
+  local format = options.format
+  -- TODO: execute as script? pass to different input parser?
+  SILE.process(content)
+end, "Creates a Quoted inline element given the quote type and quoted content.")
+
+SILE.registerCommand("SmallCaps", function (_, content)
+  SILE.call("font", { features = "+smcp" }, content)
+end, "Creates text rendered in small caps")
+
+SILE.registerCommand("Span", function (options, content)
+  handlePandocArgs(options)(content)
+end, "Creates a Span inline element")
+
+SILE.registerCommand("Strikeout", function (_, content)
+  -- TODO: cross it out, unicode munging?
+  SILE.process(content)
+end, "Creates text which is striked out.")
+
+SILE.registerCommand("Strong", function (_, content)
+  SILE.call("strong", {}, content)
+end, "Creates a Strong element, whose text is usually displayed in a bold font.")
+
+local scriptOffset = "0.7ex"
+local scriptSize = "1.5ex"
+
+SILE.registerCommand("Subscript", function (_, content)
+  SILE.call("lower", { height = scriptOffset }, function ()
+    SILE.call("font", { size = scriptSize }, content)
+  end)
+end, "Creates a Subscript inline element")
+
+SILE.registerCommand("Superscript", function (_, content)
+  SILE.call("raise", { height = scriptOffset }, function ()
+    SILE.call("font", { size = scriptSize }, content)
+  end)
+end, "Creates a Superscript inline element")
+-- -\define[command=listitem]{\smallskip{}\glue[width=-1em]• \glue[width=0.3em]\process\smallskip}%
 
 -- Needs refactoring
 
@@ -168,22 +251,6 @@ SILE.registerCommand("tt", function (options, content)
   SILE.call("verbatim:font", options, content)
 end)
 
-SILE.registerCommand("Span", function (options, content)
-  handlePandocArgs(options)(content)
-end,"Generic inline wrapper")
-
-SILE.registerCommand("Emph", function (_, content)
-  SILE.call("em", {}, content)
-end,"Inline emphasis wrapper")
-
-SILE.registerCommand("Strong", function (_, content)
-  SILE.call("strong", {}, content)
-end,"Inline strong wrapper")
-
-SILE.registerCommand("SmallCaps", function (_, content)
-  SILE.call("font", { features = "+smcp" }, content)
-end,"Inline small caps wrapper")
-
 SILE.registerCommand("csl-no-emph", function (_, content)
   SILE.call("font", { style = "Roman" }, content)
 end,"Inline upright wrapper")
@@ -195,21 +262,6 @@ end,"Inline normal weight wrapper")
 SILE.registerCommand("csl-no-smallcaps", function (_, content)
   SILE.call("font", { features = "-smcp" }, content)
 end,"Inline smallcaps disable wrapper")
-
-local scriptOffset = "0.7ex"
-local scriptSize = "1.5ex"
-
-SILE.registerCommand("Superscript", function (_, content)
-  SILE.call("raise", { height = scriptOffset }, function ()
-    SILE.call("font", { size = scriptSize }, content)
-  end)
-end,"Inline superscript wrapper")
-
-SILE.registerCommand("Subscript", function (_, content)
-  SILE.call("lower", { height = scriptOffset }, function ()
-    SILE.call("font", { size = scriptSize }, content)
-  end)
-end,"Inline subscript wrapper")
 
 SILE.registerCommand("unimplemented", function (_, content)
   SU.debug("pandoc", "Un-implemented function")
