5abae62f7c5237ae403ceb7e455dd0a9abc7478c
Caleb Maclennan <caleb@alerque.com>
2020-07-13 11:45:25 +0300

    test(inputs): Add spec for testing TeXlike input parsing

---
+++ spec/texlike_spec.lua
@@ -0,0 +1,38 @@
+SILE = require("core/sile")
+
+describe("#TeXlike #input", function ()
+  local toTree = SILE.inputs.TeXlike.docToTree
+
+  it("command with content", function()
+    local t = toTree([[\foo{bar}]])
+    assert.is.equal("foo", t.command)
+    assert.is.equal("bar", t[1][1])
+  end)
+
+  it("command with arg", function()
+    local t = toTree([[\foo[baz=qiz]{bar}]])
+    assert.is.equal("qiz", t.options.baz)
+    assert.is.equal("bar", t[1][1])
+  end)
+
+  it("command with multiple args", function()
+    local t = toTree([[\foo[baz=qiz,qiz=baz]{bar}]])
+    assert.is.equal("qiz", t.options.baz)
+    assert.is.equal("baz", t.options.qiz)
+    assert.is.equal("bar", t[1][1])
+  end)
+
+  it("command with quoted arg", function()
+    local t = toTree([[\foo[baz="qiz qiz"]{bar}]])
+    assert.is.equal("qiz qiz", t.options.baz)
+    assert.is.equal("bar", t[1][1])
+  end)
+
+  it("command with multiple quoted args", function()
+    local t = toTree([[\foo[baz="qiz, qiz",qiz="baz, baz"]{bar}]])
+    assert.is.equal("qiz, qiz", t.options.baz)
+    assert.is.equal("baz, baz", t.options.qiz)
+    assert.is.equal("bar", t[1][1])
+  end)
+
+end)
