f626867221fa57af3a3d90ad87e0bc0c80581d71
Caleb Maclennan <caleb@alerque.com>
2020-07-16 15:32:42 +0300

    fix(build): Move dynamically generated file lists out of automake

---
+++ build-aux/list-dist-files.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+echo -n "nobase_dist_pkgdata_DATA ="
+find core classes languages packages lua-libraries -type f -name '*.lua' -printf ' %p'
+find classes -type f -name '*.sil' -printf ' %p'
+
+echo -ne "\nextra_dist ="
+find lua_modules_dist -type f -printf ' %p' ||:
+
+echo -e "\nif !SYSTEM_LUAROCKS"
+echo -n "nobase_nodist_pkgdata_DATA ="
+find lua_modules -type f -printf ' %p' ||:
+echo -ne "\nendif"
+
+echo -ne "\nTESTSRCS ?="
+find tests -maxdepth 1 -type f -name '*.sil' -printf ' %p'
+find tests -maxdepth 1 -type f -name '*.xml' -printf ' %p'
+
+echo -ne "\nTESTEXPECTS ?="
+find tests -maxdepth 1 -type f -name '*.expected' -printf ' %p'
+
+echo -ne "\nEXAMPLESSRCS ="
+find examples -maxdepth 1 -type f -name '*.sil' -printf ' %p'
+find examples/docbook -maxdepth 1 -type f -name '*.xml' -printf ' %p'
