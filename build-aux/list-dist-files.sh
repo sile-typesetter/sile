c96683ef5d015ccc540214acb8b20a86f8e0ae78
Caleb Maclennan <caleb@alerque.com>
2020-07-18 15:26:00 +0300

    fix(build): Use BSD compatible find syntax

--- build-aux/list-dist-files.sh
+++ build-aux/list-dist-files.sh
@@ -1,24 +1,31 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail
 
-echo -n "nobase_dist_pkgdata_DATA ="
-find core classes languages packages lua-libraries -type f -name '*.lua' -printf ' %p'
-find classes -type f -name '*.sil' -printf ' %p'
+echo -n "nobase_dist_pkgdata_DATA = "
+{
+find core classes languages packages lua-libraries -type f -name '*.lua'
+find classes -type f -name '*.sil'
+} | xargs
 
-echo -ne "\nLUAMODULES ="
-find lua_modules -type f -not -name '*~' -printf ' %p' ||:
+echo -ne "\nLUAMODULES = "
+find lua_modules -type f ! -name '*~' | xargs ||:
 
-echo -ne "\nLUAMODULESDIST ="
-find lua_modules_dist -type f -not -name '*~' -printf ' %p' ||:
+echo -ne "\nLUAMODULESDIST = "
+find lua_modules_dist -type f ! -name '*~' | xargs ||:
 
-echo -ne "\nTESTSRCS ?="
-find tests -maxdepth 1 -type f -name '*.sil' -printf ' %p'
-find tests -maxdepth 1 -type f -name '*.xml' -printf ' %p'
+echo -ne "\nTESTSRCS ?= "
+{
+find tests -maxdepth 1 -type f -name '*.sil'
+find tests -maxdepth 1 -type f -name '*.xml'
+} | xargs
 
-echo -ne "\nTESTEXPECTS ?="
-find tests -maxdepth 1 -type f -name '*.expected' -printf ' %p'
+echo -ne "\nTESTEXPECTS ?= "
+find tests -maxdepth 1 -type f -name '*.expected' | xargs
 
-echo -ne "\nEXAMPLESSRCS ="
-find examples -maxdepth 1 -type f -name '*.sil' -printf ' %p'
-find examples/docbook -maxdepth 1 -type f -name '*.xml' -printf ' %p'
+echo -ne "\nEXAMPLESSRCS = "
+{
+find examples -maxdepth 1 -type f -name '*.sil'
+find examples/docbook -maxdepth 1 -type f -name '*.xml'
+} | xargs
