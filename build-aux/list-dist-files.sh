5e885fc2a016edf98ed8d0e223b7e4d064044209
Caleb Maclennan <caleb@alerque.com>
2020-07-20 18:18:43 +0300

    chore(build): Normalize sort between coreutils versions

--- build-aux/list-dist-files.sh
+++ build-aux/list-dist-files.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 finder () {
     echo -n ' '
-    find $@ -type f | sort | xargs echo -n ||:
+    find $@ -type f | sort -bdi | xargs echo -n ||:
 }
 
 echo -n "nobase_dist_pkgdata_DATA = \$(LUALIBRARIES)"
