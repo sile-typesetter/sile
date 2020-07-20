11efae726b307dcd63ebe99b2ba36a0cc5b512e5
Caleb Maclennan <caleb@alerque.com>
2020-07-20 13:18:01 +0300

    chore(build): Split massive file lists into chunks

--- build-aux/list-dist-files.sh
+++ build-aux/list-dist-files.sh
@@ -8,15 +8,22 @@ finder () {
     find $@ -type f | sort | xargs echo -n ||:
 }
 
-echo -n "nobase_dist_pkgdata_DATA ="
-finder core classes languages packages lua-libraries -name '*.lua'
+echo -n "nobase_dist_pkgdata_DATA = \$(LUALIBRARIES)"
+finder core classes languages packages -name '*.lua'
 finder classes -name '*.sil'
+echo -ne "\nLUALIBRARIES ="
+finder lua-libraries -name '*.lua'
 
 echo -ne "\nLUAMODULES ="
 finder lua_modules ! -name "'*~'"
 
-echo -ne "\nLUAMODULESDIST ="
-finder lua_modules_dist ! -name "'*~'"
+echo -ne "\nLUAMODULESDIST = \$(LUAMODULESDIST51) \$(LUAMODULESDIST52) \$(LUAMODULESDIST53)"
+echo -ne "\nLUAMODULESDIST51 ="
+finder lua_modules_dist -path '*5.1/*' ! -name "'*~'"
+echo -ne "\nLUAMODULESDIST52 ="
+finder lua_modules_dist -path '*5.2/*' ! -name "'*~'"
+echo -ne "\nLUAMODULESDIST53 ="
+finder lua_modules_dist -path '*5.3/*' ! -name "'*~'"
 
 echo -ne "\nTESTSRCS ?="
 finder tests -maxdepth 1 -name '*.sil'
