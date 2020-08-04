9be38de432c2c8337eee73958608db1583a8f08e
Caleb Maclennan <caleb@alerque.com>
2020-08-04 18:59:54 +0300

    style(backends): Normalize whitespace across backends for easier diffing

--- core/dummy-output.lua
+++ core/dummy-output.lua
@@ -3,21 +3,37 @@ if (not SILE.outputters) then SILE.outputters = {} end
 local dummy = function() end
 
 SILE.outputters.dummy = {
+
   init = dummy,
+
   newPage = dummy,
+
   finish = dummy,
+
   setColor = dummy,
+
   pushColor = dummy,
+
   popColor = dummy,
+
   outputHbox = dummy,
+
   setFont = dummy,
+
   drawImage = dummy,
+
   drawSVG = dummy,
+
   imageSize = dummy,
+
   moveTo = dummy,
+
   rule = dummy,
+
   debugFrame = dummy,
+
   debugHbox = dummy
+
 }
 
 SILE.outputter = SILE.outputters.dummy
