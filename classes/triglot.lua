495654ac5d180af09ba9e71461ab78a6af43a1dc
Caleb Maclennan <caleb@alerque.com>
2020-09-20 15:47:56 +0300

    fix(classes): Identify triglot class as triglot not diglot

--- classes/triglot.lua
+++ classes/triglot.lua
@@ -1,14 +1,14 @@
 local plain = SILE.require("book", "classes")
-local diglot = pl.tablex.deepcopy(plain)
+local triglot = pl.tablex.deepcopy(plain)
 SILE.require("packages/counters")
 SILE.scratch.counters.folio = { value = 1, display = "arabic" }
-diglot:declareFrame("a",    {left = "5%pw",            right = "28%pw",            top = "11.6%ph",       bottom = "80%ph"        })
-diglot:declareFrame("b",    {left = "33%pw",             right = "60%pw", top = "top(a)",      bottom = "bottom(a)"    })
-diglot:declareFrame("c",    {left = "66%pw",             right = "95%pw", top = "top(a)",      bottom = "bottom(a)"    })
-diglot:declareFrame("folio",{left = "left(a)",         right = "right(b)",       top = "bottom(a)+3%pw",bottom = "bottom(a)+8%ph" })
+triglot:declareFrame("a",    {left = "5%pw",            right = "28%pw",            top = "11.6%ph",       bottom = "80%ph"        })
+triglot:declareFrame("b",    {left = "33%pw",             right = "60%pw", top = "top(a)",      bottom = "bottom(a)"    })
+triglot:declareFrame("c",    {left = "66%pw",             right = "95%pw", top = "top(a)",      bottom = "bottom(a)"    })
+triglot:declareFrame("folio",{left = "left(a)",         right = "right(b)",       top = "bottom(a)+3%pw",bottom = "bottom(a)+8%ph" })
 
-diglot:loadPackage("parallel", { frames = { left = "a", middle = "b", right= "c" } })
+triglot:loadPackage("parallel", { frames = { left = "a", middle = "b", right= "c" } })
 SILE.settings.set("linebreak.tolerance", 5000)
 SILE.settings.set("document.parindent", SILE.nodefactory.glue())
 
-return diglot
+return triglot
