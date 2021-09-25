0a88948e5028e4f3b82ad7ee689d5f53e0358ddd
Caleb Maclennan <caleb@alerque.com>
2021-09-25 14:49:07 +0300

    feat(packages): Add shift, raise, and size options to dropcaps

--- packages/dropcaps.lua
+++ packages/dropcaps.lua
@@ -3,7 +3,7 @@ SILE.require("packages/raiselower")
 
 local shapeHbox = function (options, content)
   -- Clear irrelevant values before passing to font
-  options.lines, options.join = nil, nil
+  options.lines, options.join, options.raise, options.shift = nil, nil, nil, nil
   SILE.call("noindent")
   local hbox = SILE.call("hbox", {}, function ()
     SILE.call("font", options, content)
@@ -17,29 +17,32 @@ end
 SILE.registerCommand("dropcap", function (options, content)
   local lines = SU.cast("integer", options.lines or 3)
   local join = SU.boolean(options.join, false)
+  local raise = SU.cast("measurement", options.raise or 0)
+  local shift = SU.cast("measurement", options.shift or 0)
+  local size = SU.cast("measurement or nil", options.size or nil)
+  options.size = nil -- we need to measure the "would have been" size before using this
 
   -- We want the drop cap to spanning N lines is N-1 baselineskip plus the height of the first line.
   -- We Define the height of the first line based on measuring the size the initial would have been.
   local tmpHbox = shapeHbox(options, content)
-  local extraHeight = SILE.length((lines - 1).."bs"):tonumber()
+  local extraHeight = SILE.measurement((lines - 1).."bs"):tonumber()
   local targetHeight = tmpHbox.height:tonumber() + extraHeight
   SU.debug("dropcaps", "Target height", targetHeight)
 
   -- Now we need to figure out how to scale the dropcap font to get an initial of targetHeight.
   -- From that we can also figure out the width it will be at that height.
-  local curSize = SILE.length(SILE.settings.get("font.size")):tonumber()
+  local curSize = SILE.measurement(SILE.settings.get("font.size")):tonumber()
   local curHeight, curWidth = tmpHbox.height:tonumber(), tmpHbox.width:tonumber()
-  local targetSize = targetHeight / curHeight * curSize
-  local targetWidth = curWidth / curSize * targetSize
-  SU.debug("dropcaps", "Target font size", targetSize)
+  options.size = size and size:tonumber() or (targetHeight / curHeight * curSize)
+  local targetWidth = curWidth / curSize * options.size
+  SU.debug("dropcaps", "Target font size", options.size)
   SU.debug("dropcaps", "Target width", targetWidth)
 
   -- Typeset the dropcap with its final shape, but don't output it yet
-  options.size = targetSize
   local hbox = shapeHbox(options, content)
 
   -- Setup up the necessary indents for the final paragraph content
-  local joinOffset = SILE.length(join and "1spc" or 0):tonumber()
+  local joinOffset = SILE.measurement(join and "1spc" or 0):tonumber()
   SILE.settings.set("linebreak.hangAfter", -lines)
   SILE.settings.set("linebreak.hangIndent", targetWidth + joinOffset)
   SU.debug("dropcaps", "joinOffset", joinOffset)
@@ -47,8 +50,8 @@ SILE.registerCommand("dropcap", function (options, content)
   -- The paragraph is indented so as to leave enough space for the drop cap.
   -- We "trick" the typesetter with a zero-dimension box wrapping our original box.
   SILE.call("rebox", { height = 0, width = -joinOffset }, function ()
-    SILE.call("glue", { width = -(targetWidth + joinOffset) })
-    SILE.call("lower", { height = extraHeight }, function ()
+    SILE.call("glue", { width = shift - targetWidth - joinOffset })
+    SILE.call("lower", { height = extraHeight - raise }, function ()
       SILE.typesetter:pushHbox(hbox)
     end)
   end)
@@ -60,8 +63,12 @@ return {
 The \code{dropcaps} package allows you to format paragraphs with an 'initial capital' (also commonly
 referred as a 'drop cap'), typically one large capital letter used as a decorative element at the beginning of a paragraph.
 
-It provides the \code{\\dropcap} command, which takes as an option the number of lines the initial content should span.
+It provides the \code{\\dropcap} command.
 The content passed will be the initial character(s).
+The primary option is \code{lines}, an integer specifying the number of lines to span.
+The default value is 3.
+The \code{join} is a boolean for whether to join the dropcap to the first line, defaults to false.
+To tweak the position of the dropcap, measurements may be passed to the \code{raise} and \code{shift} options.
 Old-style typographers also have the possibility to enable, at their convenience, the join option,
 for use when the initial belongs to the first word of the sentence.
 In that case, the first line is closer to the initial than subsequent indented lines.
