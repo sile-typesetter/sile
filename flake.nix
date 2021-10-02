0cf86a7599e20f573000addbc21274b053187ff7
Doron Behar <doron.behar@gmail.com>
2021-10-02 19:49:46 +0000

    chore(nix): Ignore many files when creating src (#1255)

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   # To make user overrides of the nixpkgs flake not take effect
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gitignore = {
+    url = "github:hercules-ci/gitignore.nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
 
   # https://nixos.wiki/wiki/Flakes#Using_flakes_project_from_a_legacy_Nix
   inputs.flake-compat = {
@@ -15,6 +19,7 @@
     , nixpkgs
     , flake-utils
     , flake-compat
+    , gitignore
   }:
   flake-utils.lib.eachDefaultSystem (system:
     let
@@ -28,14 +33,41 @@
         url = "https://github.com/sile-typesetter/libtexpdf";
         rev = "${(pkgs.lib.fileContents "${self}/libtexpdf.git-rev")}";
       };
+      inherit (gitignore.lib) gitignoreSource;
       # https://discourse.nixos.org/t/passing-git-commit-hash-and-tag-to-build-with-flakes/11355/2
       version_rev = if (self ? rev) then (builtins.substring 0 8 self.rev) else "dirty";
       # Use the expression from Nixpkgs instead of rewriting it here.
       sile = pkgs.sile.overrideAttrs(oldAttr: rec {
         version = "${(pkgs.lib.importJSON ./package.json).version}-${version_rev}-flake";
-        src = builtins.filterSource
-          (path: type: type != "directory" || baseNameOf path != ".git")
-        ./.;
+        src = pkgs.lib.cleanSourceWith {
+          # Ignore many files that gitignoreSource doesn't ignore, see:
+          # https://github.com/hercules-ci/gitignore.nix/issues/9#issuecomment-635458762
+          filter = path: type:
+          ! (builtins.any (r: (builtins.match r (builtins.baseNameOf path)) != null) [
+            # Nix files
+            "flake.nix"
+            "flake.lock"
+            "default.nix"
+            "shell.nix"
+            # git commit and editing format files
+            ".commitlintrc.yml"
+            "package.json"
+            ".husky"
+            ".editorconfig"
+            # CI files
+            ".cirrus.yml"
+            "action.yml"
+            "azure-pipelines.yml"
+            "Dockerfile"
+            # Git files
+            ".github"
+            ".gitattributes"
+            ".gitignore"
+            ".git"
+          ])
+          ;
+          src = gitignoreSource ./.;
+        };
         # Add the libtexpdf src instead of the git submodule.
         preAutoreconf = ''
           rm -rf ./libtexpdf
