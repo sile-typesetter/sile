4822902fdf0cf8b075dc41a10906fd3f84823612
Caleb Maclennan <caleb@alerque.com>
2022-01-28 12:30:10 +0300

    chore(tooling): Enable direct invocation of busted with limit tests

--- spec/opentype_spec.lua
+++ spec/opentype_spec.lua
@@ -2,6 +2,14 @@ SILE = require("core/sile")
 SILE.backend = "debug"
 SILE.init()
 
+-- These tests depend on loading specific fonts from our test fixtures. Running
+-- plain `busted` is sometimes useful (e.g. for IDEs) but will not support this
+-- test because fontconfig hasn't been preloaded with the font paths it would
+-- need. If that's the case, just skip even defining these tests and call it
+-- good. To test a complete set of tests use `make busted`.
+local fcf = os.getenv("FONTCONFIG_FILE")
+if not fcf then return end
+
 describe("The OpenType loader/parser", function()
   local ot = SILE.require("core/opentype-parser")
   local face = SILE.shaper:getFace({ family = "Libertinus Serif" })
