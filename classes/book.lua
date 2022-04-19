963e39958a68e6a268165523473b040388ecbf3d
Caleb Maclennan <caleb@alerque.com>
2022-04-19 18:19:05 +0300

    chore(classes): Redo book class with Penlight class model

--- classes/book.lua
+++ classes/book.lua
@@ -1,5 +1,6 @@
 local plain = SILE.require("plain", "classes")
-local book = plain { id = "book" }
+
+local book = pl.class(plain)
 
 local counters = SILE.require("packages/counters").exports
 
@@ -30,7 +31,8 @@ book.defaultFrameset = {
   }
 }
 
-function book:init ()
+function book:_init ()
+  plain._init(self)
   self:loadPackage("masters")
   self:defineMaster({
       id = "right",
@@ -45,22 +47,22 @@ function book:init ()
     insertInto = "footnotes",
     stealFrom = { "content" }
   })
-  return plain.init(self)
+  return self
 end
 
-book.newPage = function (self)
+function book:newPage ()
   self:switchPage()
   self:newPageInfo()
   return plain.newPage(self)
 end
 
-book.finish = function (self)
+function book:finish ()
   local ret = plain.finish(self)
   self:writeToc()
   return ret
 end
 
-book.endPage = function (self)
+function book:endPage ()
   self:moveTocNodes()
   if (self:oddPage() and SILE.scratch.headers.right) then
     SILE.typesetNaturally(SILE.getFrame("runningHead"), function ()
@@ -86,46 +88,47 @@ book.endPage = function (self)
   return plain.endPage(self)
 end
 
-SILE.registerCommand("left-running-head", function (_, content)
-  local closure = SILE.settings.wrap()
-  SILE.scratch.headers.left = function () closure(content) end
-end, "Text to appear on the top of the left page")
-
-SILE.registerCommand("right-running-head", function (_, content)
-  local closure = SILE.settings.wrap()
-  SILE.scratch.headers.right = function () closure(content) end
-end, "Text to appear on the top of the right page")
-
-SILE.registerCommand("book:sectioning", function (options, content)
-  local level = SU.required(options, "level", "book:sectioning")
-  local number
-  if SU.boolean(options.numbering, true) then
-    SILE.call("increment-multilevel-counter", { id = "sectioning", level = level })
-    number = SILE.formatMultilevelCounter(counters.getMultilevelCounter("sectioning"))
-  end
-  if SU.boolean(options.toc, true) then
-    SILE.call("tocentry", { level = level, number = number }, SU.subContent(content))
-  end
-  local lang = SILE.settings.get("document.language")
-  if SU.boolean(options.numbering, true) then
-    if options.prenumber then
-      if SILE.Commands[options.prenumber .. ":"  .. lang] then
-        options.prenumber = options.prenumber .. ":" .. lang
-      end
-      SILE.call(options.prenumber)
-    end
-    SILE.call("show-multilevel-counter", { id = "sectioning" })
-    if options.postnumber then
-      if SILE.Commands[options.postnumber .. ":" .. lang] then
-        options.postnumber = options.postnumber .. ":" .. lang
-      end
-      SILE.call(options.postnumber)
-    end
-  end
-end)
-
 book.registerCommands = function (_)
+
   plain.registerCommands()
+
+  SILE.registerCommand("left-running-head", function (_, content)
+    local closure = SILE.settings.wrap()
+    SILE.scratch.headers.left = function () closure(content) end
+  end, "Text to appear on the top of the left page")
+
+  SILE.registerCommand("right-running-head", function (_, content)
+    local closure = SILE.settings.wrap()
+    SILE.scratch.headers.right = function () closure(content) end
+  end, "Text to appear on the top of the right page")
+
+  SILE.registerCommand("book:sectioning", function (options, content)
+    local level = SU.required(options, "level", "book:sectioning")
+    local number
+    if SU.boolean(options.numbering, true) then
+      SILE.call("increment-multilevel-counter", { id = "sectioning", level = level })
+      number = SILE.formatMultilevelCounter(counters.getMultilevelCounter("sectioning"))
+    end
+    if SU.boolean(options.toc, true) then
+      SILE.call("tocentry", { level = level, number = number }, SU.subContent(content))
+    end
+    local lang = SILE.settings.get("document.language")
+    if SU.boolean(options.numbering, true) then
+      if options.prenumber then
+        if SILE.Commands[options.prenumber .. ":"  .. lang] then
+          options.prenumber = options.prenumber .. ":" .. lang
+        end
+        SILE.call(options.prenumber)
+      end
+      SILE.call("show-multilevel-counter", { id = "sectioning" })
+      if options.postnumber then
+        if SILE.Commands[options.postnumber .. ":" .. lang] then
+          options.postnumber = options.postnumber .. ":" .. lang
+        end
+        SILE.call(options.postnumber)
+      end
+    end
+  end)
 
   SILE.registerCommand("book:chapter:pre", function (_, _)
     SILE.call("fluent", {}, { "book-chapter-title-pre" })
@@ -148,107 +151,111 @@ book.registerCommands = function (_)
     SILE.process({ " " })
   end)
 
-SILE.doTexlike([[%
-\define[command=book:left-running-head-font]{\font[size=9pt]}%
-\define[command=book:right-running-head-font]{\font[size=9pt,style=italic]}%
-]])
-end
-
-SILE.registerCommand("chapter", function (options, content)
-  SILE.call("open-double-page")
-  SILE.call("noindent")
-  SILE.scratch.headers.right = nil
-  SILE.call("set-counter", { id = "footnote", value = 1 })
-  SILE.call("book:chapterfont", {}, function ()
-    SILE.call("book:sectioning", {
-      numbering = options.numbering,
-      toc = options.toc,
-      level = 1,
-      prenumber = "book:chapter:pre",
-      postnumber = "book:chapter:post"
-    }, content)
+  SILE.registerCommand("book:left-running-head-font", function (_, content)
+    SILE.call("font", { size = "9pt" }, content)
   end)
-  SILE.call("book:chapterfont", {}, content)
-  SILE.call("left-running-head", {}, function ()
-    SILE.settings.temporarily(function ()
-      SILE.call("book:left-running-head-font")
-      SILE.process(content)
+
+  SILE.registerCommand("book:right-running-head-font", function (_, content)
+    SILE.call("font", { size = "9pt", style = "Italic" }, content)
+  end)
+
+  SILE.registerCommand("chapter", function (options, content)
+    SILE.call("open-double-page")
+    SILE.call("noindent")
+    SILE.scratch.headers.right = nil
+    SILE.call("set-counter", { id = "footnote", value = 1 })
+    SILE.call("book:chapterfont", {}, function ()
+      SILE.call("book:sectioning", {
+        numbering = options.numbering,
+        toc = options.toc,
+        level = 1,
+        prenumber = "book:chapter:pre",
+        postnumber = "book:chapter:post"
+      }, content)
     end)
-  end)
-  SILE.call("bigskip")
-  SILE.call("nofoliothispage")
-end, "Begin a new chapter")
-
-SILE.registerCommand("section", function (options, content)
-  SILE.typesetter:leaveHmode()
-  SILE.call("goodbreak")
-  SILE.call("bigskip")
-  SILE.call("noindent")
-  SILE.call("book:sectionfont", {}, function ()
-    SILE.call("book:sectioning", {
-      numbering = options.numbering,
-      toc = options.toc,
-      level = 2,
-      postnumber = "book:section:post"
-    }, content)
-    SILE.process(content)
-  end)
-  if not SILE.scratch.counters.folio.off then
-    SILE.call("right-running-head", {}, function ()
-      SILE.call("book:right-running-head-font")
-      SILE.call("rightalign", {}, function ()
-        SILE.settings.temporarily(function ()
-          if SU.boolean(options.numbering, true) then
-            SILE.call("show-multilevel-counter", { id = "sectioning", level = 2 })
-            SILE.typesetter:typeset(" ")
-          end
-          SILE.process(content)
-        end)
+    SILE.call("book:chapterfont", {}, content)
+    SILE.call("left-running-head", {}, function ()
+      SILE.settings.temporarily(function ()
+        SILE.call("book:left-running-head-font", {}, content)
       end)
     end)
-  end
-  SILE.call("novbreak")
-  SILE.call("bigskip")
-  SILE.call("novbreak")
-  SILE.typesetter:inhibitLeading()
-end, "Begin a new section")
+    SILE.call("bigskip")
+    SILE.call("nofoliothispage")
+  end, "Begin a new chapter")
 
-SILE.registerCommand("subsection", function (options, content)
-  SILE.typesetter:leaveHmode()
-  SILE.call("goodbreak")
-  SILE.call("noindent")
-  SILE.call("medskip")
-  SILE.call("book:subsectionfont", {}, function ()
-    SILE.call("book:sectioning", {
-          numbering = options.numbering,
-          toc = options.toc,
-          level = 3,
-          postnumber = "book:subsection:post"
-        }, content)
-    SILE.process(content)
-  end)
-  SILE.typesetter:leaveHmode()
-  SILE.call("novbreak")
-  SILE.call("medskip")
-  SILE.call("novbreak")
-  SILE.typesetter:inhibitLeading()
-end, "Begin a new subsection")
+  SILE.registerCommand("section", function (options, content)
+    SILE.typesetter:leaveHmode()
+    SILE.call("goodbreak")
+    SILE.call("bigskip")
+    SILE.call("noindent")
+    SILE.call("book:sectionfont", {}, function ()
+      SILE.call("book:sectioning", {
+        numbering = options.numbering,
+        toc = options.toc,
+        level = 2,
+        postnumber = "book:section:post"
+      }, content)
+      SILE.process(content)
+    end)
+    if not SILE.scratch.counters.folio.off then
+      SILE.call("right-running-head", {}, function ()
+        SILE.call("book:right-running-head-font", {}, function ()
+          SILE.call("rightalign", {}, function ()
+            SILE.settings.temporarily(function ()
+              if SU.boolean(options.numbering, true) then
+                SILE.call("show-multilevel-counter", { id = "sectioning", level = 2 })
+                SILE.typesetter:typeset(" ")
+              end
+              SILE.process(content)
+            end)
+          end)
+        end)
+      end)
+    end
+    SILE.call("novbreak")
+    SILE.call("bigskip")
+    SILE.call("novbreak")
+    SILE.typesetter:inhibitLeading()
+  end, "Begin a new section")
 
-SILE.registerCommand("book:chapterfont", function (_, content)
-  SILE.settings.temporarily(function ()
-    SILE.call("font", { weight = 800, size = "22pt" }, content)
-  end)
-end)
-SILE.registerCommand("book:sectionfont", function (_, content)
-  SILE.settings.temporarily(function ()
-    SILE.call("font", { weight = 800, size = "15pt" }, content)
-  end)
-end)
+  SILE.registerCommand("subsection", function (options, content)
+    SILE.typesetter:leaveHmode()
+    SILE.call("goodbreak")
+    SILE.call("noindent")
+    SILE.call("medskip")
+    SILE.call("book:subsectionfont", {}, function ()
+      SILE.call("book:sectioning", {
+            numbering = options.numbering,
+            toc = options.toc,
+            level = 3,
+            postnumber = "book:subsection:post"
+          }, content)
+      SILE.process(content)
+    end)
+    SILE.typesetter:leaveHmode()
+    SILE.call("novbreak")
+    SILE.call("medskip")
+    SILE.call("novbreak")
+    SILE.typesetter:inhibitLeading()
+  end, "Begin a new subsection")
 
-SILE.registerCommand("book:subsectionfont", function (_, content)
-  SILE.settings.temporarily(function ()
-    SILE.call("font", { weight = 800, size = "12pt" }, content)
+  SILE.registerCommand("book:chapterfont", function (_, content)
+    SILE.settings.temporarily(function ()
+      SILE.call("font", { weight = 800, size = "22pt" }, content)
+    end)
   end)
-end)
+  SILE.registerCommand("book:sectionfont", function (_, content)
+    SILE.settings.temporarily(function ()
+      SILE.call("font", { weight = 800, size = "15pt" }, content)
+    end)
+  end)
+
+  SILE.registerCommand("book:subsectionfont", function (_, content)
+    SILE.settings.temporarily(function ()
+      SILE.call("font", { weight = 800, size = "12pt" }, content)
+    end)
+  end)
+
+end
 
 return book
