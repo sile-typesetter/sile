448b6e3d28c3bceae9443ac96b1dee1f0471b049
Caleb Maclennan <caleb@alerque.com>
2022-05-10 12:13:59 +0300

    chore(classes): Redo diglot class with Penlight class model

--- classes/diglot.lua
+++ classes/diglot.lua
@@ -1,14 +1,19 @@
-local plain = SILE.require("plain", "classes")
-local diglot = plain { id = "diglot" }
+local plain = require("classes.plain")
+local diglot = pl.class(plain)
+diglot._name = "diglot"
 
-function diglot:init ()
-  SILE.require("packages/counters")
+function diglot:_init (options)
+  if self._legacy and not self._deprecated then return self:_deprecator(plain) end
+  plain._init(self, options)
+  self:loadPackage("counters")
   SILE.scratch.counters.folio = { value = 1, display = "arabic" }
   self:declareFrame("a",    { left = "8.3%pw",  right = "48%pw",          top = "11.6%ph",        bottom = "80%ph"          })
   self:declareFrame("b",    { left = "52%pw",   right = "100%pw-left(a)", top = "top(a)",         bottom = "bottom(a)"      })
   self:declareFrame("folio",{ left = "left(a)", right = "right(b)",       top = "bottom(a)+3%ph", bottom = "bottom(a)+8%ph" })
   self:loadPackage("parallel", { frames = { left = "a", right = "b" } })
-  return plain.init(self)
+  -- Avoid calling this (yet) if we're the parent of some child class
+  if self._name == "diglot" then self:post_init() end
+  return self
 end
 
 return diglot
