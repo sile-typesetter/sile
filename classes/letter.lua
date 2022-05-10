d115cd15037168dfa9c72d48453cd4d4d147c0e5
Caleb Maclennan <caleb@alerque.com>
2022-05-10 12:48:01 +0300

    chore(classes): Redo letter class with Penlight class model

--- classes/letter.lua
+++ classes/letter.lua
@@ -1,7 +1,10 @@
-local plain = SILE.require("classes/plain")
-local letter = plain { id = "letter" }
+local plain = require("classes.plain")
+local letter = pl.class(plain)
+letter._name = "letter"
 
-function letter:init ()
+function letter:_init (options)
+  if self._legacy and not self._deprecated then return self:_deprecator(plain) end
+  plain._init(self, options)
   self:declareFrame("content", {
       left = "5%pw",
       right = "95%pw",
@@ -15,58 +18,66 @@ function letter:init ()
     recipient = "",
     salutation = ""
   }
-  return plain.init(self)
+  -- Avoid calling this (yet) if we're the parent of some child class
+  if self._name == "letter" then self:post_init() end
+  return self
 end
 
-SILE.registerCommand("letter", function (_, content)
-  SILE.settings.set("current.parindent", SILE.nodefactory.glue())
-  SILE.settings.set("document.parindent", SILE.nodefactory.glue())
-  SILE.call("raggedright", {}, function ()
-    SILE.call("letter:format:date")
-    SILE.call("bigskip")
-    if SILE.scratch.letter.sender then
-      SILE.call("letter:format:sender")
+function letter:registerCommands ()
+
+  plain.registerCommands(self)
+
+  SILE.registerCommand("letter", function (_, content)
+    SILE.settings.set("current.parindent", SILE.nodefactory.glue())
+    SILE.settings.set("document.parindent", SILE.nodefactory.glue())
+    SILE.call("raggedright", {}, function ()
+      SILE.call("letter:format:date")
       SILE.call("bigskip")
-    end
-    SILE.call("letter:format:recipient")
-    SILE.call("bigskip")
-    SILE.call("letter:format:salutation")
-    SILE.call("bigskip")
-    SILE.process(content)
+      if SILE.scratch.letter.sender then
+        SILE.call("letter:format:sender")
+        SILE.call("bigskip")
+      end
+      SILE.call("letter:format:recipient")
+      SILE.call("bigskip")
+      SILE.call("letter:format:salutation")
+      SILE.call("bigskip")
+      SILE.process(content)
+    end)
   end)
-end)
 
-SILE.registerCommand("sender", function (_, content)
-  SILE.scratch.letter.sender = content
-end)
-SILE.registerCommand("recipient", function (_, content)
-  SILE.scratch.letter.recipient = content
-end)
-SILE.registerCommand("salutation", function (_, content)
-  SILE.scratch.letter.salutation = content
-end)
-SILE.registerCommand("date", function (_, content)
-  SILE.scratch.letter.date = content
-end)
+  SILE.registerCommand("sender", function (_, content)
+    SILE.scratch.letter.sender = content
+  end)
+  SILE.registerCommand("recipient", function (_, content)
+    SILE.scratch.letter.recipient = content
+  end)
+  SILE.registerCommand("salutation", function (_, content)
+    SILE.scratch.letter.salutation = content
+  end)
+  SILE.registerCommand("date", function (_, content)
+    SILE.scratch.letter.date = content
+  end)
 
-SILE.registerCommand("letter:format:date", function ()
-  if not SILE.scratch.letter.date then
-    SILE.scratch.letter.date = { os.date("%A, %d %B") }
-  end
-  SILE.process(SILE.scratch.letter.date)
-  SILE.call("par")
-end)
+  SILE.registerCommand("letter:format:date", function ()
+    if not SILE.scratch.letter.date then
+      SILE.scratch.letter.date = { os.date("%A, %d %B") }
+    end
+    SILE.process(SILE.scratch.letter.date)
+    SILE.call("par")
+  end)
 
-SILE.registerCommand("letter:format:sender", function ()
-  SILE.process(SILE.scratch.letter.sender)
-end)
+  SILE.registerCommand("letter:format:sender", function ()
+    SILE.process(SILE.scratch.letter.sender)
+  end)
 
-SILE.registerCommand("letter:format:recipient", function ()
-  SILE.process(SILE.scratch.letter.recipient)
-end)
+  SILE.registerCommand("letter:format:recipient", function ()
+    SILE.process(SILE.scratch.letter.recipient)
+  end)
 
-SILE.registerCommand("letter:format:salutation", function ()
-  SILE.process(SILE.scratch.letter.salutation)
-end)
+  SILE.registerCommand("letter:format:salutation", function ()
+    SILE.process(SILE.scratch.letter.salutation)
+  end)
+
+end
 
 return letter
