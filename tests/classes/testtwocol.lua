1ecd79abbd2a0734b059629ae275ea9dae80adbe
Caleb Maclennan <caleb@alerque.com>
2022-05-10 12:53:19 +0300

    test(classes): Redo class tests with Penlight class model

--- tests/classes/testtwocol.lua
+++ tests/classes/testtwocol.lua
@@ -1,18 +1,21 @@
-local plain = SILE.require("plain", "classes")
-local testtwocols = plain { id = "testtwocols" }
+local plain = require("classes.plain")
+local testtwocol = pl.class(plain)
+testtwocol._name = "testtwocol"
 
 local gutterWidth = "3%pw"
 
-testtwocols.defaultFrameset = {}
-testtwocols.firstContentFrame = "contentA"
+testtwocol.defaultFrameset = {}
+testtwocol.firstContentFrame = "contentA"
 
-testtwocols.init = function(self)
+function testtwocol:_init (options)
+  plain._init(self, options)
   self:declareFrame("contentA", {left = "left(content)", right = "left(gutter)", top = "5%ph", bottom = "83.3%ph", next = "contentB", balanced = true })
   self:declareFrame("contentB", {left = "right(gutter)", width="width(contentA) * 2 / 3", right = "right(content)", top = "5%ph", bottom = "top(footnotes)", balanced = true })
   self:declareFrame("gutter", {left = "right(contentA)", right = "left(contentB)", width = gutterWidth })
   self:declareFrame("footnotes", { left="left(contentB)", right = "right(contentB)", height = "0", bottom="83.3%ph"})
   self:loadPackage("footnotes", { insertInto = "footnotes", stealFrom = { contentB = 1 } } )
-  return plain.init(self)
+  self:post_init()
+  return self
 end
 
-return testtwocols
+return testtwocol
