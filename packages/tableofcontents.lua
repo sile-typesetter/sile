75c5c2614d7a0deeb2fe93ce716c208e71112617
Caleb Maclennan <caleb@alerque.com>
2022-06-01 13:44:53 +0300

    chore(packages): Work on making footnote package idempotent

--- packages/tableofcontents.lua
+++ packages/tableofcontents.lua
@@ -5,10 +5,9 @@
 --          writeToc (call this in finish)
 --          moveTocNodes (call this in endPage)
 
-SILE.scratch.tableofcontents = {}
 local _tableofcontents = {}
 
-local moveNodes = function (_)
+local function moveNodes (_)
   local node = SILE.scratch.info.thispage.toc
   if node then
     for i = 1, #node do
@@ -18,7 +17,7 @@ local moveNodes = function (_)
   end
 end
 
-local writeToc = function ()
+local function writeToc (_)
   local tocdata = pl.pretty.write(SILE.scratch.tableofcontents)
   local tocfile, err = io.open(SILE.masterFilename .. '.toc', "w")
   if not tocfile then return SU.error(err) end
@@ -30,33 +29,7 @@ local writeToc = function ()
   end
 end
 
-SILE.registerCommand("tableofcontents", function (options, _)
-  local depth = SU.cast("integer", options.depth or 3)
-  local linking = SU.boolean(options.linking, true)
-  local tocfile,_ = io.open(SILE.masterFilename .. '.toc')
-  if not tocfile then
-    SILE.call("tableofcontents:notocmessage")
-    return
-  end
-  local doc = tocfile:read("*all")
-  local toc = assert(load(doc))()
-  SILE.call("tableofcontents:header")
-  for i = 1, #toc do
-    local item = toc[i]
-    if item.level <= depth then
-      SILE.call("tableofcontents:item", {
-        level = item.level,
-        pageno = item.pageno,
-        number = item.number,
-        link = linking and item.link
-      }, item.label)
-    end
-  end
-  SILE.call("tableofcontents:footer")
-  _tableofcontents = toc
-end)
-
-local linkWrapper = function (dest, func)
+local function _linkWrapper (dest, func)
   if dest and SILE.Commands["pdf:link"] then
     return function()
       SILE.call("pdf:link", { dest = dest }, func)
@@ -66,31 +39,10 @@ local linkWrapper = function (dest, func)
   end
 end
 
-SILE.registerCommand("tableofcontents:item", function (options, content)
-  SILE.settings:temporarily(function ()
-    SILE.settings:set("typesetter.parfillskip", SILE.nodefactory.glue())
-    SILE.call("tableofcontents:level" .. options.level .. "item", {
-    }, linkWrapper(options.link,
-      function ()
-        SILE.call("tableofcontents:level" .. options.level .. "number", {
-        }, function ()
-          if options.number then
-            SILE.typesetter:typeset(options.number or "")
-            SILE.call("kern", { width = "1spc" })
-          end
-        end)
-        SILE.process(content)
-        SILE.call("dotfill")
-        SILE.typesetter:typeset(options.pageno)
-      end)
-    )
-  end)
-end)
-
 -- Flatten a node list into just its string representation.
 -- (Similar to SU.contentToString(), but allows passing typeset
 -- objects to functions that need plain strings).
-local nodesToText = function (nodes)
+local function _nodesToText (nodes)
   local string = ""
   for i = 1, #nodes do
     local node = nodes[i]
@@ -114,40 +66,18 @@ local nodesToText = function (nodes)
 end
 
 local dc = 1
-SILE.registerCommand("tocentry", function (options, content)
-  local dest
-  if SILE.Commands["pdf:destination"] then
-    dest = "dest" .. dc
-    SILE.call("pdf:destination", { name = dest })
-    SILE.typesetter:pushState()
-    SILE.process(content)
-    local title = nodesToText(SILE.typesetter.state.nodes)
-    SILE.typesetter:popState()
-    SILE.call("pdf:bookmark", { title = title, dest = dest, level = options.level })
-    dc = dc + 1
+
+local function init (class, _)
+
+  if not SILE.scratch.tableofcontents then
+    SILE.scratch.tableofcontents = {}
   end
-  SILE.call("info", {
-    category = "toc",
-    value = {
-      label = content,
-      level = (options.level or 1),
-      number = options.number,
-      link = dest
-    }
-  })
-end)
 
-SILE.registerCommand("tableofcontents:title", function (_, _)
-  SU.deprecated("\\tableofcontents:title", "\\fluent{toc-title}", "0.13.0", "0.14.0")
-  SILE.call("fluent", {}, { "toc-title" })
-end)
+  class:loadPackage("infonode")
+  class:loadPackage("leaders")
 
-return {
-  exports = { writeToc = writeToc, moveTocNodes = moveNodes },
-  init = function (self)
-    self:loadPackage("infonode")
-    self:loadPackage("leaders")
-SILE.doTexlike([[%
+class:registerPostinit(function ()
+  SILE.doTexlike([[%
 \define[command=tableofcontents:notocmessage]{\tableofcontents:headerfont{\fluent{toc-not-generated}}}%
 \define[command=tableofcontents:headerfont]{\font[size=24pt,weight=800]{\process}}%
 \define[command=tableofcontents:header]{\par\noindent\tableofcontents:headerfont{\fluent{toc-title}}\medskip}%
@@ -159,8 +89,96 @@ SILE.doTexlike([[%
 \define[command=tableofcontents:level2number]{}%
 \define[command=tableofcontents:level3number]{}%
 ]])
+end)
 
-  end,
+end
+
+local function registerCommands (_)
+
+  SILE.registerCommand("tableofcontents", function (options, _)
+    local depth = SU.cast("integer", options.depth or 3)
+    local linking = SU.boolean(options.linking, true)
+    local tocfile,_ = io.open(SILE.masterFilename .. '.toc')
+    if not tocfile then
+      SILE.call("tableofcontents:notocmessage")
+      return
+    end
+    local doc = tocfile:read("*all")
+    local toc = assert(load(doc))()
+    SILE.call("tableofcontents:header")
+    for i = 1, #toc do
+      local item = toc[i]
+      if item.level <= depth then
+        SILE.call("tableofcontents:item", {
+          level = item.level,
+          pageno = item.pageno,
+          number = item.number,
+          link = linking and item.link
+        }, item.label)
+      end
+    end
+    SILE.call("tableofcontents:footer")
+    _tableofcontents = toc
+  end)
+
+  SILE.registerCommand("tableofcontents:item", function (options, content)
+    SILE.settings:temporarily(function ()
+      SILE.settings:set("typesetter.parfillskip", SILE.nodefactory.glue())
+      SILE.call("tableofcontents:level" .. options.level .. "item", {
+      }, _linkWrapper(options.link,
+      function ()
+        SILE.call("tableofcontents:level" .. options.level .. "number", {
+        }, function ()
+          if options.number then
+            SILE.typesetter:typeset(options.number or "")
+            SILE.call("kern", { width = "1spc" })
+          end
+        end)
+        SILE.process(content)
+        SILE.call("dotfill")
+        SILE.typesetter:typeset(options.pageno)
+      end)
+      )
+    end)
+  end)
+
+  SILE.registerCommand("tocentry", function (options, content)
+    local dest
+    if SILE.Commands["pdf:destination"] then
+      dest = "dest" .. dc
+      SILE.call("pdf:destination", { name = dest })
+      SILE.typesetter:pushState()
+      SILE.process(content)
+      local title = _nodesToText(SILE.typesetter.state.nodes)
+      SILE.typesetter:popState()
+      SILE.call("pdf:bookmark", { title = title, dest = dest, level = options.level })
+      dc = dc + 1
+    end
+    SILE.call("info", {
+      category = "toc",
+      value = {
+        label = content,
+        level = (options.level or 1),
+        number = options.number,
+        link = dest
+      }
+    })
+  end)
+
+  SILE.registerCommand("tableofcontents:title", function (_, _)
+    SU.deprecated("\\tableofcontents:title", "\\fluent{toc-title}", "0.13.0", "0.14.0")
+    SILE.call("fluent", {}, { "toc-title" })
+  end)
+
+end
+
+return {
+  init = init,
+  registerCommands = registerCommands,
+  exports = {
+    writeToc = writeToc,
+    moveTocNodes = moveNodes
+  },
   documentation = [[
 \begin{document}
 The \autodoc:package{tableofcontents} package provides tools for class authors to
