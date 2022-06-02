5fb4c531f91ded9ad13b547ca34d0f4cd7609ac3
Caleb Maclennan <caleb@alerque.com>
2022-06-02 15:19:34 +0300

    chore(packages): Work on making image package idempotent

--- packages/image.lua
+++ packages/image.lua
@@ -1,30 +1,35 @@
-SILE.registerCommand("img", function (options, _)
-  SU.required(options, "src", "including image file")
-  local width =  SILE.parseComplexFrameDimension(options.width or 0) or 0
-  local height = SILE.parseComplexFrameDimension(options.height or 0) or 0
-  local src = SILE.resolveFile(options.src) or SU.error("Couldn't find file "..options.src)
-  local box_width, box_height = SILE.outputter:getImageSize(src)
-  local sx, sy = 1, 1
-  if width > 0 or height > 0 then
-    sx = width > 0 and box_width / width
-    sy = height > 0 and box_height / height
-    sx = sx or sy
-    sy = sy or sx
-  end
+local function registerCommands (_)
 
-  SILE.typesetter:pushHbox({
-    width= box_width / (sx),
-    height= box_height / (sy),
-    depth= 0,
-    value= src,
-    outputYourself= function (self, typesetter, _)
-      SILE.outputter:drawImage(self.value, typesetter.frame.state.cursorX, typesetter.frame.state.cursorY-self.height, self.width, self.height)
-      typesetter.frame:advanceWritingDirection(self.width)
-  end})
+  SILE.registerCommand("img", function (options, _)
+    SU.required(options, "src", "including image file")
+    local width =  SILE.parseComplexFrameDimension(options.width or 0) or 0
+    local height = SILE.parseComplexFrameDimension(options.height or 0) or 0
+    local src = SILE.resolveFile(options.src) or SU.error("Couldn't find file "..options.src)
+    local box_width, box_height = SILE.outputter:getImageSize(src)
+    local sx, sy = 1, 1
+    if width > 0 or height > 0 then
+      sx = width > 0 and box_width / width
+      sy = height > 0 and box_height / height
+      sx = sx or sy
+      sy = sy or sx
+    end
 
-end, "Inserts the image specified with the <src> option in a box of size <width> by <height>")
+    SILE.typesetter:pushHbox({
+      width= box_width / (sx),
+      height= box_height / (sy),
+      depth= 0,
+      value= src,
+      outputYourself= function (self, typesetter, _)
+        SILE.outputter:drawImage(self.value, typesetter.frame.state.cursorX, typesetter.frame.state.cursorY-self.height, self.width, self.height)
+        typesetter.frame:advanceWritingDirection(self.width)
+    end})
+
+  end, "Inserts the image specified with the <src> option in a box of size <width> by <height>")
+
+end
 
 return {
+  registerCommands = registerCommands,
   documentation = [[
 \begin{document}
 
