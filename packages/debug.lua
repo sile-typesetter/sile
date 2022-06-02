7b7cf168110c47f81cf3c175b5f5e721de8484bf
Caleb Maclennan <caleb@alerque.com>
2022-06-02 14:45:05 +0300

    chore(packages): Work on making debug package idempotent

--- packages/debug.lua
+++ packages/debug.lua
@@ -1,14 +1,19 @@
-SILE.registerCommand("debug", function (options, _)
-  for k, v in pairs(options) do
-    SILE.debugFlags[k] = SU.boolean(v, true)
-  end
-end)
+local function registerCommands (_)
 
-SILE.registerCommand("disable-pushback", function (_, _)
-  SILE.typesetter.pushBack = function() end
-end)
+  SILE.registerCommand("debug", function (options, _)
+    for k, v in pairs(options) do
+      SILE.debugFlags[k] = SU.boolean(v, true)
+    end
+  end)
+
+  SILE.registerCommand("disable-pushback", function (_, _)
+    SILE.typesetter.pushBack = function() end
+  end)
+
+end
 
 return {
+  registerCommands = registerCommands,
 documentation = [[
 \begin{document}
 This package provides two commands: \autodoc:command{\debug}, which turns
