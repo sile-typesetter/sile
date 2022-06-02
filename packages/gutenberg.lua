6e73d10553ac74fad1508a87992460ecf6af7d89
Caleb Maclennan <caleb@alerque.com>
2022-06-02 15:08:12 +0300

    chore(packages): Work on making gutenberg package idempotent

--- packages/gutenberg.lua
+++ packages/gutenberg.lua
@@ -1,24 +1,29 @@
-SILE.registerCommand("alt", function (_, content)
-  SU.warn("Use of \\alt  is deprecated, please use \\alternative.")
-  SILE.call("alternative", {}, content)
-end)
+local function registerCommands (_)
 
-SILE.registerCommand("alternative", function (_, content)
-  local options = {}
-  for _, fragment in ipairs(content) do
-    SILE.call("hbox", {}, { fragment })
-    options[#options + 1] = SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes]
-    SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes] = nil
-  end
-  local alternative = SILE.nodefactory.alternative({
-    options=options,
-    selected=1
-    })
-  alternative.width=nil
-  SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes+1] = alternative
-end)
+  SILE.registerCommand("alt", function (_, content)
+    SU.warn("Use of \\alt  is deprecated, please use \\alternative.")
+    SILE.call("alternative", {}, content)
+  end)
+
+  SILE.registerCommand("alternative", function (_, content)
+    local options = {}
+    for _, fragment in ipairs(content) do
+      SILE.call("hbox", {}, { fragment })
+      options[#options + 1] = SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes]
+      SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes] = nil
+    end
+    local alternative = SILE.nodefactory.alternative({
+      options=options,
+      selected=1
+      })
+    alternative.width=nil
+    SILE.typesetter.state.nodes[#SILE.typesetter.state.nodes+1] = alternative
+  end)
+
+end
 
 return {
+  registerCommands = registerCommands,
   documentation = [[
 \begin{document}
 One of the reasons why Johann Gutenberg’s 42 line Bible is considered a
