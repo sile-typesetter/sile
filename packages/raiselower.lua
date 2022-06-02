e029baa674289483dc6c0f8c0b713c7c5e2796f3
Caleb Maclennan <caleb@alerque.com>
2022-06-02 19:28:25 +0300

    chore(packages): Work on making raiselower package idempotent

--- packages/raiselower.lua
+++ packages/raiselower.lua
@@ -1,27 +1,32 @@
-SILE.registerCommand("raise", function (options, content)
-  local height = options.height or 0
-  height = SILE.parseComplexFrameDimension(height)
-  SILE.typesetter:pushHbox({
-      outputYourself = function (_, typesetter, _)
-        typesetter.frame:advancePageDirection(-height)
-      end
-    })
-  SILE.process(content)
-  SILE.typesetter:pushHbox({
-      outputYourself = function (_, typesetter, _)
-        if (type(typesetter.state.cursorY)) == "table" then
-          typesetter.state.cursorY = typesetter.state.cursorY.length
-        end
-        typesetter.frame:advancePageDirection(height)
-      end
-    })
-end, "Raises the contents of the command by the amount specified in the <height> option")
+local function registerCommands (_)
 
-SILE.registerCommand("lower", function (options, content)
-  SILE.call("raise", { height = "-" .. options.height }, content)
-end, "Lowers the contents of the command by the amount specified in the <height> option")
+  SILE.registerCommand("raise", function (options, content)
+    local height = options.height or 0
+    height = SILE.parseComplexFrameDimension(height)
+    SILE.typesetter:pushHbox({
+        outputYourself = function (_, typesetter, _)
+          typesetter.frame:advancePageDirection(-height)
+        end
+      })
+    SILE.process(content)
+    SILE.typesetter:pushHbox({
+        outputYourself = function (_, typesetter, _)
+          if (type(typesetter.state.cursorY)) == "table" then
+            typesetter.state.cursorY = typesetter.state.cursorY.length
+          end
+          typesetter.frame:advancePageDirection(height)
+        end
+      })
+  end, "Raises the contents of the command by the amount specified in the <height> option")
+
+  SILE.registerCommand("lower", function (options, content)
+    SILE.call("raise", { height = "-" .. options.height }, content)
+  end, "Lowers the contents of the command by the amount specified in the <height> option")
+
+end
 
 return {
+  registerCommands = registerCommands,
   documentation = [[
 \begin{document}
 
@@ -46,5 +51,4 @@ some text lowered by \\lower[height=4pt]\{four points\}.
 \end{verbatim}
 
 \end{document}
-]]
-}
+]]}
