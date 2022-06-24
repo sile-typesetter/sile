db83e9ede06dedf89112a7d9d76e185df90f6dba
Fred Brennan <copypaste@kittens.ph>
2022-06-23 20:14:24 -0400

    fix(classes): Clarify the scopes of `tate` and `jplain`
    As I've been attempting to enforce through my commits, counter to how
    Simon did things initially:
    
    * `tate` and `ruby`: General purpose libraries that *should not be loading
      any fonts* or *assuming a Japanese document*.
    
    * `jplain`: The Japanese language class, *may* enforce
      `\language[main=ja]` but I didn't for backwards compatibility.

--- classes/jplain.lua
+++ classes/jplain.lua
@@ -17,13 +17,21 @@ function jplain:_j_common ()
   self:loadPackage("font-fallback")
   self:loadPackage("hanmenkyoshi")
   self:registerPostinit(function (class)
-      class:bidiDisableTypesetter(SILE.typesetter)
-      class:bidiDisableTypesetter(SILE.defaultTypesetter)
-      SILE.call("font:add-fallback", { family = "Noto Sans CJK JP" })
-    end)
+    class:bidiDisableTypesetter(SILE.typesetter)
+    class:bidiDisableTypesetter(SILE.defaultTypesetter)
+  end)
   self.defaultFrameset.content.tate = self.options.layout == "tate"
   self:declareHanmenFrame("content", self.defaultFrameset.content)
   SILE.settings:set("document.parindent", SILE.nodefactory.glue("10pt"))
+  if SILE.settings:get("document.language") ~= "ja" then
+    SU.deprecated("document.language ≠ \"ja\" & jplain:…", nil, "0.13.2", "0.14.0", "Prior to SILE v0.13.2, `jplain`, despite its name, did not enforce the use of Japanese in its documents. To use a class like `jplain` for other languages, please base a  *new* custom class off of `jplain`; do not use `jplain` itself. To use `jplain` for Japanese, you *must* specify \\language[main=ja]. This will become a hard error in SILE v0.14.")
+    --[[ @alerque — Remove these lines post v0.14.0! ]]
+    self:registerPostinit(function (_)
+      SILE.call("font:add-fallback", { family = "Noto Sans CJK JP" })
+    end)
+    SILE.settings:set("document.language", "ja")
+    SILE.languageSupport.loadLanguage("ja")
+  end
 end
 
 function jplain:_init (options)
