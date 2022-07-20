19b74445ab5327360656385efe9be8d258e5d20d
Caleb Maclennan <caleb@alerque.com>
2022-07-21 01:15:14 +0300

    refactor(packages): Update date package with new interface

--- packages/date/init.lua
+++ packages/date/init.lua
@@ -1,3 +1,8 @@
+local base = require("packages.base")
+
+local package = pl.class(base)
+package._name = "date"
+
 local localeify = function (lang)
   lang = lang == "en-u-va-posix" and "en" or lang
   return lang .. "_" .. string.upper(lang) ..  ".utf-8"
@@ -15,25 +20,30 @@ local date = function (class, options)
   return os.date(options.format, options.time)
 end
 
-local function registerCommands (class)
+function package:_init (class)
 
-  class:registerCommand("date", function (options, _)
-    SILE.typesetter:typeset(class:date(options))
+  base._init(self, class)
+
+  --exports
+  date = date
+
+end
+
+function package:registerCommands ()
+
+  self.class:registerCommand("date", function (options, _)
+    local datestring = self.class:date(options)
+    SILE.typesetter:typeset(datestring)
   end, "Output a timestamp using the system date function")
 
 end
 
-return {
-  registerCommands = registerCommands,
-  exports = {
-    date = date
-  },
-documentation = [[
+package.documentation = [[
 \begin{document}
-The \autodoc:package{date} package provides the \autodoc:command{\date} command, which simply
-outputs the date using the system’s date function. You can customize
-the format by passing the \autodoc:parameter{format} parameter, following the
-formatting codes in the Lua manual. (\url{https://www.lua.org/pil/22.1.html})
+The \autodoc:package{date} package provides the \autodoc:command{\date} command, which simply outputs the date using the system’s date function.
+You can customize the format by passing the \autodoc:parameter{format} parameter, following the formatting codes in the Lua manual.
+(\url{https://www.lua.org/pil/22.1.html})
 \end{document}
 ]]
-}
+
+return package
