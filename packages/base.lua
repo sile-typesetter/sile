774f602fc82067d22052a4f6055f48f3cd1ac1a1
Caleb Maclennan <caleb@alerque.com>
2022-07-28 17:12:33 +0300

    chore(packages): Add shims to make legacy package *usage* easier

--- packages/base.lua
+++ packages/base.lua
@@ -5,6 +5,9 @@ package._name = "base"
 package._initialized = false
 package.class = nil
 
+-- For shimming packages that used to have legacy exports
+package.exports = {}
+
 function package:_init (_)
   self.class = SILE.documentState.documentClass or SILE.scratch.half_initialized_class
   if not self.class then
@@ -29,6 +32,52 @@ function package.registerCommands (_) end
 -- un-export them if we ever need to unload modules and revert functions
 function package:export (name, func)
   self.class[name] = func
+end
+
+-- Shims for two possible kinds of legacy exports: blind direct stuffing into
+-- the class but not expecting to be called as a method AND the exports table
+-- to package modules...
+
+local _deprecate_class_funcs = [[
+  Please explicitly use functions provided by packages by referencing
+  them in the document class's list of loaded packages rather than the
+  legacy solution that added non-method functions to the class.]]
+
+local _deprecate_exports_table = [[
+  Please explicitly use functions provided by packages by referencing
+  them in the document class's list of loaded packages rather than the
+  legacy solution of calling them from an exports table.]]
+
+function package:deprecatedExport (name, func, noclass, notable)
+
+  if not noclass then
+    self.class[name] = function (...)
+      -- http://lua-users.org/wiki/VarargTheSecondClassCitizen
+      local inputs = {...}
+      -- local inputs = table.unpack({...}, 1, select("#", ...))
+      if type(inputs[1]) ~= "table" or inputs[1].type ~= "class" then
+        table.insert(inputs, 1, self.class)
+      end
+      SU.deprecated(("class.%s"):format(name),
+                    ("class.packages[%s]:%s"):format(self._name, name),
+                    "0.14.0", "0.16.0", _deprecate_class_funcs)
+      return func(table.unpack(inputs, 1, select("#", ...) + 1))
+    end
+  end
+
+  if not notable then
+    self.exports[name] = function (...)
+      local inputs = {...}
+      if type(inputs[1]) ~= "table" or inputs[1].type ~= "package" then
+        table.insert(inputs, 1, self)
+      end
+      SU.deprecated(("require('packages.%s').exports.%s"):format(self._name, name),
+                    ("class.packages[%s]:%s"):format(self._name, name),
+                    "0.14.0", "0.16.0", _deprecate_exports_table)
+      return func(table.unpack(inputs, 1, select("#", ...) + 1))
+    end
+  end
+
 end
 
 return package
