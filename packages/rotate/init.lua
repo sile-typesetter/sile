35219360e0e5ad54f3363624148181eb619715c7
Caleb Maclennan <caleb@alerque.com>
2022-09-08 00:27:54 +0300

    fix(packages): Correct rotation origin calculation back to pre v0.10.0
    Co-authored-by: Omikhleia <didier.willis@gmail.com>

--- packages/rotate/init.lua
+++ packages/rotate/init.lua
@@ -41,7 +41,7 @@ local outputRotatedHbox = function (self, typesetter, line)
   typesetter.frame.state.cursorX = typesetter.frame.state.cursorX - (origbox.width.length-self.width)/2
 
   local horigin = (typesetter.frame.state.cursorX + origbox.width.length / 2):tonumber()
-  local vorigin = -(typesetter.frame.state.cursorY + origbox.height / 2):tonumber()
+  local vorigin = -(typesetter.frame.state.cursorY - origbox.height / 2):tonumber()
   pdf:gsave()
   pdf.setmatrix(1, 0, 0, 1, horigin, vorigin)
   pdf.setmatrix(math.cos(x), math.sin(x), -math.sin(x), math.cos(x), 0, 0)
