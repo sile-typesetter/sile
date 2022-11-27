d9867f57ff4ab846bf2368e09fd1f08901ed793a
Omikhleia <didier.willis@gmail.com>
2022-11-27 02:47:48 +0100

    docs(packages): Small case error in counters package documentation
    Mentioned Roman (title case) but actually needed ROMAN (upper case)

--- packages/counters/init.lua
+++ packages/counters/init.lua
@@ -219,7 +219,7 @@ The available built-in display types are:
 \item{\code{alpha}, for lower-case alphabetic counting;}
 \item{\code{Alpha}, for upper-case alphabetic counting;}
 \item{\code{roman}, for lower-case Roman numerals; and,}
-\item{\code{Roman} for upper-case Roman numerals.}
+\item{\code{ROMAN} for upper-case Roman numerals.}
 \end{itemize}
 
 The ICU library also provides ways of formatting numbers in global (non-Latin) scripts.
