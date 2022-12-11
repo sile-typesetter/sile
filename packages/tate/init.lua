b7215f0aad342a96012271f6514e73cf4f02fd50
Caleb Maclennan <caleb@alerque.com>
2022-12-11 15:26:18 +0300

    refactor(typesetters): Move tate typesetter functions to class

--- packages/tate/init.lua
+++ packages/tate/init.lua
@@ -6,26 +6,16 @@ package._name = "tate"
 SILE.tateFramePrototype = pl.class(SILE.framePrototype)
 SILE.tateFramePrototype.direction = "TTB-RTL"
 
+
 SILE.tateFramePrototype.enterHooks = {
     function (_, typesetter)
-      typesetter.old_leadingFor = typesetter.leadingFor
-      typesetter.old_breakIntoLines = typesetter.breakIntoLines
-      typesetter.leadingFor = function(_, v)
-        v.height = SILE.length("1zw"):absolute()
-        local bls = SILE.settings:get("document.baselineskip")
-        local d = bls.height:absolute() - v.height
-        local len = SILE.length(d.length, bls.height.stretch, bls.height.shrink)
-        return SILE.nodefactory.vglue({height = len})
-      end
-      local firstfit = SILE.use("packages-break-firstfit")
-      typesetter.breakIntoLines = firstfit.breakIntoLines
+      SILE.typesetters.tate:cast(typesetter)
     end
   }
 
 SILE.tateFramePrototype.leaveHooks = {
     function (_, typesetter)
-      typesetter.leadingFor = typesetter.old_leadingFor
-      typesetter.breakIntoLines = typesetter.old_breakIntoLines
+      SILE.typesetters.base:cast(typesetter)
     end
   }
 
