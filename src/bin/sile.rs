a0ca2dcf072c21bc1bb0c7a5c4fcdca210158c80
Caleb Maclennan <caleb@alerque.com>
2023-04-06 16:23:11 +0300

    chore(cli): Add Lua interpreter as main CLI function

--- src/bin/sile.rs
+++ src/bin/sile.rs
@@ -1,11 +1,19 @@
 use clap::CommandFactory;
+use mlua::prelude::*;
 
 use sile::cli::Cli;
 
 fn main() -> sile::Result<()> {
     let version = option_env!("VERGEN_GIT_SEMVER").unwrap_or_else(|| env!("VERGEN_BUILD_SEMVER"));
     let app = Cli::command().version(version);
+    #[allow(unused_variables)]
     let matches = app.get_matches();
-    println!("Hello, world!");
+    sile_lua()?;
+    Ok(())
+}
+
+fn sile_lua() -> LuaResult<()> {
+    let lua = Lua::new();
+    lua.load("print('Hello, world!')").exec()?;
     Ok(())
 }
