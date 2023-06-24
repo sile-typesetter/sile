39f51215d90a0ae0299146743d71578baf985c3b
leorosa <leorosa@gmail.com>
2023-06-23 22:07:10 -0300

    add a 'numbered' mode to math in typesetter.lua

--- packages/math/typesetter.lua
+++ packages/math/typesetter.lua
@@ -129,6 +129,8 @@ end
 local function handleMath (_, mbox, mode)
   if mode == 'display' then
     mbox.mode = b.mathMode.display
+  elseif mode == 'numbered' then
+    mbox.mode = b.mathMode.display
   elseif mode == 'text' then
     mbox.mode = b.mathMode.textCramped
   else
@@ -147,6 +149,18 @@ local function handleMath (_, mbox, mode)
     SILE.call("center", {}, function()
       SILE.typesetter:pushHorizontal(mbox)
     end)
+    SILE.typesetter:endline()
+    SILE.typesetter:pushExplicitVglue(SILE.settings:get("math.displayskip"))
+  elseif mode == "numbered" then
+    SILE.typesetter:endline()
+    SILE.typesetter:pushExplicitVglue(SILE.settings:get("math.displayskip"))
+    SILE.typesetter:pushGlue(SILE.nodefactory.hfillglue())
+    SILE.typesetter:pushHorizontal(mbox)
+    SILE.typesetter:pushGlue(SILE.nodefactory.hfillglue())
+    SILE.call("increment-counter", { id="equation" })
+    SILE.typesetter:typeset("(")
+    SILE.call("show-counter", { id="equation" })
+    SILE.typesetter:typeset(")")
     SILE.typesetter:endline()
     SILE.typesetter:pushExplicitVglue(SILE.settings:get("math.displayskip"))
   else
