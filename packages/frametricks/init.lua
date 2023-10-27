8329053b6d0fe664c58c05079eadbbdcc2dfe43c
Caleb Maclennan <caleb@alerque.com>
2023-10-27 13:46:26 +0300

    refactor(frames): Only set default frame count for makecolumns in one place

--- packages/frametricks/init.lua
+++ packages/frametricks/init.lua
@@ -41,7 +41,7 @@ end
 
 local makecolumns = function (options)
   local cFrame = SILE.typesetter.frame
-  local cols = options.columns or 2
+  local cols = options.columns
   local gutterWidth = options.gutter or "3%pw"
   local right = cFrame:right()
   local origId = cFrame.id
@@ -174,8 +174,9 @@ function package:registerCommands ()
   end, "Breaks the current frame in two vertically at the current location or at a point <offset> below the current location")
 
   self:registerCommand("makecolumns", function (options, _)
-    local aligned = {}
+    -- Set a default value for column count
     options.columns = options.columns or 2
+    local aligned = {}
     for key in pairs(SILE.frames) do
       -- this must be checked before makecolumns() call, since the function changes the right() value of content
       if SILE.getFrame(key):right() == SILE.getFrame("content"):right() and key ~= "content" then
