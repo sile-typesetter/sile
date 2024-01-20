0253aa3727f86f8b4740d7e5ab55a921827e55e8
Caleb Maclennan <caleb@alerque.com>
2024-01-20 16:51:18 +0300

    chore(inputters): Bring new LPEG.RE variant to parity as optional parser

--- inputters/sil-lpeg-re.lua
+++ inputters/sil-lpeg-re.lua
@@ -1,7 +1,6 @@
-local lpeg = require('lpeg')
-local re = require('re')
-local bits = require('parserbits')
-local inspect = require('inspect')
+local lpeg = require("lpeg")
+local re = require("re")
+local bits = require("core.parserbits")
 
 local P, C, S = lpeg.P, lpeg.C, lpeg.S
 local myID = C(bits.silidentifier) / 1
@@ -9,8 +8,10 @@ local myID = C(bits.silidentifier) / 1
 local wrapper = function (a) return type(a)=="table" and a or {} end
 local specials = S"{}%\\"
 
-local g = re.compile([=[
+local expression = [=[
+
 document         <- texlike_stuff !.
+
 texlike_stuff    <- {: environment / comment / texlike_text / texlike_braced_stuff / texlike_command :}*
 
 environment      <- '\begin' {:options: %parameters :}
@@ -18,8 +19,11 @@ environment      <- '\begin' {:options: %parameters :}
                     '{' {:command: %cmdID :} '}' texlike_stuff notpass_end)
 
 comment          <- ('%' (!%eol .)* %eol ) -> ''
+
 texlike_text     <- { (!%specials . / %escaped_specials)+ } -> unescapeSpecials
+
 texlike_braced_stuff <- '{' texlike_stuff '}'
+
 texlike_command  <- '\' ({:command: passthrough_cmd :} {:options: %parameters :}
                     passthrough_braced_stuff / {:command: %cmdID :} {:options: %parameters :}
                     texlike_braced_stuff)
@@ -27,16 +31,26 @@ texlike_command  <- '\' ({:command: passthrough_cmd :} {:options: %parameters :}
 passthrough_cmd  <- 'ftl' / 'lua' / 'math' / 'raw' / 'script' / 'sil' / 'use' / 'xml'
 
 passthrough_stuff <- { {: passthrough_text / passthrough_debraced_stuff :} }
+
 passthrough_env_stuff <- {: passthrough_env_text :}*
+
 passthrough_text <- { [^{}]+ }
+
 passthrough_env_text <- { (!('\end{' =command '}') .)+ }
+
 passthrough_braced_stuff <- '{' passthrough_stuff '}'
+
 passthrough_debraced_stuff <- { passthrough_braced_stuff }
+
 notpass_end <- '\end{' =command '}' _
+
 pass_end <- '\end{' =command '}' _
 
 _   <- %s*
-]=], {
+
+]=]
+
+local grammar = re.compile(expression, {
   unescapeSpecials = function (str)
     return str:gsub('\\([{}%%\\])', '%1')
   end,
@@ -47,9 +61,8 @@ _   <- %s*
   escaped_specials = P"\\" * specials
 })
 
-print(inspect(g:match([[
-% this is a sample comment
-\begin[papersize=a6]{document}
-Hello, world!
-\end{document}
-]])))
+local function parser (string)
+  return re.match(string, grammar)
+end
+
+return parser
