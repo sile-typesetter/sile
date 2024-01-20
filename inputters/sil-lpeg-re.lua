762fcc9b164c47b0e7544460f95099a6ff5932ec
AlexanderMisel <7542439+AlexanderMisel@users.noreply.github.com>
2024-01-20 14:16:47 +0300

    feat(inputters): Add lpeg-re based alternative parser for SIL
    As found in GH issue comment:
    
    https://github.com/sile-typesetter/sile/issues/1435#issuecomment-1891300997

---
+++ inputters/sil-lpeg-re.lua
@@ -0,0 +1,55 @@
+local lpeg = require('lpeg')
+local re = require('re')
+local bits = require('parserbits')
+local inspect = require('inspect')
+
+local P, C, S = lpeg.P, lpeg.C, lpeg.S
+local myID = C(bits.silidentifier) / 1
+
+local wrapper = function (a) return type(a)=="table" and a or {} end
+local specials = S"{}%\\"
+
+local g = re.compile([=[
+document         <- texlike_stuff !.
+texlike_stuff    <- {: environment / comment / texlike_text / texlike_braced_stuff / texlike_command :}*
+
+environment      <- '\begin' {:options: %parameters :}
+                   ('{' {:command: passthrough_cmd :} '}' passthrough_env_stuff pass_end /
+                    '{' {:command: %cmdID :} '}' texlike_stuff notpass_end)
+
+comment          <- ('%' (!%eol .)* %eol ) -> ''
+texlike_text     <- { (!%specials . / %escaped_specials)+ } -> unescapeSpecials
+texlike_braced_stuff <- '{' texlike_stuff '}'
+texlike_command  <- '\' ({:command: passthrough_cmd :} {:options: %parameters :}
+                    passthrough_braced_stuff / {:command: %cmdID :} {:options: %parameters :}
+                    texlike_braced_stuff)
+
+passthrough_cmd  <- 'ftl' / 'lua' / 'math' / 'raw' / 'script' / 'sil' / 'use' / 'xml'
+
+passthrough_stuff <- { {: passthrough_text / passthrough_debraced_stuff :} }
+passthrough_env_stuff <- {: passthrough_env_text :}*
+passthrough_text <- { [^{}]+ }
+passthrough_env_text <- { (!('\end{' =command '}') .)+ }
+passthrough_braced_stuff <- '{' passthrough_stuff '}'
+passthrough_debraced_stuff <- { passthrough_braced_stuff }
+notpass_end <- '\end{' =command '}' _
+pass_end <- '\end{' =command '}' _
+
+_   <- %s*
+]=], {
+  unescapeSpecials = function (str)
+    return str:gsub('\\([{}%%\\])', '%1')
+  end,
+  cmdID = myID - P"begin" - P"end",
+  parameters = (P"[" * bits.parameters * P"]")^-1 / wrapper,
+  eol = S"\r\n",
+  specials = specials,
+  escaped_specials = P"\\" * specials
+})
+
+print(inspect(g:match([[
+% this is a sample comment
+\begin[papersize=a6]{document}
+Hello, world!
+\end{document}
+]])))
