93c6de70e8c54f7fd4bccf9de0ea857123268a14
Caleb Maclennan <caleb@alerque.com>
2024-06-16 00:50:54 +0300

    test(api): Mockup how to test a Rust function from Lua

--- rusile/src/lib.rs
+++ rusile/src/lib.rs
@@ -7,5 +7,12 @@ use mlua::prelude::*;
 #[mlua::lua_module]
 fn rusile(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table().unwrap();
+    let demo: LuaFunction = lua.create_function(demo).unwrap();
+    exports.set("demo", demo)?;
     Ok(exports)
+}
+
+fn demo(lua: &Lua, (): ()) -> LuaResult<LuaString> {
+    let res = rusile_demo().unwrap();
+    lua.create_string(res)
 }
