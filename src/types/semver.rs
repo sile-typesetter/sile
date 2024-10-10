c955bbfc6dd155f6a5a3b241d24311f680bac7d3
Caleb Maclennan <caleb@alerque.com>
2024-10-10 18:01:09 +0300

    [NFY] Wrap semver::Version as Lua type

--- src/types/semver.rs
+++ src/types/semver.rs
@@ -1,4 +1,18 @@
+use mlua::prelude::*;
 use semver::Version;
+
+pub struct Semver {
+    version: Version,
+}
+
+impl IntoLua for Semver {
+    #[inline]
+    fn into_lua(self, lua: &Lua) -> LuaResult<LuaValue> {
+        let semver = lua.create_table()?;
+        Ok(LuaValue::Table(semver))
+        //"just an str".into_lua(lua)
+    }
+}
 
 pub fn foo() -> crate::Result<()> {
     eprintln!("Run");
