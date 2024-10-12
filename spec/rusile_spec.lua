932252e67bfca8586d252f57875e592b59453148
Caleb Maclennan <caleb@alerque.com>
2024-10-12 23:47:53 +0300

    test(utilities): Add unit tests for semver type

--- spec/rusile_spec.lua
+++ spec/rusile_spec.lua
@@ -1,9 +1,37 @@
 SILE = require("core.sile")
 
-describe("rusile", function ()
+local rusile = SILE._rusile
 
+local callable = require("luassert.util").callable
+
+describe("rusile", function ()
    it("should exist", function ()
-      assert.is.truthy(SILE._rusile)
+      assert.is.truthy(rusile)
    end)
 
+   describe("semver", function ()
+      -- semver = SILE._rusile.semver
+      semver = require("semver")
+
+      it("constructor should exist", function ()
+         assert.is.truthy(callable(semver))
+      end)
+
+      describe("instance", function ()
+         local a = semver("1.3.5")
+         local b = semver("1.3.5")
+         local c = semver("2.4.6")
+
+         it("should evaluate comparisons", function ()
+            assert.is.equal(a, b)
+            assert.is.truthy(a == b)
+            assert.is_not.equal(a, c)
+            assert.is_not.truthy(a == c)
+            assert.is.truthy(a < c)
+            assert.is.truthy(c > b)
+            assert.is.truthy(a <= b)
+            assert.is.truthy(c >= b)
+         end)
+      end)
+   end)
 end)
