b850b24e49de4a30e1e29bf4b5642e7b1423f7c6
Caleb Maclennan <caleb@alerque.com>
2024-10-12 16:46:57 +0300

    chore(rusile): Remove demo, make room to land real stuff

--- rusile/src/lib.rs
+++ rusile/src/lib.rs
@@ -7,6 +7,5 @@ use mlua::prelude::*;
 #[mlua::lua_module]
 fn rusile(lua: &Lua) -> LuaResult<LuaTable> {
     let exports = lua.create_table().unwrap();
-    exports.set("demo", LuaFunction::wrap_raw(sile::rusile_demo))?;
     Ok(exports)
 }
