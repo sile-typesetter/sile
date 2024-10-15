36a5a06d272a01522620fde7dadfb39c46bd1e7f
Caleb Maclennan <caleb@alerque.com>
2024-10-16 01:01:53 +0300

    fixup! fixup! refactor(utilities): Rewrite semver in Rust and wrap as Lua type

--- src/types/semver.rs
+++ src/types/semver.rs
@@ -15,65 +15,10 @@ impl Semver {
     }
 }
 
-//// TODO: cfg gate this so it only ends up in Lua module?
+// TODO: cfg gate this so it only ends up in Lua module?
 pub fn semver(version: String) -> crate::Result<Semver> {
     Ok(Semver::new(&version)?)
 }
-
-//fn use_registered_metatable(lua: &Lua) -> LuaResult<LuaTable> {
-//    let key = "semver_type_metatable";
-//    let metatable: LuaTable = match lua.named_registry_value(key)? {
-//        LuaValue::Table(metatable) => metatable,
-//        LuaValue::Nil => {
-//            let metatable = lua.create_table()?;
-//            metatable.set(
-//                "__tostring",
-//                lua.create_function(|_, arg: LuaTable| {
-//                    let major: u16 = arg.get("major")?;
-//                    let minor: u16 = arg.get("minor")?;
-//                    let patch: u16 = arg.get("patch")?;
-//                    Ok(format!("{}.{}.{}", major, minor, patch))
-//                })?,
-//            )?;
-//            metatable.set(
-//                "__eq",
-//                lua.create_function(|_, _args: (LuaTable, LuaTable)| {
-//                    //dbg!(args);
-//                    //let major: u16 = args.0.get("major")?;
-//                    //let minor: u16 = args.0.get("minor")?;
-//                    //let patch: u16 = args.0.get("patch")?;
-//                    Ok(false)
-//                })?,
-//            )?;
-//            metatable.set(
-//                "__le",
-//                lua.create_function(|_, _args: (LuaTable, LuaTable)| {
-//                    //dbg!(args);
-//                    Ok(false)
-//                })?,
-//            )?;
-//            metatable.set(
-//                "__lt",
-//                lua.create_function(|_, (a, b): (LuaTable, LuaTable)| {
-//                    //let major_is_less: bool =
-//                    //    args.0.get::<u16>("major")? < args.1.get::<u16>("major")?;
-//                    //let m: Semver = args.0;
-//                    dbg!(a);
-//                    //let _minor_is_less: bool =
-//                    //    args.0.get::<u16>("minor")? < args.1.get::<u16>("minor")?;
-//                    //let _patch_is_less: bool =
-//                    //    args.0.get::<u16>("patch")? < args.1.get::<u16>("patch")?;
-//                    //Ok(major_is_less)
-//                    Ok(false)
-//                })?,
-//            )?;
-//            lua.set_named_registry_value(key, &metatable)?;
-//            metatable
-//        }
-//        _ => unreachable!(),
-//    };
-//    Ok(metatable)
-//}
 
 impl Deref for Semver {
     type Target = Version;
@@ -94,23 +39,19 @@ impl LuaUserData for Semver {
             Ok(this.version.to_string())
         });
 
+        methods.add_meta_method(LuaMetaMethod::Eq, |_, this, other: Self| {
+            Ok(this.version == other.version)
+        });
+
+        methods.add_meta_method(LuaMetaMethod::Le, |_, this, other: Self| {
+            Ok(this.version <= other.version)
+        });
+
         methods.add_meta_method(LuaMetaMethod::Lt, |_, this, other: Self| {
             Ok(this.version < other.version)
         });
     }
 }
-
-//impl IntoLua for Semver {
-//    fn into_lua(self, lua: &Lua) -> LuaResult<LuaValue> {
-//        let semver = lua.create_table()?;
-//        semver.set("major", self.version.major)?;
-//        semver.set("minor", self.version.minor)?;
-//        semver.set("patch", self.version.patch)?;
-//        //let metatable: mlua::Table = use_registered_metatable(&lua)?;
-//        //semver.set_metatable(Some(metatable));
-//        Ok(LuaValue::Table(semver))
-//    }
-//}
 
 impl FromLua for Semver {
     fn from_lua(value: LuaValue, _: &Lua) -> LuaResult<Self> {
