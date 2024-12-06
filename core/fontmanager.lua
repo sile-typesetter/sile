4b6aa5ed00747ea22932547770aca62c8add28a4
Caleb Maclennan <caleb@alerque.com>
2024-12-06 13:45:13 +0300

    feat(fonts): Configure macOS to fallback from macfonts to fontconfig

--- core/fontmanager.lua
+++ core/fontmanager.lua
@@ -1,20 +1,36 @@
 local fontManager = {}
+
 fontManager.fontconfig = require("justenoughfontconfig")
-pcall(function ()
-   fontManager.macfonts = require("macfonts")
-end)
+
+local has_macfonts, macfonts = pcall(require, "macfonts")
+if has_macfonts and macfonts then
+   fontManager.macfonts = macfonts
+end
+
+local function create_macfonts_fallback (self)
+   return function (...)
+      SU.debug("fonts", "Checking via macfonts")
+      local status, result = pcall(self.macfonts._face, ...)
+      if status and result and result.filename then
+         SU.debug("fonts", "Found, returning result")
+         return result
+      else
+         SU.debug("fonts", "Not found, trying fontconfig instead")
+         return self.fontconfig._face(...)
+      end
+   end
+end
 
 fontManager.face = function (self, ...)
-   local manager
+   local face
    if SILE.input.fontmanager then
-      manager = self[SILE.input.fontmanager]
+      face = self[SILE.input.fontmanager]._face
+   elseif has_macfonts then
+      face = create_macfonts_fallback(self)
    else
-      manager = self.macfonts and self.macfonts or self.fontconfig
+      face = self.fontconfig._face
    end
-   if not manager then
-      SU.error("Failed to load any working font manager")
-   end
-   return manager._face(...)
+   return face(...)
 end
 
 return fontManager
