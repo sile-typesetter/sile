1105aef3eba61fd19a4952c5ff500e680ab8ff6c
no-vici <no-vici@users.noreply.github.com>
2025-03-25 08:52:28 +0700

    style: Reformat Lua with stylua

--- classes/diglot.lua
+++ classes/diglot.lua
@@ -14,45 +14,45 @@ function class:_init (options)
       SILE.scratch.counters.folio = { value = 1, display = "arabic" }
    end)
 
-	self:declareFrame("a", {
-		left = "2.5%pw",
-		right = "47.5%pw",
-		top = "3.5%ph",
-		bottom = "85%ph",
-	})
-	self:declareFrame("b", {
-		left = "52.5%pw",
-		right = "100%pw-left(a)",
-		top = "top(a)",
-		bottom = "bottom(a)",
-	})
-	self:declareFrame("c", {
-		left = "left(a)",
-		right = "right(a)",
-		top = "bottom(a)",
-		bottom = "bottom(a)+10%ph", -- Adjust as needed for footnote space
-	})
-	self:declareFrame("d", {
-		left = "left(b)",
-		right = "right(b)",
-		top = "bottom(b)",
-		bottom = "bottom(b)+10%ph", -- Adjust as needed for footnote space
-	})
-	self:declareFrame("folio", {
-		left = "left(a)",
-		right = "right(b)",
-		top = "bottom(c)+1.5%ph",
-		bottom = "bottom(c)+4.75%ph",
-	})
+   self:declareFrame("a", {
+      left = "2.5%pw",
+      right = "47.5%pw",
+      top = "3.5%ph",
+      bottom = "85%ph",
+   })
+   self:declareFrame("b", {
+      left = "52.5%pw",
+      right = "100%pw-left(a)",
+      top = "top(a)",
+      bottom = "bottom(a)",
+   })
+   self:declareFrame("c", {
+      left = "left(a)",
+      right = "right(a)",
+      top = "bottom(a)",
+      bottom = "bottom(a)+10%ph", -- Adjust as needed for footnote space
+   })
+   self:declareFrame("d", {
+      left = "left(b)",
+      right = "right(b)",
+      top = "bottom(b)",
+      bottom = "bottom(b)+10%ph", -- Adjust as needed for footnote space
+   })
+   self:declareFrame("folio", {
+      left = "left(a)",
+      right = "right(b)",
+      top = "bottom(c)+1.5%ph",
+      bottom = "bottom(c)+4.75%ph",
+   })
    self:loadPackage("parallel", {
       frames = {
          left = "a",
          right = "b",
       },
-		ftn_frames = {
-			ftn_left = "c",
-			ftn_right = "d",
-		},
+      ftn_frames = {
+         ftn_left = "c",
+         ftn_right = "d",
+      },
    })
 end
 
