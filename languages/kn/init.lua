580d105d65e623e2618bada62d4e4fc956107650
Caleb Maclennan <caleb@alerque.com>
2025-03-31 00:07:26 +0300

    refactor(languages): Onboard Kannada to new module layout

--- languages/kn/init.lua
+++ languages/kn/init.lua
@@ -1,3 +1,14 @@
-SILE.languageSupport.languages.kn = { defaultScript = "Knda" }
-local hyphens = require("languages.kn.hyphens-tex")
-SILE.hyphenator.languages["kn"] = hyphens
+local unicode = require("languages.unicode")
+
+local language = pl.class(unicode)
+language._name = "kn"
+
+function language.declareSettings (_)
+   -- TODO get this *unset* when switching to other languages
+   SILE.settings:set("font.script", "Knda")
+end
+
+-- local hyphens = require("languages.kn.hyphens-tex")
+-- SILE.hyphenator.languages["kn"] = hyphens
+
+return language
