9fc336095af2656526f53930199c62bc657b19e7
Caleb Maclennan <caleb@alerque.com>
2025-04-05 15:41:54 +0300

    refactor(languages): Onboard Norwegian and variants to new module layout

---
+++ languages/nn/hyphens.lua
@@ -0,0 +1,3 @@
+-- Note 'nn' hyphenation rules are the same as 'no' but with different exceptions.
+-- See languages.nn for overrides added after this loads.
+return require("languages.no.hyphens-tex")
