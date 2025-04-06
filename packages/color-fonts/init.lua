0d735765c7d7b9b72f0f4beaa386b90c82338d04
Caleb Maclennan <caleb@alerque.com>
2025-04-06 11:38:22 +0300

    chore(packages)!: Mark color-fonts package as deprecated, handled in shaper modules
    BREAKING CHANGE: Explicitly loading a package to support color fonts is
    no longer necessary. Support is built into the default shaper module and
    the correct shaper will be used automatically when color fonts are
    detected.

--- packages/color-fonts/init.lua
+++ packages/color-fonts/init.lua
@@ -3,11 +3,25 @@ local base = require("packages.base")
 local package = pl.class(base)
 package._name = "color-fonts"
 
+function package._init (_)
+   SU.deprecated(
+      "\\use[module=color-fonts]",
+      nil,
+      "0.16.0",
+      "0.17.0",
+      [[
+         It is no longer necessary to load the color-fonts package. The shaper it
+         provided is automatically loaded when a color font is detected.
+         ]]
+   )
+end
+
 package.documentation = [[
 \begin{document}
-The \autodoc:package{color-fonts} package adds support for fonts with multi-colored glyphs (that is,
-OpenType fonts with \code{COLR} and \code{CPAL} tables).
-This package is automatically loaded when such a font is detected.
+The \autodoc:package{color-fonts} package is obsolete.
+The functionality is provided by a shaper module.
+The correct shaper will be loaded automatically when support for multi-colored glyphs is needed.
+Using OpenType fonts with \code{COLR} and \code{CPAL} tables will trigger the switch.
 \end{document}
 ]]
 
