7d62529bc92dceb0dbd504506fd80eb93953ab7e
Caleb Maclennan <caleb@alerque.com>
2025-04-26 12:08:03 +0500

    chore(utilities): Pass through errors from loading core modules

--- core/utilities/init.lua
+++ core/utilities/init.lua
@@ -875,14 +875,14 @@ function utilities._module_loader (scope)
          local status, module = pcall(require, spec)
          if not status then
             if scope == "languages" then
-               SU.warn(("Unable to load support for language '%s', handling with generic unicode module."):format(key))
+               SU.warn(("Unable to load support for language '%s' (not found or unparsable), handling with generic unicode module."):format(key))
                local unicode = require("languages.unicode")
                local language = pl.class(unicode)
                language._name = key
                self[key] = language
                return language
             else
-               SU.error(("Unable to load core module %s"):format(spec))
+               SU.error(("Unable to load core module %s:\n%s"):format(spec, module))
             end
          end
          self[key] = module
